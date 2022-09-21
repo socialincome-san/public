dd6529dd0e26555432ec80f6249977aa35284977
René Stalder <rene@socialincome.org>
2022-09-21 17:51:43 +0200

    ui: ensure tailwind classes can be applied inside storybook

--- ui/.storybook/preview.js
+++ ui/.storybook/preview.js
@@ -1,3 +1,5 @@
+import "../src/style.css";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "so-primary": "yellow",
+    },
     extend: {},
   },
   plugins: [],
