747df97e6f676d3a07dc4a298168cd58322bb0b4
András Heé <andrashee@users.noreply.github.com>
2022-09-23 10:52:16 +0200

    Copy existing collection interfaces to shared directory (#86)

---
+++ shared/src/interfaces/collections/users.ts
@@ -0,0 +1,27 @@
+import { EntityReference } from "../entityReference";
+
+export type User = {
+  personal: {
+    name: string;
+    lastname: string;
+    gender: string;
+    company: string;
+    phone: string;
+  };
+  address: {
+    city: string;
+    country: string;
+    number: string;
+    street: string;
+    zip: string;
+  };
+  email: string;
+  status: number;
+  stripe_customer_id: string;
+  test_user: boolean;
+  institution: boolean;
+  language: string;
+  location: string;
+  currency: string;
+  organisations_contributors?: EntityReference[];
+};
