003ac8a0ba1095d8c92018ce020e7dfa1ab854f2
renestalder <renestalder@users.noreply.github.com>
2022-09-27 15:21:36 +0000

    Remove preview for PR 85 🛬

--- pr-preview/pr-85/assets/es.map.constructor.6076f549.js
+++
@@ -1,2 +0,0 @@
-import{n as o,o as t}from"./iframe.9d3fb10e.js";var r=o,e=t;r("Map",function(n){return function(){return n(this,arguments.length?arguments[0]:void 0)}},e);
-//# sourceMappingURL=es.map.constructor.6076f549.js.map
--- pr-preview/pr-85/assets/preview.c05f0c48.js
+++
@@ -1,2 +0,0 @@
-const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}};export{o as parameters};
-//# sourceMappingURL=preview.c05f0c48.js.map
