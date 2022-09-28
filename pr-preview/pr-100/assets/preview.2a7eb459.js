6097d47dd43db9eaef91bf74c88240378881ed6f
ssandino <ssandino@users.noreply.github.com>
2022-09-28 17:59:13 +0000

    Remove preview for PR 100 🛬

--- pr-preview/pr-100/assets/preview.2a7eb459.js
+++
@@ -1,2 +0,0 @@
-const e={actions:{argTypesRegex:"^on[A-Z].*"},controls:{expanded:!0,matchers:{color:/(background|color)$/i,date:/Date$/}}};export{e as parameters};
-//# sourceMappingURL=preview.2a7eb459.js.map
--- pr-preview/pr-100/assets/preview.bcee1c10.js
+++
@@ -1,2 +0,0 @@
-import"./iframe.0b42a3ae.js";import{w as o}from"./utils.651f99e3.js";import"./make-decorator.8875dc85.js";module&&module.hot&&module.hot.decline&&module.hot.decline();var m=[o];export{m as decorators};
-//# sourceMappingURL=preview.bcee1c10.js.map
