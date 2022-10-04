11bc1f7e85cf7701821828daf933bd024e9c9a40
andrashee <accounts@ahee.ch>
2022-10-04 08:15:36 +0200

    Cleanup

--- backend/functions/scripts/migrateStripeCharges.ts
+++
@@ -1,8 +0,0 @@
-import {collection} from '../src/useFirestoreAdmin'
-import { storeCharge } from '../src/etl/stripeWebhook'
-import Stripe from 'stripe'
-
-const stripeCharges = await collection<Stripe.Charge>('strip-charges').get()
-stripeCharges.docs.forEach((doc => 
-    storeCharge(doc.data)))
-
