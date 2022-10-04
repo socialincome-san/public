777cc66c6047a678f7051df34369be8d1d1e658f
andrashee <andrashee@users.noreply.github.com>
2022-10-04 06:14:20 +0000

    Prettified Code!

--- backend/functions/scripts/migrateStripeCharges.ts
+++ backend/functions/scripts/migrateStripeCharges.ts
@@ -1,8 +1,6 @@
-import {collection} from '../src/useFirestoreAdmin'
-import { storeCharge } from '../src/etl/stripeWebhook'
-import Stripe from 'stripe'
+import { collection } from '../src/useFirestoreAdmin';
+import { storeCharge } from '../src/etl/stripeWebhook';
+import Stripe from 'stripe';
 
-const stripeCharges = await collection<Stripe.Charge>('strip-charges').get()
-stripeCharges.docs.forEach((doc => 
-    storeCharge(doc.data)))
-
+const stripeCharges = await collection<Stripe.Charge>('strip-charges').get();
+stripeCharges.docs.forEach((doc) => storeCharge(doc.data));
