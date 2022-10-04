98f3fc43977a7dcbd6f0afeea100a7ea3610b711
andrashee <accounts@ahee.ch>
2022-10-04 08:02:54 +0200

    Adjust github actions

---
+++ backend/functions/scripts/migrateStripeCharges.ts
@@ -0,0 +1,8 @@
+import {collection} from '../src/useFirestoreAdmin'
+import { storeCharge } from '../src/etl/stripeWebhook'
+import Stripe from 'stripe'
+
+const stripeCharges = await collection<Stripe.Charge>('strip-charges').get()
+stripeCharges.docs.forEach((doc => 
+    storeCharge(doc.data)))
+
