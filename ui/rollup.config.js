f254fd28f13dab273038f13b1800bc28aef12310
René Stalder <rene@socialincome.org>
2022-10-07 19:09:21 +0200

    chore(ui): create esm/commonjs distribution package
    Create a bundle that can be consumed by other applications, currently
    only available in the same workspaces/repository.

---
+++ ui/rollup.config.js
@@ -0,0 +1,33 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import typescript from 'rollup-plugin-typescript2';
+
+const packageJson = require('./package.json');
+
+export default {
+	input: 'src/index.ts',
+	output: [
+		{
+			file: packageJson.main,
+			format: 'cjs',
+			sourcemap: true,
+		},
+		{
+			file: packageJson.module,
+			format: 'esm',
+			sourcemap: true,
+		},
+	],
+	plugins: [
+		peerDepsExternal(),
+		resolve(),
+		commonjs(),
+		typescript({ tsconfig: './tsconfig.json' }),
+		postcss({
+			sourceMap: true,
+		}),
+		// terser(),
+	],
+};
