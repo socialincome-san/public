f2e30e84be73988579395145abb0101a806564c7
Michael Kündig <michael.kuendig@gmail.com>
2022-10-08 17:48:45 +0200

    Rename function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import { dummyFunction } from './dummy/dummyFunction';
 import { importBalanceMailFunc } from './etl/importPostfinanceBalance';
 
-export const importbalancemail = importBalanceMailFunc;
+export const importBalanceMail = importBalanceMailFunc;
 export const dummyfunction = dummyFunction;
