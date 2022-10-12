d00326bc50ed7dde3ba13f5dcb2b8de7379f7b9e
René Stalder <rene@socialincome.org>
2022-10-12 16:08:11 +0200

    fix(ui): ensure all CSS styles are included in the build

--- survey/src/App.tsx
+++ survey/src/App.tsx
@@ -1,30 +1,23 @@
-import { SoButton } from '@socialincome/ui';
+import { SoButton, SoTypography } from '@socialincome/ui';
+import '@socialincome/ui/dist/index.css';
 import { useState } from 'react';
 import './App.css';
-import reactLogo from './assets/react.svg';
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<div className="App">
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src="/vite.svg" className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://reactjs.org" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
+			<SoTypography element="h1" size="5xl">
+				Vite + React
+			</SoTypography>
 			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+				<SoButton onClick={() => setCount((count) => count + 1)}>count is {count}</SoButton>
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
 			</div>
 			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-			<SoButton onClick={() => console.log('click')}>Social Income Button</SoButton>
 		</div>
 	);
 }
