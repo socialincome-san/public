36417ce862f09fe36abcb124a5b9e3b10c47e6a2
andrashee <accounts@ahee.ch>
2022-11-05 14:53:02 +0100

    rename

--- functions/src/etl/stripeWebhook.ts
+++ functions/src/etl/stripeWebhook.ts
@@ -7,10 +7,10 @@ import { STRIPE_API_READ_KEY, STRIPE_WEBHOOK_SECRET } from '../config';
 import { findFirst } from '../useFirestoreAdmin';
 
 /**
- * Stripe webhook to ingest successfully paid charges into firestore.
+ * Stripe webhook to ingest charge events into firestore.
  * Adds the relevant information to the contributions subcollection of users.
  */
-export const stripeChargeSucceededHookFunc = functions.https.onRequest(async (request, response) => {
+export const stripeChargeHookFunc = functions.https.onRequest(async (request, response) => {
 	const stripe = new Stripe(STRIPE_API_READ_KEY, { apiVersion: '2022-08-01' });
 	try {
 		const sig = request.headers['stripe-signature']!;
