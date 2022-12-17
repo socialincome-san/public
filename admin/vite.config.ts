1d190a85a77606bb281b96ddf333d179dad7294c
Michael Kündig <michael.kuendig@gmail.com>
2022-12-17 10:59:34 +0100

    Update vite.config.ts

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, ViteDevServer } from 'vite';
 
-export default defineConfig(() => ({
+export default defineConfig(({ mode }) => ({
 	server: {
 		host: '0.0.0.0',
 		port: 3000,
@@ -9,9 +9,9 @@ export default defineConfig(() => ({
 		'process.env': {},
 	},
 	plugins: [
-		{
+		mode === 'development' && {
 			// FireCMS relies on a rewrite rule that redirect all traffic to index.html in production (see firebase.json)
-			// For this reason, we need to manually rewrite a few requests manually.
+			// For this reason, we need to manually rewrite a few requests in development.
 			name: 'fix-404-on-refresh',
 			configureServer(server: ViteDevServer) {
 				server.middlewares.use((req, res, next) => {
