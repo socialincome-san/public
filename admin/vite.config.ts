ea6eb8aa4ed2ee12d1571d982c2b79b1c531943c
Michael Kündig <michael.kuendig@gmail.com>
2022-12-17 10:22:56 +0100

    Fix refresh issue in development

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
+import { defineConfig, ViteDevServer } from 'vite';
 
-export default defineConfig(() => ({
+export default defineConfig(({mode}) => ({
 	server: {
 		host: '0.0.0.0',
 		port: 3000,
@@ -8,4 +8,18 @@ export default defineConfig(() => ({
 	define: {
 		'process.env': {},
 	},
+	plugins: [
+		mode === 'development' && {
+			// FireCMS relies on a rewrite rule that redirect all traffic to index.html in production (see firebase.json)
+			// For this reason, we need to manually rewrite a few requests in development.
+			name: 'fix-404-on-refresh',
+			configureServer(server: ViteDevServer) {
+				server.middlewares.use((req, res, next) => {
+					if (req.url === '/c/src/index.tsx') req.url = '/src/index.tsx';
+					if (req.url === '/c/logo.svg') req.url = '/logo.svg';
+					next();
+				});
+			},
+		},
+	],
 }));
