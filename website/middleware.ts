53d2c45abda8b744c46e3e9bfb02adf5af8a42f1
andrashee <accounts@ahee.ch>
2022-12-18 16:48:25 +0530

    modularize

--- website/middleware.ts
+++ website/middleware.ts
@@ -11,15 +11,24 @@ export const middleware = (request: NextRequest) => {
 
 /**
  * Forwards from /transparency/finances to /transparency/finances/[currency] according to the user's location.
- * This allows us to combine incremental static prebuilt pages and personalization based on user's location.
  * @param request
  */
 export const financesMiddleware = (request: NextRequest) => {
 	if (request.nextUrl.pathname.endsWith('/transparency/finances')) {
-		const currency = bestGuessCurrency(request.geo?.country);
-		const redirectUrl = request.nextUrl.clone();
-		redirectUrl.pathname = redirectUrl.pathname + '/' + currency.toLowerCase();
-		return NextResponse.redirect(redirectUrl);
+		return redirectToCurrencyPage(request);
 	}
 	return undefined;
 };
+
+/**
+ * Redirects to the currency subpage of the routed page.
+ * E.g. a request to /transparency/finances gets redirected to /transparency/finances/chf if the request is coming from Switzerland
+ * This allows us to combine incremental static prebuilt pages and personalization based on user's location.
+ * @param request
+ */
+function redirectToCurrencyPage(request: NextRequest) {
+	const currency = bestGuessCurrency(request.geo?.country);
+	const redirectUrl = request.nextUrl.clone();
+	redirectUrl.pathname = redirectUrl.pathname + '/' + currency.toLowerCase();
+	return NextResponse.redirect(redirectUrl);
+}
