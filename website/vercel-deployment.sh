0b8dc08c1f9bfd8827e7acdb528334938ed184b3
andrashee <accounts@ahee.ch>
2022-12-29 10:49:47 +0530

    [Website] Bugfix: Ignore step for vercel deployment

--- website/vercel-deployment.sh
+++ website/vercel-deployment.sh
@@ -3,8 +3,8 @@
 # Proceed with the build if a change in the website directory happened and
 # it's not the gh-pages branch.
 
-if [[ "$VERCEL_GIT_COMMIT_REF" != "gh-pages"]] ; then
-  exit git diff HEAD^ HEAD --quiet .;
+if [[ "$VERCEL_GIT_COMMIT_REF" != "gh-pages" ]] ; then
+  git diff HEAD^ HEAD --quiet .;
 else
   exit 0;
 fi
