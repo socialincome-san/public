102855a246d3ea6be0192022fabf831c9556d6bf
András Heé <andrashee@users.noreply.github.com>
2022-12-31 18:12:16 +0530

    feature(functions): migrate exchange rates import (#242)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,10 @@
 import { createDonationCertificatesFunction } from './donation_certificates/createDonationCertificatesFunction';
+import { importExchangeRatesFunction } from './etl/importExchangeRates';
 import { importBalanceMailFunction } from './etl/importPostfinanceBalance';
 import { batchImportStripeChargesFunction, stripeChargeHookFunction } from './etl/stripeWebhook';
 
 export const batchImportStripeCharges = batchImportStripeChargesFunction;
 export const createDonationCertificates = createDonationCertificatesFunction;
+export const importExchangeRates = importExchangeRatesFunction;
 export const importBalanceMail = importBalanceMailFunction;
 export const stripeChargeHook = stripeChargeHookFunction;
