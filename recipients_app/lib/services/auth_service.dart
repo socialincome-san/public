014fc7634de6dfb67b9d449ea57739dad3be15bf
Verena Zaiser <development@verena-zaiser.de>
2023-01-06 10:24:30 +0100

    added ignore

--- recipients_app/lib/services/auth_service.dart
+++ recipients_app/lib/services/auth_service.dart
@@ -5,7 +5,6 @@ import "package:intl/intl.dart";
 
 // Authentication service provides methods to register, verify phone number and sign in
 class AuthService {
-
   static final AuthService _instance = AuthService._();
 
   // Implementation of Singleton pattern, so all Registration components use the same instance
@@ -72,6 +71,7 @@ class AuthService {
       }
 
       return DateFormat("dd.MM.yyyy").format(creationTime);
+      // ignore: avoid_catching_errors
     } on NoSuchMethodError {
       return "";
     }
