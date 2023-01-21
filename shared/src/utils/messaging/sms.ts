726baf304b2ff8e3db4c7d877dfbf8e21e90c71e
brennerthomas <brennerthomas@users.noreply.github.com>
2023-01-21 15:38:00 +0000

    Prettified Code!

--- shared/src/utils/messaging/sms.ts
+++ shared/src/utils/messaging/sms.ts
@@ -1,46 +1,40 @@
-import { renderTemplate } from "../templates";
+import { renderTemplate } from '../templates';
 
 export interface SendSmsProps {
-	messageRecipientPhone: string,
-	messageContext: object,
-    smsServiceId: string,
-    smsServiceSecret: string,
-    statusCallbackUrl: URL,
-    messageSenderPhone: string
+	messageRecipientPhone: string;
+	messageContext: object;
+	smsServiceId: string;
+	smsServiceSecret: string;
+	statusCallbackUrl: URL;
+	messageSenderPhone: string;
 	templateParameter: {
-        language: string,
-        templatePath?: string,
-        translationNamespace?: string
-    },
+		language: string;
+		templatePath?: string;
+		translationNamespace?: string;
+	};
 }
 
 export const sendSms = async ({
 	messageRecipientPhone,
 	messageContext,
-    smsServiceId,
-    smsServiceSecret,
-    statusCallbackUrl,
-    messageSenderPhone,
-    templateParameter: {
-        language,
-        templatePath,
-        translationNamespace
-    }
+	smsServiceId,
+	smsServiceSecret,
+	statusCallbackUrl,
+	messageSenderPhone,
+	templateParameter: { language, templatePath, translationNamespace },
 }: SendSmsProps) => {
-
 	let messageStatus = 'failed';
 	let messageSid = 'unknown';
 
-
-    const body = await renderTemplate({
+	const body = await renderTemplate({
 		language: language,
-        translationNamespace: translationNamespace as string,
+		translationNamespace: translationNamespace as string,
 		hbsTemplatePath: templatePath as string,
 		context: messageContext,
 	});
 
 	const client = require('twilio')(smsServiceId, smsServiceSecret);
- 
+
 	await client.messages
 		.create({
 			body: body,
@@ -49,11 +43,10 @@ export const sendSms = async ({
 			to: messageRecipientPhone,
 		})
 		.then((message: any) => {
-            console.log(message);
-            messageSid = message.sid;
+			console.log(message);
+			messageSid = message.sid;
 			messageStatus = message.status;
 		});
-        
-    return [messageSid, messageStatus];
-    
+
+	return [messageSid, messageStatus];
 };
