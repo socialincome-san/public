995ba19ad34b2fddce0e85b483e7a705957a9d56
Sandino Scheidegger <sandino@socialincome.org>
2023-01-30 15:44:32 +0100

    Adding the basic pages to rebuild the old website. To be done: connecting the translation variables to the commmon file.

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -7,6 +7,7 @@ import Layout from '../components/Layout';
 
 export default function Home() {
 	const { t } = useTranslation('website-index');
+
 	return (
 		<Layout title={t('index.title')}>
 			<section>
@@ -16,6 +17,28 @@ export default function Home() {
 				<Link href="/transparency/finances">{t('index.linkToFinances')}</Link>
 				<br />
 				<Link href="/social-responsibility">{t('index.linkToSocialResponsibility')}</Link>
+				<br />
+				<Link href="/take-action">{t('common.takeAction')}</Link>
+				<br />
+				<Link href="/our-work">{t('common.ourWork')}</Link>
+				<br />
+				<Link href="/about-us">{t('common.aboutUs')}</Link>
+				<br />
+				<Link href="/newsletter">{t('common.newsletter')}</Link>
+				<br />
+				<Link href="/privacy">{t('common.privacy')}</Link>
+				<br />
+				<Link href="/terms-contributions">{t('common.termsContributions')}</Link>
+				<br />
+				<Link href="/terms-use">{t('common.termsUse')}</Link>
+				<br />
+				<Link href="/the-arts">{t('common.theArts')}</Link>
+				<br />
+				<Link href="/faq">{t('common.faq')}</Link>
+				<br />
+				<Link href="/downloads">{t('common.downloads')}</Link>
+				<br />
+				<Link href="/bank-details">{t('common.bankDetails')}</Link>
 				<br />
 				<br />
 			</section>
---
+++ website/pages/take-action.tsx
@@ -0,0 +1,23 @@
+import { GetStaticProps } from 'next';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Layout from '../components/Layout';
+
+export default function TakeAction() {
+	const { t } = useTranslation('website-take-action');
+	return (
+		<Layout title={t('takeAction.title')}>
+			<section>
+				<p></p>
+			</section>
+		</Layout>
+	);
+}
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+	return {
+		props: {
+			...(await serverSideTranslations(locale!, ['website-take-action'])),
+		},
+	};
+};
