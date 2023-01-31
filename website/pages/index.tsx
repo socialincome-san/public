2768037bcdfb3ab5d2f560de8a9824d9694640f2
andrashee <accounts@ahee.ch>
2023-01-31 23:20:48 +0100

    common fix

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -6,39 +6,40 @@ import ApplePay from '../components/ApplePay';
 import Layout from '../components/Layout';
 
 export default function Home() {
-	const { t } = useTranslation(['website-index', 'common']);
+	const index = useTranslation('website-index');
+	const common = useTranslation('common');
 
 	return (
-		<Layout title={t('index.title')}>
+		<Layout title={index.t('index.title')}>
 			<section>
-				<p>{t('index.welcome')}</p>
+				<p>{index.t('index.welcome')}</p>
 			</section>
 			<section>
-				<Link href="/transparency/finances">{t('index.linkToFinances')}</Link>
+				<Link href="/transparency/finances">{index.t('index.linkToFinances')}</Link>
 				<br />
-				<Link href="/social-responsibility">{t('index.linkToSocialResponsibility')}</Link>
+				<Link href="/social-responsibility">{index.t('index.linkToSocialResponsibility')}</Link>
 				<br />
-				<Link href="/take-action">{t('common.takeAction')}</Link>
+				<Link href="/take-action">{common.t('common.takeAction')}</Link>
 				<br />
-				<Link href="/our-work">{t('common.ourWork')}</Link>
+				<Link href="/our-work">{common.t('common.ourWork')}</Link>
 				<br />
-				<Link href="/about-us">{t('common.aboutUs')}</Link>
+				<Link href="/about-us">{common.t('common.aboutUs')}</Link>
 				<br />
-				<Link href="/newsletter">{t('common.newsletter')}</Link>
+				<Link href="/newsletter">{common.t('common.newsletter')}</Link>
 				<br />
-				<Link href="/privacy">{t('common.privacy')}</Link>
+				<Link href="/privacy">{common.t('common.privacy')}</Link>
 				<br />
-				<Link href="/terms-contributions">{t('common.termsContributions')}</Link>
+				<Link href="/terms-contributions">{common.t('common.termsContributions')}</Link>
 				<br />
-				<Link href="/terms-use">{t('common.termsUse')}</Link>
+				<Link href="/terms-use">{common.t('common.termsUse')}</Link>
 				<br />
-				<Link href="/the-arts">{t('common.theArts')}</Link>
+				<Link href="/the-arts">{common.t('common.theArts')}</Link>
 				<br />
-				<Link href="/faq">{t('common.faq')}</Link>
+				<Link href="/faq">{common.t('common.faq')}</Link>
 				<br />
-				<Link href="/downloads">{t('common.downloads')}</Link>
+				<Link href="/downloads">{common.t('common.downloads')}</Link>
 				<br />
-				<Link href="/bank-details">{t('common.bankDetails')}</Link>
+				<Link href="/bank-details">{common.t('common.bankDetails')}</Link>
 				<br />
 				<br />
 			</section>
