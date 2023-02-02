07ebd980c498a28d82e8c9411f0e93833a5c1d9f
Sandino Scheidegger <sandino@socialincome.org>
2023-02-02 21:46:36 +0100

    Added "website-common" and cleaned up all files for all pages.

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -9,37 +9,49 @@ export default function Home() {
 	const { t } = useTranslation('website-index');
 
 	return (
-		<Layout title={t('title')}>
+		<Layout title={t('website-index.title')}>
 			<section>
-				<p>{t('welcome')}</p>
+				<p>{t('website-index.welcome')}</p>
 			</section>
 			<section>
-				<Link href="/transparency/finances">{t('index.linkToFinances')}</Link>
+				Example translations from website-index
 				<br />
-				<Link href="/social-responsibility">{t('index.linkToSocialResponsibility')}</Link>
+				<Link href="/transparency/finances">{t('website-index.linkToFinances')}</Link>
 				<br />
-				<Link href="/take-action">{t('common.takeAction', { ns: 'common' })}</Link>
+				<Link href="/social-responsibility">{t('website-index.linkToSocialResponsibility')}</Link>
 				<br />
-				<Link href="/our-work">{t('common.ourWork', { ns: 'common' })}</Link>
 				<br />
-				<Link href="/about-us">{t('common.aboutUs', { ns: 'common' })}</Link>
+				Example translations from website-common
 				<br />
-				<Link href="/newsletter">{t('common.newsletter', { ns: 'common' })}</Link>
+				<Link href="/take-action">{t('website-common.takeAction', { ns: 'website-common' })}</Link>
 				<br />
-				<Link href="/privacy">{t('common.privacy', { ns: 'common' })}</Link>
+				<Link href="/our-work">{t('website-common.ourWork', { ns: 'website-common' })}</Link>
 				<br />
-				<Link href="/terms-contributions">{t('common.termsContributions', { ns: 'common' })}</Link>
+				<Link href="/about-us">{t('website-common.aboutUs', { ns: 'website-common' })}</Link>
 				<br />
-				<Link href="/terms-use">{t('common.termsUse', { ns: 'common' })}</Link>
+				<Link href="/newsletter">{t('website-common.newsletter', { ns: 'website-common' })}</Link>
 				<br />
-				<Link href="/the-arts">{t('common.theArts', { ns: 'common' })}</Link>
+				<Link href="/privacy">{t('website-common.privacyPolicy', { ns: 'website-common' })}</Link>
 				<br />
-				<Link href="/faq">{t('common.faq', { ns: 'common' })}</Link>
+				<Link href="/terms-contributions">{t('website-common.termsContributions', { ns: 'website-common' })}</Link>
 				<br />
-				<Link href="/downloads">{t('common.downloads', { ns: 'common' })}</Link>
+				<Link href="/terms-use">{t('website-common.termsUse', { ns: 'website-common' })}</Link>
 				<br />
-				<Link href="/bank-details">{t('common.bankDetails', { ns: 'common' })}</Link>
+				<Link href="/the-arts">{t('website-common.theArts', { ns: 'website-common' })}</Link>
 				<br />
+				<Link href="/faq">{t('website-common.faq', { ns: 'website-common' })}</Link>
+				<br />
+				<Link href="/downloads">{t('website-common.downloads', { ns: 'website-common' })}</Link>
+				<br />
+				<Link href="/bank-details">{t('website-common.bankDetails', { ns: 'website-common' })}</Link>
+				<br />
+				<Link href="/bank-details">{t('website-common.login', { ns: 'website-common' })}</Link>
+				<br />
+				<Link href="/bank-details">{t('website-common.account', { ns: 'website-common' })}</Link>
+				<br />
+				<Link href="/bank-details">{t('website-common.books', { ns: 'website-common' })}</Link>
+				<br />
+				<Link href="/bank-details">{t('website-common.social-responsibility', { ns: 'website-common' })}</Link>
 				<br />
 			</section>
 			<section>
@@ -52,7 +64,7 @@ export default function Home() {
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
 	return {
 		props: {
-			...(await serverSideTranslations(locale!, ['website-index', 'common'])),
+			...(await serverSideTranslations(locale!, ['website-index', 'website-common', 'common'])),
 		},
 	};
 };
