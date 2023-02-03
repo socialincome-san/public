78d360e78a6e3ea8a0f619f71cf9390043500f14
Sandino Scheidegger <sandino@socialincome.org>
2023-02-03 16:50:09 +0100

    Basic website setup with all pages  (#302)
    * Misspellings, temporary gender-specific amendment for French  (long term solution with #300), small text optimizations in German.
    
    * Adding some basic translations that are used all over the page (therefore in the common file)
    
    * Adding the basic pages to rebuild the old website. To be done: connecting the translation variables to the commmon file.
    
    * Attempt to include the common translations #failed
    
    There is also a warning: Cannot resolve file 'finances'
    
    * Added some small changes
    
    * Prettified Code!
    
    * Update translation
    
    * common fix
    
    * json typo
    
    * Prettified Code!
    
    * better approach to use multiple namespaces
    
    * better approach to use multiple namespaces (readme)
    
    * Prettified Code!
    
    * Make sure only (common) translations used in the pages are actually in the common file (by running 'website:extract-translations' and re-adding the text translations.
    
    * Prettified Code!
    
    * Added "website-common" and cleaned up all files for all pages.
    
    * Prettified Code!
    
    * [CI] Update Website Snapshots
    
    * Small change
    
    * adding playwright specs
    
    ---------
    
    Co-authored-by: ssandino <ssandino@users.noreply.github.com>
    Co-authored-by: andrashee <accounts@ahee.ch>
    Co-authored-by: andrashee <andrashee@users.noreply.github.com>

---
+++ website/pages/downloads.tsx
@@ -0,0 +1,23 @@
+import { GetStaticProps } from 'next';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Layout from '../components/Layout';
+
+export default function Downloads() {
+	const { t } = useTranslation('website-downloads');
+	return (
+		<Layout title={t('downloads.title')}>
+			<section>
+				<p></p>
+			</section>
+		</Layout>
+	);
+}
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+	return {
+		props: {
+			...(await serverSideTranslations(locale!, ['website-downloads'])),
+		},
+	};
+};
