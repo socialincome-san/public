bd8287ddc4cbde7d278cbd28f176753771fa110c
Sandino Scheidegger <sandino@socialincome.org>
2023-02-06 21:15:36 +0100

    Work in progress

--- website/pages/bank-details.tsx
+++ website/pages/bank-details.tsx
@@ -2,13 +2,24 @@ import { GetStaticProps } from 'next';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Layout from '../components/Layout';
+import Link from "next/link";
 
 export default function BankDetails() {
 	const { t } = useTranslation('website-bank-details');
 	return (
-		<Layout title={t('bankDetails.title')}>
+		<Layout title={t('website-bank-details.title')}>
 			<section>
-				<p></p>
+				<p>
+					{t('website-bank-details.linkToFinances')}<br/>
+					{t('website-bank-details.bankIntroText')}<br/>
+					{t('website-bank-details.bankInfoTextRecurring')}<br/>
+					{t('website-bank-details.bankOneTime')}<br/>
+					{t('website-bank-details.bankPostFinance')}<br/>
+					{t('website-bank-details.bankAccountHolder')}<br/>
+					{t('website-bank-details.bankTxtIBAN1')}<br/>
+					{t('website-bank-details.bankTxtIBAN2')}<br/>
+					{t('website-bank-details.bankTxtBIC')}
+				</p>
 			</section>
 		</Layout>
 	);
