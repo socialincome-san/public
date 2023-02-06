37a1cc7ca5de6339ed1629f594e948eed278a718
kscujia <kscujia@users.noreply.github.com>
2023-02-06 15:08:26 +0000

    Prettified Code!

--- functions/src/etl/FirestoreNewsletter.ts
+++ functions/src/etl/FirestoreNewsletter.ts
@@ -5,7 +5,7 @@ import { Dictionary } from 'lodash';
 import { FirestoreAdmin } from '../../../shared/src/firebase/FirestoreAdmin';
 import { MAILCHIMP_API_KEY, MAILCHIMP_SERVER, MAILCHIMP_LIST_ID } from '../config';
 
-const client = require("@mailchimp/mailchimp_marketing");
+const client = require('@mailchimp/mailchimp_marketing');
 
 export class FirestoreNewsletter {
 	readonly firestoreAdmin: FirestoreAdmin;
@@ -14,36 +14,33 @@ export class FirestoreNewsletter {
 		this.firestoreAdmin = firestoreAdmin;
 	}
 
-	addToMailchimp = async (dobject_old: Dictionary<String>, dobject: Dictionary<String>, fb_docid: String) : Promise<any> => {
-
+	addToMailchimp = async (
+		dobject_old: Dictionary<String>,
+		dobject: Dictionary<String>,
+		fb_docid: String
+	): Promise<any> => {
 		client.setConfig({
 			apiKey: MAILCHIMP_API_KEY,
-			server: MAILCHIMP_SERVER
+			server: MAILCHIMP_SERVER,
 		});
 
 		try {
 			/**
 			 * if nothing has changed, skip
 			 */
-			if (dobject_old == dobject){
-				console.log("nothings changed");
-				return Promise.resolve("Successful");
+			if (dobject_old == dobject) {
+				console.log('nothings changed');
+				return Promise.resolve('Successful');
 			}
 
 			/**
 			 * if email has changed, archive old mc entry and create a new one
 			 */
 			if (dobject_old.email != dobject.email) {
-				const subscriberHash = crypto
-					.createHash("md5")
-					.update(dobject_old.email.toLowerCase())
-					.digest("hex");
-	
-				let response = client.lists.deleteListMember(
-					MAILCHIMP_LIST_ID,
-					subscriberHash,
-				);
-				await(response);
+				const subscriberHash = crypto.createHash('md5').update(dobject_old.email.toLowerCase()).digest('hex');
+
+				let response = client.lists.deleteListMember(MAILCHIMP_LIST_ID, subscriberHash);
+				await response;
 			}
 
 			/**
@@ -53,34 +50,29 @@ export class FirestoreNewsletter {
 				email_address: dobject.email,
 				status: dobject.mc_status,
 				merge_fields: {
-					'FNAME': dobject.fname,
-					'LNAME':  dobject.lname,
-					'COUNTRY': dobject.country,
-					'GENDER': dobject.gender,
-					'CURRENCY': dobject.curency,
-					'LANGUAGE': dobject.language,
-					'SI_STATUS': dobject.si_status,
-					'FB_UID': fb_docid
-				}
-			}
-			let response = client.lists.setListMember(
-				MAILCHIMP_LIST_ID,
-				dobject.email,
-				mc_data,
-			);
-			await(response);
-			return Promise.resolve("Successful");
+					FNAME: dobject.fname,
+					LNAME: dobject.lname,
+					COUNTRY: dobject.country,
+					GENDER: dobject.gender,
+					CURRENCY: dobject.curency,
+					LANGUAGE: dobject.language,
+					SI_STATUS: dobject.si_status,
+					FB_UID: fb_docid,
+				},
+			};
+			let response = client.lists.setListMember(MAILCHIMP_LIST_ID, dobject.email, mc_data);
+			await response;
+			return Promise.resolve('Successful');
 		} catch (error) {
 			functions.logger.error(`Error upating subscribers-newsletter for: ${dobject.email}`, error);
 			return Promise.resolve(undefined);
 		}
 	};
 
-	registerTrigger = functions.firestore.document("newsletter-subscribers/{docId}")
-		.onWrite(async (change, context) => {
-			console.log('Syncing changes in newsletter-subscribers');
-			let dobject_before = (change.before.data() as Dictionary<String>)!
-			let dobject_after = (change.after.data() as Dictionary<String>)!
-			await this.addToMailchimp(dobject_before, dobject_after, context.params.docId);
-		});
+	registerTrigger = functions.firestore.document('newsletter-subscribers/{docId}').onWrite(async (change, context) => {
+		console.log('Syncing changes in newsletter-subscribers');
+		let dobject_before = (change.before.data() as Dictionary<String>)!;
+		let dobject_after = (change.after.data() as Dictionary<String>)!;
+		await this.addToMailchimp(dobject_before, dobject_after, context.params.docId);
+	});
 }
