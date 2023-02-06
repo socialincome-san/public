0425b7c806214c51597e3244bf0f7a161cd3e07b
ssandino <ssandino@users.noreply.github.com>
2023-02-06 20:24:12 +0000

    Prettified Code!

--- website/pages/bank-details.tsx
+++ website/pages/bank-details.tsx
@@ -2,7 +2,7 @@ import { GetStaticProps } from 'next';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Layout from '../components/Layout';
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function BankDetails() {
 	const { t } = useTranslation('website-bank-details');
@@ -10,14 +10,22 @@ export default function BankDetails() {
 		<Layout title={t('website-bank-details.title')}>
 			<section>
 				<p>
-					{t('website-bank-details.linkToFinances')}<br/>
-					{t('website-bank-details.bankIntroText')}<br/>
-					{t('website-bank-details.bankInfoTextRecurring')}<br/>
-					{t('website-bank-details.bankOneTime')}<br/>
-					{t('website-bank-details.bankPostFinance')}<br/>
-					{t('website-bank-details.bankAccountHolder')}<br/>
-					{t('website-bank-details.bankTxtIBAN1')}<br/>
-					{t('website-bank-details.bankTxtIBAN2')}<br/>
+					{t('website-bank-details.linkToFinances')}
+					<br />
+					{t('website-bank-details.bankIntroText')}
+					<br />
+					{t('website-bank-details.bankInfoTextRecurring')}
+					<br />
+					{t('website-bank-details.bankOneTime')}
+					<br />
+					{t('website-bank-details.bankPostFinance')}
+					<br />
+					{t('website-bank-details.bankAccountHolder')}
+					<br />
+					{t('website-bank-details.bankTxtIBAN1')}
+					<br />
+					{t('website-bank-details.bankTxtIBAN2')}
+					<br />
 					{t('website-bank-details.bankTxtBIC')}
 				</p>
 			</section>
