4adbaf4f0b5f90b9d8fad5c681e4ecf515ea5f08
andrashee <accounts@ahee.ch>
2023-02-25 12:02:23 +0100

    language

--- shared/src/types/admin/Survey.ts
+++ shared/src/types/admin/Survey.ts
@@ -1,4 +1,6 @@
-export const SURVEY_PATH = 'surveys';
+import { RecipientMainLanguage } from './Recipient';
+
+export const SURVEY_FIRETORE_PATH = 'surveys';
 
 export enum SurveyStatus {
 	New = 'new',
@@ -9,11 +11,14 @@ export enum SurveyStatus {
 }
 
 export type Survey = {
+	recipient_name: string; // used to address the recipient in the form. E.g. Hello XYZ.
+	language: RecipientMainLanguage; // used to issue the survey in the appropriate language
+	due_date_at: Date; // till when the survey should be completed
+	sent_at?: Date; // date when the survey was sent to the recipient
+	completed_at?: Date; // date when the survey was completed
+	status: SurveyStatus; // the different states of a survey
+	comments?: string; // additional comments
+	data: any; // the survey data
 	access_email: string; // firebase auth user who can update this survey
 	access_pw: string; // firebase auth user who can update this survey
-	due_date_at: Date; // till when the survey should be completed
-	sent_at: Date; // date when the survey was sent to the recipient
-	completed_at: Date; // date when the survey was completed
-	status: SurveyStatus;
-	data: any; // the survey data
 };
