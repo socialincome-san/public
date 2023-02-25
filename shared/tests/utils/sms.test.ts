29fa27a510c217d678888443316bfad91471fa87
thomasbrenner-ch <thomasbrenner-ch@users.noreply.github.com>
2023-02-25 13:35:17 +0000

    Prettified Code!

--- shared/tests/utils/sms.test.ts
+++ shared/tests/utils/sms.test.ts
@@ -1,28 +1,26 @@
-import { TWILIO_SID, TWILIO_TOKEN } from "../../src/config";
-import { sendSms } from "../../src/utils/messaging/sms"
+import { TWILIO_SID, TWILIO_TOKEN } from '../../src/config';
+import { sendSms } from '../../src/utils/messaging/sms';
 
 describe('send simple Sms', () => {
 	test('send simple free text sms', async () => {
-
-		let [messageSid, messageStatus] = ['',''];
+		let [messageSid, messageStatus] = ['', ''];
 
 		const statusCallbackUrl = new URL('https://test.test');
 
 		[messageSid, messageStatus] = await sendSms({
-			messageRecipientPhone:'+41767777777',
+			messageRecipientPhone: '+41767777777',
 			messageContext: {
-				content: "This is a Test SMS"
+				content: 'This is a Test SMS',
 			},
 			smsServiceId: TWILIO_SID,
 			smsServiceSecret: TWILIO_TOKEN,
 			statusCallbackUrl: statusCallbackUrl,
-			messageSenderPhone: "+15005550006",
+			messageSenderPhone: '+15005550006',
 			templateParameter: {
 				language: 'de',
 				templatePath: 'sms/freetext.hbs',
-				translationNamespace: "freetext.json"
-
-			}
+				translationNamespace: 'freetext.json',
+			},
 		});
 
 		expect(messageStatus).toBe('queued');
