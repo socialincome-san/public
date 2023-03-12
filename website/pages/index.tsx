d89a706e79912eee1a896ece3d8310058a9690c3
Sandino Scheidegger <sandino@socialincome.org>
2023-03-12 22:20:57 +0100

    Added text variables on index page

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -9,17 +9,64 @@ export default function Home() {
 	const { t } = useTranslation('website-index');
 	// noinspection HtmlUnknownTarget
 	return (
-		<Layout title={t('website-index.title')}>
+		<Layout title={t('index.title')}>
+			<br />Index Page Content:<br />
 			<section>
-				<p>{t('website-index.welcome')}</p>
+				<p>{t('index.slogan-1')}</p>
+				<p>{t('index.calculator-title')}</p>
+				<p>{t('index.calculator-title-help')}</p>
+				<p>{t('index.calculator-cta')}</p>
+				<p>{t('index.calculator-privacy')}</p>
+				<p>{t('index.calculator-taxes')}</p>
 			</section>
 			<section>
-				Example translations from website-index
-				<br />
-				<Link href="/transparency/finances">{t('website-index.linkToFinances')}</Link>
-				<br />
-				<Link href="/social-responsibility">{t('website-index.linkToSocialResponsibility')}</Link>
-				<br />
+				<p>{t('index.video-title')}</p>
+				<p>{t('index.video-subtitle')}</p>
+				<p>{t('index.video-cta')}</p>
+			</section>
+			<section>
+				<p>{t('index.idea-chapter')}</p>
+				<p>{t('index.idea-title')}</p>
+				<p>{t('index.idea-lead')}</p>
+				<p>{t('index.idea-p1')}</p>
+				<p>{t('index.idea-p2')}</p>
+				<p>{t('index.idea-p3')}</p>
+				<p>{t('index.idea-p4')}</p>
+			</section>
+			<section>
+				<p>{t('index.approach-title')}</p>
+				<p>{t('index.approach-box1-title')}</p>
+				<p>{t('index.approach-box1-argument1')}</p>
+				<p>{t('index.approach-box1-argument2')}</p>
+				<p>{t('index.approach-box1-argument3')}</p>
+				<p>{t('index.approach-box1-argument4')}</p>
+				<p>{t('index.approach-box1-modal-p1')}</p>
+				<p>{t('index.approach-box1-modal-p2')}</p>
+				<p>{t('index.approach-box2-title')}</p>
+				<p>{t('index.approach-box2-argument1')}</p>
+				<p>{t('index.approach-box2-argument2')}</p>
+				<p>{t('index.approach-box2-argument3')}</p>
+				<p>{t('index.approach-box2-argument4')}</p>
+				<p>{t('index.approach-box2-modal-p1')}</p>
+				<p>{t('index.approach-box2-modal-p2')}</p>
+				<p>{t('index.approach-box3-title')}</p>
+				<p>{t('index.approach-box3-argument1')}</p>
+				<p>{t('index.approach-box3-argument2')}</p>
+				<p>{t('index.approach-box3-argument3')}</p>
+				<p>{t('index.approach-box3-argument4')}</p>
+				<p>{t('index.approach-box3-modal-p1')}</p>
+				<p>{t('index.approach-box3-modal-p2')}</p>
+				<p>{t('index.approach-box-articles')}</p>
+				<p>{t('index.approach-box-faq')}</p>
+				<p>{t('index.approach-box-close')}</p>
+				<p>{t('index.approach-box-evidence')}</p>
+				<p>{t('index.approach-box-takeaction')}</p>
+			</section>
+			<section>
+				<p>{t('index.recognition-title')}</p>
+				<p>{t('index.recognition-lead')}</p>
+			</section>
+			<section>
 				<br />
 				Example translations from website-common
 				<br />
@@ -45,13 +92,13 @@ export default function Home() {
 				<br />
 				<Link href="/bank-details">{t('website-common.bankDetails', { ns: 'website-common' })}</Link>
 				<br />
-				<Link href="/bank-details">{t('website-common.login', { ns: 'website-common' })}</Link>
+				<Link href="/login">{t('website-common.login', { ns: 'website-common' })}</Link>
 				<br />
-				<Link href="/bank-details">{t('website-common.account', { ns: 'website-common' })}</Link>
+				<Link href="/account">{t('website-common.account', { ns: 'website-common' })}</Link>
 				<br />
-				<Link href="/bank-details">{t('website-common.books', { ns: 'website-common' })}</Link>
+				<Link href="/books">{t('website-common.books', { ns: 'website-common' })}</Link>
 				<br />
-				<Link href="/bank-details">{t('website-common.social-responsibility', { ns: 'website-common' })}</Link>
+				<Link href="/social-responsibility">{t('website-common.social-responsibility', { ns: 'website-common' })}</Link>
 				<br />
 			</section>
 			<section>
