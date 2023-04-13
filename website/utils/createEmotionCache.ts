555001abdf097986ea7bbe30b6d45c1fbfeedb7f
andrashee <andrashee@users.noreply.github.com>
2023-04-13 16:06:41 +0000

    Prettified Code!

--- website/utils/createEmotionCache.ts
+++ website/utils/createEmotionCache.ts
@@ -6,14 +6,12 @@ const isBrowser = typeof document !== 'undefined';
 // This assures that MUI styles are loaded first.
 // It allows developers to easily override MUI styles with other styling solutions, like CSS modules.
 export default function createEmotionCache() {
-  let insertionPoint;
+	let insertionPoint;
 
-  if (isBrowser) {
-    const emotionInsertionPoint = document.querySelector<HTMLMetaElement>(
-      'meta[name="emotion-insertion-point"]',
-    );
-    insertionPoint = emotionInsertionPoint ?? undefined;
-  }
+	if (isBrowser) {
+		const emotionInsertionPoint = document.querySelector<HTMLMetaElement>('meta[name="emotion-insertion-point"]');
+		insertionPoint = emotionInsertionPoint ?? undefined;
+	}
 
-  return createCache({ key: 'mui-style', insertionPoint });
+	return createCache({ key: 'mui-style', insertionPoint });
 }
