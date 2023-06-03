25ec663082bdf84e97df2b822ee5760575600d6e
mkue <mkue@users.noreply.github.com>
2023-06-03 09:24:42 +0000

    Deploy preview for PR 427 🛫

---
+++ pr-preview/pr-427/assets/preview-7a9a49d5.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-8657f533.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-f285e414.js"),["assets/DocsRenderer-EYKKDMVH-f285e414.js","assets/iframe-8657f533.js","assets/index-ec8b93d8.js","assets/_commonjsHelpers-042e6b4d.js","assets/react-18-c16649b5.js","assets/index-a620515c.js","assets/index-bfd2ba30.js","assets/index-d475d2ea.js","assets/index-d37d4223.js","assets/index-356e4a49.js"]);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-7a9a49d5.js.map
