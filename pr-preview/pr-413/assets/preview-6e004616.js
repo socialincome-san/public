295365a544d23d6a0742d6953e1bd3471443cd49
mkue <mkue@users.noreply.github.com>
2023-06-03 09:24:19 +0000

    Deploy preview for PR 413 🛫

---
+++ pr-preview/pr-413/assets/preview-6e004616.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-107168ed.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-725b779c.js"),["assets/DocsRenderer-EYKKDMVH-725b779c.js","assets/iframe-107168ed.js","assets/index-ec8b93d8.js","assets/_commonjsHelpers-042e6b4d.js","assets/react-18-c16649b5.js","assets/index-a620515c.js","assets/index-e89b86c1.js","assets/index-d475d2ea.js","assets/index-d37d4223.js","assets/index-356e4a49.js"]);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-6e004616.js.map
---
+++ pr-preview/pr-413/assets/preview-72eec15b.js
@@ -0,0 +1,2 @@
+const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{expanded:!0,matchers:{color:/(background|color)$/i,date:/Date$/}}};export{o as parameters};
+//# sourceMappingURL=preview-72eec15b.js.map
