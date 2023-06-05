36c7e4a8c0f1e4bb4f9c1390c547942cad0b6463
mkue <michael@socialincome.org>
2023-06-05 14:19:17 +0200

    WiP

---
+++ admin/src/init.ts
@@ -0,0 +1,65 @@
+import { getApp } from 'firebase/app';
+import { browserSessionPersistence, connectAuthEmulator, getAuth } from 'firebase/auth';
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
+import { connectFunctionsEmulator, getFunctions } from 'firebase/functions';
+import { connectStorageEmulator, getStorage } from 'firebase/storage';
+
+export function getFirebaseConfig() {
+	return import.meta.env.MODE === 'development'
+		? {
+				apiKey: import.meta.env.VITE_ADMIN_FB_API_KEY,
+				authDomain: import.meta.env.VITE_ADMIN_FB_AUTH_DOMAIN,
+				databaseURL: import.meta.env.VITE_ADMIN_FB_DATABASE_URL,
+				projectId: import.meta.env.VITE_ADMIN_FB_PROJECT_ID,
+				storageBucket: import.meta.env.VITE_ADMIN_FB_STORAGE_BUCKET,
+				messagingSenderId: import.meta.env.VITE_ADMIN_FB_MESSAGING_SENDER_ID,
+				measurementId: import.meta.env.VITE_ADMIN_FB_MEASUREMENT_ID,
+		  }
+		: undefined; // In staging/production, config is loaded from the environment variables set by Firebase Hosting
+}
+
+export async function onFirebaseInit() {
+	const auth = getAuth();
+	if (import.meta.env.VITE_PLAYWRIGHT_TESTS === 'true') {
+		console.log('Running Playwright tests, using session storage for persistence');
+		await auth.setPersistence(browserSessionPersistence);
+	}
+	if (import.meta.env.VITE_ADMIN_FB_AUTH_EMULATOR_URL) {
+		connectAuthEmulator(auth, import.meta.env.VITE_ADMIN_FB_AUTH_EMULATOR_URL, { disableWarnings: true });
+		console.log('Using auth emulator');
+	} else {
+		console.log('Using production auth');
+	}
+	if (import.meta.env.VITE_ADMIN_FB_FIRESTORE_EMULATOR_HOST && import.meta.env.VITE_ADMIN_FB_FIRESTORE_EMULATOR_PORT) {
+		connectFirestoreEmulator(
+			getFirestore(),
+			import.meta.env.VITE_ADMIN_FB_FIRESTORE_EMULATOR_HOST,
+			+import.meta.env.VITE_ADMIN_FB_FIRESTORE_EMULATOR_PORT
+		);
+		console.log('Using firestore emulator');
+	} else {
+		console.log('Using production firestore');
+	}
+	if (import.meta.env.VITE_ADMIN_FB_STORAGE_EMULATOR_HOST && import.meta.env.VITE_ADMIN_FB_STORAGE_EMULATOR_PORT) {
+		connectStorageEmulator(
+			getStorage(),
+			import.meta.env.VITE_ADMIN_FB_STORAGE_EMULATOR_HOST,
+			+import.meta.env.VITE_ADMIN_FB_STORAGE_EMULATOR_PORT
+		);
+		console.log('Using storage emulator');
+	} else {
+		console.log('Using production storage');
+	}
+	if (import.meta.env.VITE_FB_FUNCTIONS_EMULATOR_HOST && import.meta.env.VITE_FB_FUNCTIONS_EMULATOR_PORT) {
+		const app = getApp();
+		const functions = getFunctions(app);
+		connectFunctionsEmulator(
+			functions,
+			import.meta.env.VITE_FB_FUNCTIONS_EMULATOR_HOST,
+			+import.meta.env.VITE_FB_FUNCTIONS_EMULATOR_PORT
+		);
+		console.log('Using functions emulator');
+	} else {
+		console.log('Using production functions');
+	}
+}
--- functions/src/admin/TwilioIncomingMessageHandler.ts
+++ functions/src/admin/TwilioIncomingMessageHandler.ts
@@ -15,49 +15,6 @@ export class TwilioIncomingMessageHandler {
 		this.firestoreAdmin = firestoreAdmin;
 	}
 
-	/**
-	 * For local testing purposes we use ngrok to forward webhooks:
-	 * 1. Setup Whatsapp Sandbox in Twilio and activate your personal phone number on https://console.twilio.com/us1/develop/sms/try-it-out/whatsapp-learn
-	 * 2. Install ngrok on your local machine and make an account on ngrok
-	 * 3. Start ngrok locally with ngrok http 5001
-	 * 4. Update on https://console.twilio.com/us1/develop/sms/try-it-out/whatsapp-learn
-	 *    the incoming message field with https://xxxx-yyy-vvv-www-zzz.eu.ngrok.io/social-income-prod/us-central1/twilioIncomingMessage
-	 * 5. Send test whatsapp to your personal phone number, by answering the function below is triggered
-	 */
-	twilioIncomingMessageFunction = functions.https.onRequest(async (request, response) => {
-		if (request.body) {
-			console.log(request.body);
-
-			const recipientId: string = await this.findRecipient(request.body.WaId);
-			console.log(recipientId);
-
-			if (recipientId.length > 0) {
-				switch ((request.body.Body as String).toLocaleLowerCase()) {
-					case 'yes': {
-						await this.optInWhatsapp(recipientId);
-						break;
-					}
-					//TODO: Implement logic to deal with confirmed payments
-					/*
-                    case "confirmed": {
-                        await this.confirmPaymentWhatsapp(recipientId);
-                        break;
-                    }
-                    */
-					default: {
-						await this.defaultWhatsapp(recipientId);
-					}
-				}
-
-				const messageCollection = this.firestoreAdmin.collection<TwilioMessage>(
-					`${RECIPIENT_FIRESTORE_PATH}/${recipientId}/${MESSAGE_FIRESTORE_PATH}`
-				);
-				await messageCollection.add({ type: MessageType.WHATSAPP, ...request.body });
-			}
-		}
-		response.send();
-	});
-
 	defaultWhatsapp = async (WaID: string) => {
 		//We can think whether we want to implement a re-try logic, but there is a risk that we pay a lot of money
 	};
@@ -97,4 +54,47 @@ export class TwilioIncomingMessageHandler {
 			});
 		return recipientId;
 	};
+
+	/**
+	 * For local testing purposes we use ngrok to forward webhooks:
+	 * 1. Setup Whatsapp Sandbox in Twilio and activate your personal phone number on https://console.twilio.com/us1/develop/sms/try-it-out/whatsapp-learn
+	 * 2. Install ngrok on your local machine and make an account on ngrok
+	 * 3. Start ngrok locally with ngrok http 5001
+	 * 4. Update on https://console.twilio.com/us1/develop/sms/try-it-out/whatsapp-learn
+	 *    the incoming message field with https://xxxx-yyy-vvv-www-zzz.eu.ngrok.io/social-income-staging/us-central1/twilioIncomingMessage
+	 * 5. Send test whatsapp to your personal phone number, by answering the function below is triggered
+	 */
+	twilioIncomingMessageFunction = functions.https.onRequest(async (request, response) => {
+		if (request.body) {
+			console.log(request.body);
+
+			const recipientId: string = await this.findRecipient(request.body.WaId);
+			console.log(recipientId);
+
+			if (recipientId.length > 0) {
+				switch ((request.body.Body as String).toLocaleLowerCase()) {
+					case 'yes': {
+						await this.optInWhatsapp(recipientId);
+						break;
+					}
+					//TODO: Implement logic to deal with confirmed payments
+					/*
+                    case "confirmed": {
+                        await this.confirmPaymentWhatsapp(recipientId);
+                        break;
+                    }
+                    */
+					default: {
+						await this.defaultWhatsapp(recipientId);
+					}
+				}
+
+				const messageCollection = this.firestoreAdmin.collection<TwilioMessage>(
+					`${RECIPIENT_FIRESTORE_PATH}/${recipientId}/${MESSAGE_FIRESTORE_PATH}`
+				);
+				await messageCollection.add({ type: MessageType.WHATSAPP, ...request.body });
+			}
+		}
+		response.send();
+	});
 }
