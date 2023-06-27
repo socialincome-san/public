b4b68ff3afc2900b6734e5c20f5de2670d9a032a
mkue <mkue@users.noreply.github.com>
2023-06-27 08:52:06 +0000

    Deploy preview for PR 464 🛫

---
+++ pr-preview/pr-464/assets/preview-0804e521.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-a8b4fad8.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-65787539.js"),["assets/DocsRenderer-EYKKDMVH-65787539.js","assets/iframe-a8b4fad8.js","assets/index-5ac02154.js","assets/_commonjsHelpers-725317a4.js","assets/react-18-a380e325.js","assets/index-b4c6cb00.js","assets/index-5440658a.js","assets/index-d475d2ea.js","assets/index-d37d4223.js","assets/index-356e4a49.js"]);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-0804e521.js.map
