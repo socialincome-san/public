adce0ef0972fa94f3efdb1a18247070db91f3a1f
mkue <michael@socialincome.org>
2023-07-10 13:15:16 +0200

    bugfix(website): escape newline characters before parsing service account json

--- website/src/firebase/admin.ts
+++ website/src/firebase/admin.ts
@@ -4,12 +4,12 @@ import { StorageAdmin } from '@socialincome/shared/src/firebase/admin/StorageAdm
 import { getOrInitializeFirebaseAdmin } from '@socialincome/shared/src/firebase/admin/app';
 import { credential } from 'firebase-admin';
 
-// Escape line breaks from the environment variable so that JSON.parse() can parse it.
+// FIREBASE_SERVICE_ACCOUNT_JSON should only be a single line where the content of private_key contains \n characters.
+// Escape line breaks from the environment variable so that JSON.parse() can parse the string.
 const serviceAccountJSON = process.env.FIREBASE_SERVICE_ACCOUNT_JSON?.replaceAll('\n', '\\n')
 const databaseURL = process.env.FIREBASE_DATABASE_URL
 
 export const app = getOrInitializeFirebaseAdmin(
-	// Important: The content of FIREBASE_SERVICE_ACCOUNT_JSON should not contain any line breaks
 	serviceAccountJSON && databaseURL
 		? {
 				credential: credential.cert(JSON.parse(serviceAccountJSON)),
