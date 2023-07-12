984c98d43ea9f2e67c464110cfed7230555d6bd5
Michael Kündig <michael@socialincome.org>
2023-07-12 11:38:51 +0200

    refactor(ui): update components (wip) (#480)

--- ui/src/components/index.ts
+++ ui/src/components/index.ts
@@ -3,6 +3,7 @@ export * from './collapsible-menu/index';
 export * from './combobox/index';
 export * from './container/index';
 export * from './dropdown/index';
+export * from './input/index';
 export * from './select/index';
 export * from './typography/index';
 
