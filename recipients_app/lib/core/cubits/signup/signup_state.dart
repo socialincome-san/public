09a1a1415956e56d58f68db0f87e8121b0c283d5
Mikolaj <mdemkow@gmail.com>
2023-08-02 23:04:29 +0200

    Passing forceResendingToken to make it possible to not wait for timeout after going back to phone input

--- recipients_app/lib/core/cubits/signup/signup_state.dart
+++ recipients_app/lib/core/cubits/signup/signup_state.dart
@@ -14,28 +14,32 @@ class SignupState extends Equatable {
   final SignupStatus status;
   final String? phoneNumber;
   final String? verificationId;
+  final int? forceResendingToken;
   final Exception? exception;
 
   const SignupState({
     this.status = SignupStatus.enterPhoneNumber,
     this.phoneNumber,
     this.verificationId,
+    this.forceResendingToken,
     this.exception,
   });
 
   @override
-  List<Object?> get props => [status, phoneNumber, verificationId, exception];
+  List<Object?> get props => [status, phoneNumber, verificationId, forceResendingToken, exception];
 
   SignupState copyWith({
     SignupStatus? status,
     String? phoneNumber,
     String? verificationId,
+    int? forceResendingToken,
     Exception? exception,
   }) {
     return SignupState(
       status: status ?? this.status,
       phoneNumber: phoneNumber ?? this.phoneNumber,
       verificationId: verificationId ?? this.verificationId,
+      forceResendingToken: forceResendingToken ?? this.forceResendingToken,
       exception: exception ?? this.exception,
     );
   }
