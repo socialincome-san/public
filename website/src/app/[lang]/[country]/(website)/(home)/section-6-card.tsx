1893d6a617561da39db40e1dd705ca3aa75971dd
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-08-07 15:44:47 +0100

    section-6 website completed

---
+++ website/src/app/[lang]/[country]/(website)/(home)/section-6-card.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import { Card, Modal, Typography } from '@socialincome/ui';
+import { useCallback, useRef } from 'react';
+import Image from 'next/image'
+import sdgLogo from './sdgLogo.svg'
+
+
+
+type SectionCardProps = {
+	titles: {
+		main: string;
+		articles: string;
+		faqs: string;
+	};
+	items?: string[];
+	paragraphs?: string[];
+	articles?: { title: string; author: string; link: string }[];
+};
+
+export function SectionCardSix({ titles, items = [], paragraphs = [], articles = [] }: SectionCardProps) {
+	const ref = useRef<HTMLDialogElement>(null);
+	const handleShow = useCallback(() => ref.current?.showModal(), [ref]);
+
+	return (
+		<>
+			<Card normal bordered className="border-neutral my-4 cursor-pointer lg:mx-4" onClick={handleShow}>
+				<Card.Body>
+					<Card.Title>
+						<Typography size="2xl" weight="bold" color="accent">
+							{titles.main}
+						</Typography>
+					</Card.Title>
+					<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-1">
+						{items.map((item, index) => (
+							<div key={index} className="flex flex-row items-start space-x-2">
+								<Typography size="3xl" weight="bold">
+									{item}
+								</Typography>
+							</div>
+						))}
+					<Image className="h-auto max-w-xs" src={sdgLogo} alt="" />
+					</div>
+				</Card.Body>
+			</Card>
+
+			<Modal ref={ref} backdrop className="w-11/12 max-w-3xl">
+				<Modal.Header>
+					<Typography as="h2" size="2xl" weight="bold">
+						{titles.main}
+					</Typography>
+				</Modal.Header>
+				<Modal.Body className="flex flex-col space-y-8">
+					{paragraphs.map((paragraph, index) => (
+						<Typography key={index} as="p" size="lg">
+							{paragraph}
+						</Typography>
+					))}
+					{/* {articles.length > 0 && (
+						<div>
+							<Typography size="xl" weight="medium">
+								{titles.articles}
+							</Typography>
+							<div className="grid grid-cols-1 divide-y">
+								{articles.map((article, index) => (
+									<div key={index} className="py-4">
+										<Typography>{article.author}</Typography>
+										<Link href={article.link} target="_blank">
+											<Typography color="secondary">{article.title}</Typography>
+										</Link>
+									</div>
+								))}
+							</div>
+						</div>
+					)} */}
+				</Modal.Body>
+			</Modal>
+		</>
+	);
+}
---
+++ website/src/app/[lang]/[country]/(website)/(home)/section-6.tsx
@@ -0,0 +1,68 @@
+import { DefaultPageProps } from '@/app/[lang]/[country]';
+import { Translator } from '@socialincome/shared/src/utils/i18n';
+import { BaseContainer, Typography } from '@socialincome/ui';
+import { SectionCardSix } from './section-6-card';
+
+export default async function Section6({ params }: DefaultPageProps) {
+	const translator = await Translator.getInstance({
+		language: params.lang,
+		namespaces: ['website-home'],
+	});
+
+	return (
+		<BaseContainer className="bg-base-red">
+			<div className="flex min-h-screen flex-col  justify-center py-20">
+				<p className="mb-8 lg:mb-16">
+					<Typography as="span" size="4xl" weight="bold" lineHeight="relaxed">
+						{translator.t('section-6.title-1')}
+					</Typography>
+					<Typography as="span" size="4xl" weight="bold" lineHeight="relaxed" color="accent">
+						{translator.t('section-6.title-2')}
+					</Typography>
+				</p>
+				<div className="grid grid-rows-5s-1 gap-4 md:grid-rows-2 lg:grid-rows-3">
+					<SectionCardSix
+						titles={{
+							main: translator.t('section-6.card-1.title'),
+							articles: translator.t('section-6.articles'),
+							faqs: translator.t('section-6.faqs'),
+						}}
+						items={[
+							translator.t('section-6.card-1.item-1')
+						]}
+						paragraphs={[translator.t('section-5.card-2.paragraph-1')]}
+						articles={[
+							{
+								title: translator.t('section-5.card-1.article-1-title'),
+								author: translator.t('section-5.card-1.article-1-author'),
+								link: translator.t('section-5.card-1.article-1-link'),
+							}
+						]}
+
+						
+					/>
+
+					<SectionCardSix
+						titles={{
+							main: translator.t('section-6.card-2.title'),
+							articles: translator.t('section-6.articles'),
+							faqs: translator.t('section-6.faqs'),
+						}}
+						items={[
+							translator.t('section-6.card-2.item-1'),
+						]}
+						paragraphs={[translator.t('section-6.card-2.paragraph-1')]}
+						articles={[
+							{
+								title: translator.t('section-6.card-2.article-1-title'),
+								author: translator.t('section-6.card-2.article-1-author'),
+								link: translator.t('section-6.card-2.article-1-link'),
+							}
+						]}
+					/>
+				</div>
+	
+			</div>
+		</BaseContainer>
+	);
+}
