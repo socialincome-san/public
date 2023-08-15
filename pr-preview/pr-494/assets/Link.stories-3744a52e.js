f40d05828a073bef439fdb2476821d1cdaf78956
mkue <mkue@users.noreply.github.com>
2023-08-15 20:01:31 +0000

    Deploy preview for PR 494 🛫

--- pr-preview/pr-494/assets/Link.stories-3744a52e.js
+++
@@ -1,8 +0,0 @@
-import{j as p}from"./jsx-runtime-de33e161.js";import{L as o}from"./index-1cb74ddc.js";import"./index-37ba2b57.js";import"./_commonjsHelpers-de833af9.js";import"./clsx.m-1229b3e0.js";import"./tw-merge-1166cefb.js";const f={title:"Navigation/Link",component:o},r=t=>p(o,{...t,children:"It's just a simple link"});r.args={};const e=t=>p(o,{...t,href:"https://google.com",target:"_blank",children:"Take me to Google!"});e.args={};var s,a,n;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`args => {
-  return <Link {...args}>It's just a simple link</Link>;
-}`,...(n=(a=r.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};var i,m,c;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`args => {
-  return <Link {...args} href="https://google.com" target="_blank">
-      Take me to Google!
-    </Link>;
-}`,...(c=(m=e.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const h=["Default","LinkWithHref"];export{r as Default,e as LinkWithHref,h as __namedExportsOrder,f as default};
-//# sourceMappingURL=Link.stories-3744a52e.js.map
---
+++ pr-preview/pr-494/assets/syntaxhighlighter-2MZ4Y3HO-e1f4b277.js
@@ -0,0 +1,2 @@
+import{S as d,c as f,s as g}from"./chunk-S4VUQJ4A-28870d83.js";import"./iframe-b62d35bb.js";import"../sb-preview/runtime.js";import"./index-37ba2b57.js";import"./_commonjsHelpers-de833af9.js";import"./index-1718beda.js";import"./assert-a1982797.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{d as SyntaxHighlighter,f as createCopyToClipboardFunction,g as default};
+//# sourceMappingURL=syntaxhighlighter-2MZ4Y3HO-e1f4b277.js.map
