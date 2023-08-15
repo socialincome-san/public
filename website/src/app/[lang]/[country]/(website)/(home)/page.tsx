da41bf8dea7eead497135aae1c30c86494f49aa7
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-08-15 21:02:46 +0100

    feature(website) section-6 on home page completed (#494)

--- website/src/app/[lang]/[country]/(website)/(home)/page.tsx
+++ website/src/app/[lang]/[country]/(website)/(home)/page.tsx
@@ -4,6 +4,7 @@ import Section2 from './section-2';
 import Section3 from './section-3';
 import Section4 from './section-4';
 import Section5 from './section-5';
+import Section6 from './section-6';
 
 export default async function Page(props: DefaultPageProps) {
 	return (
@@ -13,6 +14,7 @@ export default async function Page(props: DefaultPageProps) {
 			<Section3 {...props} />
 			<Section4 {...props} />
 			<Section5 {...props} />
+			<Section6 {...props} />
 		</>
 	);
 }
