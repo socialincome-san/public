2ae18430b9e9fbeab8d839d073e1378be526856e
Patrick McClurg <patrick.mcclurg@protocol.ai>
2023-09-12 11:39:46 +0200

    combined financial and selection transparency into a single navbar item

--- website/src/app/[lang]/[country]/(website)/selection/page.tsx
+++ website/src/app/[lang]/[country]/(website)/selection/page.tsx
@@ -1,82 +1,90 @@
-import * as React from "react"
-import {BaseContainer, Typography} from "@socialincome/ui"
-import {Translator} from "@socialincome/shared/src/utils/i18n"
-import {DefaultPageProps} from "@/app/[lang]/[country]"
-import {Collapsible, DrawDetail, DrawSummary} from "@/app/[lang]/[country]/(website)/selection/collapsible"
-import {futureDraws, pastDraws} from "@/app/[lang]/[country]/(website)/selection/state"
+import { DefaultPageProps } from '@/app/[lang]/[country]';
+import { Collapsible, DrawDetail, DrawSummary } from '@/app/[lang]/[country]/(website)/selection/collapsible';
+import { futureDraws, pastDraws } from '@/app/[lang]/[country]/(website)/selection/state';
+import { Translator } from '@socialincome/shared/src/utils/i18n';
+import { BaseContainer, Typography } from '@socialincome/ui';
 
 export default async function Page(props: DefaultPageProps) {
-    const translator = await Translator.getInstance({language: props.params.lang, namespaces: 'website-selection'});
+	const translator = await Translator.getInstance({ language: props.params.lang, namespaces: 'website-selection' });
 
-    return (
-        <BaseContainer className="bg-base-blue min-h-screen">
-            <IntroText translations={{
-                readDraws: translator.t("read-draws"),
-                readDraws2: translator.t("read-draws-2"),
-                readDrawsLink: translator.t("read-draws-link"),
-            }}/>
+	return (
+		<BaseContainer className="bg-base-blue min-h-screen">
+			<IntroText
+				translations={{
+					readDraws: translator.t('read-draws'),
+					readDraws2: translator.t('read-draws-2'),
+					readDrawsLink: translator.t('read-draws-link'),
+				}}
+			/>
 
-            <Typography as="h2" size="3xl">{translator.t("upcoming")}</Typography>
-            <Typography>{translator.t("future-draws")}</Typography>
-            {futureDraws.map(draw =>
-                <Collapsible
-                    key={draw.time}
-                    summary={
-                        <DrawSummary
-                            draw={draw}
-                            translations={{
-                                from: translator.t("from")
-                            }}
-                        />
-                    }
-                />)
-            }
+			<Typography as="h2" size="3xl">
+				{translator.t('upcoming')}
+			</Typography>
+			<Typography>{translator.t('future-draws')}</Typography>
+			{futureDraws.map((draw) => (
+				<Collapsible
+					key={draw.time}
+					summary={
+						<DrawSummary
+							draw={draw}
+							translations={{
+								from: translator.t('from'),
+							}}
+						/>
+					}
+				/>
+			))}
 
-            <Typography as="h2" size="3xl">{translator.t("past")}</Typography>
-            {pastDraws.map(draw =>
-                <Collapsible
-                    key={draw.time}
-                    summary={
-                        <DrawSummary
-                            draw={draw}
-                            translations={{
-                                from: translator.t("from")
-                            }}
-                        />
-                    }
-                    detail={
-                        <DrawDetail
-                            draw={draw}
-                            translations={{
-                                randomNumber: translator.t("random-number"),
-                                confirmGithub: translator.t("confirm-github"),
-                                confirmDrand: translator.t("confirm-drand"),
-                                people: translator.t("people"),
-                                longlist: translator.t("longlist", {context: {total: draw.total, count: draw.count}})
-                            }}
-                        />
-                    }
-                />)
-            }
-        </BaseContainer>
-    )
+			<Typography as="h2" size="3xl">
+				{translator.t('past')}
+			</Typography>
+			{pastDraws.map((draw) => (
+				<Collapsible
+					key={draw.time}
+					summary={
+						<DrawSummary
+							draw={draw}
+							translations={{
+								from: translator.t('from'),
+							}}
+						/>
+					}
+					detail={
+						<DrawDetail
+							draw={draw}
+							translations={{
+								randomNumber: translator.t('random-number'),
+								confirmGithub: translator.t('confirm-github'),
+								confirmDrand: translator.t('confirm-drand'),
+								people: translator.t('people'),
+								longlist: translator.t('longlist', { context: { total: draw.total, count: draw.count } }),
+							}}
+						/>
+					}
+				/>
+			))}
+		</BaseContainer>
+	);
 }
 
 type IntroTextProps = {
-    translations: {
-        readDraws: string
-        readDraws2: string
-        readDrawsLink: string
-    }
-}
-function IntroText({translations}: IntroTextProps) {
-    return (
-        <div className="p-2">
-            <Typography size="xl">
-                {translations.readDraws}
-                <a className="underline" href="https://some-blog-post.com">{translations.readDrawsLink}</a>
-                {translations.readDraws2}
-            </Typography>
-        </div>
-    )
+	translations: {
+		readDraws: string;
+		readDraws2: string;
+		readDrawsLink: string;
+	};
+};
+function IntroText({ translations }: IntroTextProps) {
+	return (
+		<div className="p-2">
+			<Typography size="xl">
+				{translations.readDraws}&nbsp;
+				<a className="underline" href="https://some-blog-post.com">
+					{translations.readDrawsLink}
+				</a>
+				&nbsp;
+				{translations.readDraws2}
+			</Typography>
+		</div>
+	);
 }
--- website/src/app/[lang]/[country]/(website)/selection/state.ts
+++ website/src/app/[lang]/[country]/(website)/selection/state.ts
@@ -1,41 +1,46 @@
 export type FutureDraw = {
-    time: number
-    name: string
-    count: number
-    total: number
-}
+	time: number;
+	name: string;
+	count: number;
+	total: number;
+};
 
 export type PastDraw = {
-    time: number
-    name: string
-    count: number
-    total: number
-    drandRound: number
-    drandRandomness: string
-    commitHash: string
-}
+	time: number;
+	name: string;
+	count: number;
+	total: number;
+	drandRound: number;
+	drandRandomness: string;
+	commitHash: string;
+};
 
-export const futureDraws: Array<FutureDraw> = [{
-    time: new Date(2023, 9, 15, 12).getUTCMilliseconds(),
-    name: "Aurora Draw",
-    count: 10,
-    total: 400
-}]
+export const futureDraws: Array<FutureDraw> = [
+	{
+		time: new Date(2023, 9, 15, 12).getUTCMilliseconds(),
+		name: 'Aurora Draw',
+		count: 10,
+		total: 400,
+	},
+];
 
-export const pastDraws: Array<PastDraw> = [{
-    time: Date.now(),
-    name: "some great draw",
-    count: 10,
-    total: 400,
-    drandRandomness: "deadbeefdeadbeefdeadbeefdeadbeef",
-    drandRound: 5,
-    commitHash: "abc1234"
-}, {
-    time: Date.now() - (1000 * 60 * 60 * 24),
-    name: "great draw1234",
-    count: 8,
-    total: 200,
-    drandRound: 5,
-    drandRandomness: "deadbeefdeadbeefdeadbeefdeadbeef",
-    commitHash: "abc1234"
-}]
+export const pastDraws: Array<PastDraw> = [
+	{
+		time: Date.now(),
+		name: 'some great draw',
+		count: 10,
+		total: 400,
+		drandRandomness: 'deadbeefdeadbeefdeadbeefdeadbeef',
+		drandRound: 5,
+		commitHash: 'abc1234',
+	},
+	{
+		time: Date.now() - 1000 * 60 * 60 * 24,
+		name: 'great draw1234',
+		count: 8,
+		total: 200,
+		drandRound: 5,
+		drandRandomness: 'deadbeefdeadbeefdeadbeefdeadbeef',
+		commitHash: 'abc1234',
+	},
+];
