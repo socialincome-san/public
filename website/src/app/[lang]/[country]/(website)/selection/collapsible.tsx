40ccd342a07b5c9091612a54d5a8e48d4b3b9e45
Patrick McClurg <patrick.mcclurg@protocol.ai>
2023-09-12 11:29:03 +0200

    moved translations into server component
    added the actual next upcoming draw

---
+++ website/src/app/[lang]/[country]/(website)/selection/collapsible.tsx
@@ -0,0 +1,105 @@
+'use client';
+import * as React from "react"
+import {Disclosure} from "@headlessui/react"
+import {ChevronDownIcon} from "@heroicons/react/20/solid"
+import {Translator} from "@socialincome/shared/src/utils/i18n"
+import {Typography} from "@socialincome/ui"
+import {FutureDraw, PastDraw} from "@/app/[lang]/[country]/(website)/selection/state"
+
+export interface CollapsibleProps extends React.PropsWithChildren {
+    summary: React.ReactNode
+    detail?: React.ReactNode
+}
+
+// a collapsible element rendering the summary, and optionally a button to open the detailed view
+export function Collapsible(props: CollapsibleProps) {
+    return (
+        <Disclosure>
+            <div className="bg-gray-200 shadow-xl p-2 m-4">
+                <div className="h-20 flex flex-row w-full h-full items-center">
+                    <div className="grow">
+                        {props.summary}
+                    </div>
+                    <div>
+                        {!!props.detail
+                            ? <Disclosure.Button>
+                                <ChevronDownIcon height="2em"></ChevronDownIcon>
+                            </Disclosure.Button>
+                            /* this is a filthy hack for spacing, but is actually responsive by some sorcery */
+                            : <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                        }
+                    </div>
+                </div>
+                {!!props.detail && <Disclosure.Panel>{props.detail}</Disclosure.Panel>}
+            </div>
+        </Disclosure>
+    )
+}
+
+export type DrawSummaryProps = {
+    translations: {
+        from: string
+    }
+    draw: PastDraw | FutureDraw,
+    translator?: Translator
+}
+
+export function DrawSummary({draw, translations}: DrawSummaryProps) {
+    return (
+        <div className="flex flex-row w-full h-full items-center">
+            <div className="basis-1/4">
+                {new Date(draw.time).toLocaleDateString()}
+            </div>
+            <div className="grow">
+                {draw.name}
+            </div>
+            <div className="basis-1/4">
+                {draw.count} {translations.from} {draw.total}
+            </div>
+        </div>
+    )
+}
+
+export type DrawDetailProps = {
+    translations: {
+        randomNumber: string,
+        longlist: string,
+        people: string,
+        confirmDrand: string,
+        confirmGithub: string
+    }
+    draw: PastDraw,
+}
+
+export function DrawDetail({draw, translations}: DrawDetailProps) {
+    return (
+        <div>
+            <div className="flex p-2 justify-between">
+                <div>
+                    <Typography weight="bold">{translations.randomNumber}:</Typography>
+                    <Typography>{draw.drandRandomness}</Typography>
+                </div>
+                <div>
+                    <a
+                        className="underline"
+                        href={`https://api.drand.sh/public/${draw.drandRound}`}
+                    >{translations.confirmDrand}</a>
+                </div>
+            </div>
+            <div className="flex p-2 justify-between">
+                <div>
+                    <Typography weight="bold">{translations.people}:</Typography>
+                    <Typography>{translations.longlist}</Typography>
+                </div>
+                <div>
+                    <a
+                        className="underline"
+                        href={`https://github.com/socialincome-san/public/tree/main/lists${draw.name}`}
+                    >
+                        {translations.confirmGithub}
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
--- website/src/app/[lang]/[country]/(website)/selection/page.tsx
+++ website/src/app/[lang]/[country]/(website)/selection/page.tsx
@@ -1,196 +1,82 @@
-'use client';
 import * as React from "react"
 import {BaseContainer, Typography} from "@socialincome/ui"
 import {Translator} from "@socialincome/shared/src/utils/i18n"
 import {DefaultPageProps} from "@/app/[lang]/[country]"
-import {LoaderIcon} from "react-hot-toast"
-import {ChevronDownIcon} from "@heroicons/react/20/solid"
-import {useEffect, useState} from "react"
-import {Disclosure} from "@headlessui/react"
+import {Collapsible, DrawDetail, DrawSummary} from "@/app/[lang]/[country]/(website)/selection/collapsible"
+import {futureDraws, pastDraws} from "@/app/[lang]/[country]/(website)/selection/state"
 
-export default function Page(props: DefaultPageProps) {
-    const [translator, setTranslator] = useState<Translator>();
-
-    useEffect(() => {
-        Translator.getInstance({
-            language: props.params.lang,
-            namespaces: ['website-selection'],
-        }).then(t => setTranslator(t));
-    }, [props.params.lang])
-
-    if (!translator) {
-        return <LoaderIcon></LoaderIcon>
-    }
+export default async function Page(props: DefaultPageProps) {
+    const translator = await Translator.getInstance({language: props.params.lang, namespaces: 'website-selection'});
 
     return (
         <BaseContainer className="bg-base-blue min-h-screen">
-            <div className="p-2">
-                <Typography size="xl">
-                    {translator?.t("read-draws")}
-                    <a className="underline" href="https://some-blog-post.com">{translator?.t("read-draws-link")}</a>
-                    {translator?.t("read-draws-2")}
-                </Typography>
-            </div>
-            <Typography as="h2" size="3xl">{translator?.t("upcoming")}</Typography>
-            <Typography>{translator?.t("future-draws")}</Typography>
+            <IntroText translations={{
+                readDraws: translator.t("read-draws"),
+                readDraws2: translator.t("read-draws-2"),
+                readDrawsLink: translator.t("read-draws-link"),
+            }}/>
 
+            <Typography as="h2" size="3xl">{translator.t("upcoming")}</Typography>
+            <Typography>{translator.t("future-draws")}</Typography>
             {futureDraws.map(draw =>
                 <Collapsible
                     key={draw.time}
-                    summary={<DrawSummary draw={draw}/>}
+                    summary={
+                        <DrawSummary
+                            draw={draw}
+                            translations={{
+                                from: translator.t("from")
+                            }}
+                        />
+                    }
                 />)
             }
 
-            <Typography as="h2" size="3xl">{translator?.t("past")}</Typography>
+            <Typography as="h2" size="3xl">{translator.t("past")}</Typography>
             {pastDraws.map(draw =>
                 <Collapsible
                     key={draw.time}
-                    summary={<DrawSummary draw={draw}/>}
-                    detail={<DrawDetail draw={draw}/>}
+                    summary={
+                        <DrawSummary
+                            draw={draw}
+                            translations={{
+                                from: translator.t("from")
+                            }}
+                        />
+                    }
+                    detail={
+                        <DrawDetail
+                            draw={draw}
+                            translations={{
+                                randomNumber: translator.t("random-number"),
+                                confirmGithub: translator.t("confirm-github"),
+                                confirmDrand: translator.t("confirm-drand"),
+                                people: translator.t("people"),
+                                longlist: translator.t("longlist", {context: {total: draw.total, count: draw.count}})
+                            }}
+                        />
+                    }
                 />)
             }
         </BaseContainer>
     )
 }
 
-interface CollapsibleProps extends React.PropsWithChildren {
-    summary: React.ReactNode
-    detail?: React.ReactNode
+type IntroTextProps = {
+    translations: {
+        readDraws: string
+        readDraws2: string
+        readDrawsLink: string
+    }
 }
-
-// a collapsible element rendering the summary, and optionally a button to open the detailed view
-function Collapsible(props: CollapsibleProps) {
+function IntroText({translations}: IntroTextProps) {
     return (
-        <Disclosure>
-            <div className="bg-gray-200 shadow-xl p-2 m-4">
-                <div className="h-20 flex flex-row w-full h-full items-center">
-                    <div className="grow">
-                        {props.summary}
-                    </div>
-                    <div>
-                        {!!props.detail
-                            ? <Disclosure.Button>
-                                <ChevronDownIcon height="2em"></ChevronDownIcon>
-                            </Disclosure.Button>
-                            /* this is a filthy hack for spacing, but is actually responsive by some sorcery */
-                            : <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                        }
-                    </div>
-                </div>
-                {!!props.detail && <Disclosure.Panel>{props.detail}</Disclosure.Panel>}
-            </div>
-        </Disclosure>
-    )
-}
-
-type DrawSummaryProps = {
-    draw: PastDraw | FutureDraw,
-    translator?: Translator
-
-}
-
-function DrawSummary({draw, translator}: DrawSummaryProps) {
-    return (
-        <div className="flex flex-row w-full h-full items-center">
-            <div className="basis-1/4">
-                {new Date(draw.time).toLocaleDateString()}
-            </div>
-            <div className="grow">
-                {draw.name}
-            </div>
-            <div className="basis-1/4">
-                {draw.count} {translator?.t("from")} {draw.total}
-            </div>
+        <div className="p-2">
+            <Typography size="xl">
+                {translations.readDraws}
+                <a className="underline" href="https://some-blog-post.com">{translations.readDrawsLink}</a>
+                {translations.readDraws2}
+            </Typography>
         </div>
     )
 }
-
-type DrawDetailProps = {
-    draw: PastDraw,
-    translator?: Translator
-}
-
-function DrawDetail({draw, translator}: DrawDetailProps) {
-    return (
-        <div>
-            <div className="flex p-2 justify-between">
-                <div>
-                    <Typography weight="bold">{translator?.t("random-number")}:</Typography>
-                    <Typography>{draw.drandRandomness}</Typography>
-                </div>
-                <div>
-                    <a
-                        className="underline"
-                        href={`https://api.drand.sh/public/${draw.drandRound}`}
-                    >{translator?.t("confirm-drand")}</a>
-                </div>
-            </div>
-            <div className="flex p-2 justify-between">
-                <div>
-                    <Typography weight="bold">People:</Typography>
-                    <Typography>{translator?.t("longlist", {
-                        context: {
-                            total: draw.total,
-                            count: draw.count
-                        }
-                    })}</Typography>
-                </div>
-                <div>
-                    <a
-                        className="underline"
-                        href={`https://github.com/socialincome-san/public/tree/main/lists${draw.name}`}
-                    >
-                        {translator?.t("confirm-github")}
-                    </a>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-type FutureDraw = {
-    time: number
-    name: string
-    count: number
-    total: number
-}
-
-const futureDraws: Array<FutureDraw> = [{
-    time: Date.now() + (1000 * 60 * 60 * 24),
-    name: "some great future draw",
-    count: 5,
-    total: 500
-}, {
-    time: Date.now() + (1000 * 60 * 60 * 24 * 2),
-    name: "some other draw",
-    count: 2,
-    total: 500
-}]
-
-type PastDraw = {
-    time: number
-    name: string
-    count: number
-    total: number
-    drandRound: number
-    drandRandomness: string
-    commitHash: string
-}
-
-const pastDraws: Array<PastDraw> = [{
-    time: Date.now(),
-    name: "some great draw",
-    count: 10,
-    total: 400,
-    drandRandomness: "deadbeefdeadbeefdeadbeefdeadbeef",
-    drandRound: 5,
-    commitHash: "abc1234"
-}, {
-    time: Date.now() - (1000 * 60 * 60 * 24),
-    name: "great draw1234",
-    count: 8,
-    total: 200,
-    drandRound: 5,
-    drandRandomness: "deadbeefdeadbeefdeadbeefdeadbeef",
-    commitHash: "abc1234"
-}]
