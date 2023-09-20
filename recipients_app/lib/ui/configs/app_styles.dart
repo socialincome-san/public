e5b3da1547a36df9d945a13815ae1f38c3eefb16
Mikolaj <mdemkow@gmail.com>
2023-09-20 20:15:11 +0200

    Add fallback font family

--- recipients_app/lib/ui/configs/app_styles.dart
+++ recipients_app/lib/ui/configs/app_styles.dart
@@ -7,56 +7,66 @@ abstract class AppStyles {
   static const bodySmall = TextStyle(
     fontSize: 11,
     fontFamily: "Unica77LL",
+    fontFamilyFallback: ["sans-serif"],
   );
 
   static const bodyMedium = TextStyle(
     fontSize: 13,
     fontFamily: "Unica77LL",
+    fontFamilyFallback: ["sans-serif"],
   );
 
   static const bodyLarge = TextStyle(
     fontSize: 15,
     fontFamily: "Unica77LL",
     color: Colors.black,
+    fontFamilyFallback: ["sans-serif"],
   );
 
   static const headlineSmall = TextStyle(
     fontSize: 16,
     fontFamily: "Unica77LL",
+    fontFamilyFallback: ["sans-serif"],
   );
 
   static const headlineMedium = TextStyle(
     fontSize: 18,
     fontFamily: "Unica77LL",
+    fontFamilyFallback: ["sans-serif"],
   );
 
   static const headlineLarge = TextStyle(
     fontSize: 24,
     fontFamily: "Unica77LL",
+    fontFamilyFallback: ["sans-serif"],
   );
 
   static const iconLabel = TextStyle(
     fontSize: 13,
     fontFamily: "Unica77LL",
     color: Colors.white,
+    fontFamilyFallback: ["sans-serif"],
   );
 
   static const buttonLabelBig = TextStyle(
     fontSize: 24,
     fontFamily: "Unica77LL",
     color: Colors.white,
+    fontFamilyFallback: ["sans-serif"],
   );
 
   static const buttonLabelSmall = TextStyle(
     fontSize: 13,
     fontFamily: "Unica77LL",
     color: AppColors.fontColorDark,
+    fontFamilyFallback: ["sans-serif"],
   );
 
   static const inputHint = TextStyle(
     fontSize: 15,
     fontFamily: "Unica77LL",
     color: Colors.black,
+    fontFamilyFallback: ["sans-serif"],
   );
 
   static const inputText = TextStyle(
@@ -64,5 +74,6 @@ abstract class AppStyles {
     fontFamily: "Unica77LL",
     color: AppColors.primaryColor,
     fontWeight: FontWeight.bold,
+    fontFamilyFallback: ["sans-serif"],
   );
 }
