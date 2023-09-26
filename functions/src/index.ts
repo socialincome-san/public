a168e8e0bb5f5296e5a249130a04afdfc535bea0
Michael Kündig <michael@socialincome.org>
2023-09-26 11:55:33 +0200

    feature(functions): set maxInstances in setGlobalOptions (#575)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,7 @@
+import { setGlobalOptions } from 'firebase-functions/v2/options';
+
+setGlobalOptions({ maxInstances: 10 });
+
 export * from './cron/index';
 export * from './firestore/index';
 export * from './storage/index';
