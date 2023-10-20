56e65aaf96fbb7ce57f7a9c220640df3604ee2e8
mkue <michael@socialincome.org>
2023-10-20 16:47:36 +0200

    undo unwanted changes

--- website/src/app/[lang]/[country]/(website)/me/contributions/page.tsx
+++ website/src/app/[lang]/[country]/(website)/me/contributions/page.tsx
@@ -10,7 +10,7 @@ export default async function Page({ params }: DefaultPageProps) {
 		<div className="grid grid-cols-1 gap-4 md:grid-cols-2">
 			<div className="flex max-w-lg flex-col space-y-4">
 				<ContributionsTable
-					translations={{ title: translator.t('contributions.date'), amount: translator.t('contributions.amount') }}
+					translations={{ date: translator.t('contributions.date'), amount: translator.t('contributions.amount') }}
 					{...params}
 				/>
 			</div>
