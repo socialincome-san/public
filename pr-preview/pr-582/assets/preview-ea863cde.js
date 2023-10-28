1b16ff81a11716c713a6897163af07362bfa6b9a
MDikkii <MDikkii@users.noreply.github.com>
2023-10-28 19:48:08 +0000

    Deploy preview for PR 582 🛫

---
+++ pr-preview/pr-582/assets/preview-ea863cde.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-4ebd3998.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-5713353a.js"),["assets/DocsRenderer-NNNQARDV-5713353a.js","assets/chunk-HLWAVYOI-9c3a2460.js","assets/iframe-4ebd3998.js","assets/index-c013ead5.js","assets/_commonjsHelpers-725317a4.js","assets/index-b06778de.js","assets/index-d37d4223.js","assets/index-356e4a49.js"]);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-ea863cde.js.map
