ded2d5df7db5e5720299e18457578ceb569f8dce
Mikołaj Demków <mdemkow@gmail.com>
2023-11-13 23:17:18 +0100

    Fixing nullable fields and converters (#624)
    * Fixing nullable fields and converters
    
    * Bump version
    
    * Fix version

--- recipients_app/lib/core/helpers/timestamp_converter.dart
+++ recipients_app/lib/core/helpers/timestamp_converter.dart
@@ -1,16 +1,16 @@
 import "package:cloud_firestore/cloud_firestore.dart";
 import "package:json_annotation/json_annotation.dart";
 
-class TimestampConverter implements JsonConverter<Timestamp?, dynamic> {
+class TimestampConverter implements JsonConverter<Timestamp, Object> {
   const TimestampConverter();
 
   @override
-  Timestamp? fromJson(dynamic json) {
-    return json as Timestamp?;
+  Timestamp fromJson(dynamic json) {
+    return json as Timestamp;
   }
 
   @override
-  dynamic toJson(Timestamp? object) {
+  Object toJson(Timestamp object) {
     return object;
   }
 }
--- recipients_app/lib/data/models/survey/survey.g.dart
+++ recipients_app/lib/data/models/survey/survey.g.dart
@@ -9,8 +9,10 @@ part of 'survey.dart';
 Survey _$SurveyFromJson(Map<String, dynamic> json) => Survey(
       id: json['id'] as String? ?? '',
       status: $enumDecodeNullable(_$SurveyServerStatusEnumMap, json['status']),
-      dueDateAt: const TimestampConverter().fromJson(json['due_date_at']),
-      completedAt: const TimestampConverter().fromJson(json['completed_at']),
+      dueDateAt: _$JsonConverterFromJson<Object, Timestamp>(
+          json['due_date_at'], const TimestampConverter().fromJson),
+      completedAt: _$JsonConverterFromJson<Object, Timestamp>(
+          json['completed_at'], const TimestampConverter().fromJson),
       accessEmail: json['access_email'] as String?,
       accessPassword: json['access_pw'] as String?,
     );
@@ -18,8 +20,10 @@ Survey _$SurveyFromJson(Map<String, dynamic> json) => Survey(
 Map<String, dynamic> _$SurveyToJson(Survey instance) => <String, dynamic>{
       'id': instance.id,
       'status': _$SurveyServerStatusEnumMap[instance.status],
-      'due_date_at': const TimestampConverter().toJson(instance.dueDateAt),
-      'completed_at': const TimestampConverter().toJson(instance.completedAt),
+      'due_date_at': _$JsonConverterToJson<Object, Timestamp>(
+          instance.dueDateAt, const TimestampConverter().toJson),
+      'completed_at': _$JsonConverterToJson<Object, Timestamp>(
+          instance.completedAt, const TimestampConverter().toJson),
       'access_email': instance.accessEmail,
       'access_pw': instance.accessPassword,
     };
@@ -32,3 +36,15 @@ const _$SurveyServerStatusEnumMap = {
   SurveyServerStatus.completed: 'completed',
   SurveyServerStatus.missed: 'missed',
 };
+
+Value? _$JsonConverterFromJson<Json, Value>(
+  Object? json,
+  Value? Function(Json json) fromJson,
+) =>
+    json == null ? null : fromJson(json as Json);
+
+Json? _$JsonConverterToJson<Json, Value>(
+  Value? value,
+  Json? Function(Value value) toJson,
+) =>
+    value == null ? null : toJson(value);
