9dc7fd30c0a2fc44e6f92b5e3b73d6711fc8e76c
mkue <mkue@users.noreply.github.com>
2023-11-24 19:30:51 +0000

    Deploy preview for PR 647 🛫

---
+++ pr-preview/pr-647/assets/preview-c7c59e7e.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-505c487a.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-f087e262.js"),["assets/DocsRenderer-NNNQARDV-f087e262.js","assets/chunk-HLWAVYOI-fd1a46f0.js","assets/iframe-505c487a.js","assets/index-c013ead5.js","assets/_commonjsHelpers-725317a4.js","assets/index-b06778de.js","assets/index-d37d4223.js","assets/index-356e4a49.js"]);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-c7c59e7e.js.map
