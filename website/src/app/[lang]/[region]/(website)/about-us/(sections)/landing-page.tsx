b37d893d30755ef811edb0e1e823d400391ab564
ssandino <ssandino@users.noreply.github.com>
2023-11-29 00:26:55 +0000

    Prettified Code!

--- website/src/app/[lang]/[region]/(website)/about-us/(sections)/landing-page.tsx
+++ website/src/app/[lang]/[region]/(website)/about-us/(sections)/landing-page.tsx
@@ -1,6 +1,6 @@
 import { WebsiteLanguage } from '@/i18n';
 import { Translator } from '@socialincome/shared/src/utils/i18n';
-import { BaseContainer, Typography, Badge, Popover, PopoverContent, PopoverTrigger, Button } from '@socialincome/ui';
+import { Badge, BaseContainer, Button, Popover, PopoverContent, PopoverTrigger, Typography } from '@socialincome/ui';
 import { FontColor } from '@socialincome/ui/src/interfaces/color';
 import Image from 'next/image';
 import changeGif from '../(assets)/change.gif';
@@ -13,35 +13,38 @@ export default async function LandingPage({ lang }: { lang: WebsiteLanguage }) {
 
 	return (
 		<BaseContainer className="min-h-screen-navbar grid grid-cols-1 content-center items-center gap-16 md:grid-cols-5">
-			<div className="mx-auto max-w-lg md:col-span-3 space-y-5">
+			<div className="mx-auto max-w-lg space-y-5 md:col-span-3">
 				{translator.t<{ text: string; color?: FontColor }[]>('landing-page.title').map((title, index) => (
 					<Typography as="span" key={index} size="5xl" weight="bold" lineHeight="tight" color={title.color}>
 						{title.text}
 					</Typography>
 				))}
-			<Typography size="2xl" lineHeight="normal">
-				{translator.t('landing-page.subtitle')}
-			</Typography>
-				<div className="pt-5 space-x-2 flex">
+				<Typography size="2xl" lineHeight="normal">
+					{translator.t('landing-page.subtitle')}
+				</Typography>
+				<div className="flex space-x-2 pt-5">
 					<div>
 						<Popover>
 							<PopoverTrigger>
-						<Badge variant="outline" className="hover:bg-muted-foreground text-muted-foreground hover:text-secondary-foreground transition-transform duration-300 hover:scale-105">
-							<Typography size="md" weight="normal"  className="p-1">
-								{translator.t('landing-page.contact')}
-						</Typography>
-						</Badge>
+								<Badge
+									variant="outline"
+									className="hover:bg-muted-foreground text-muted-foreground hover:text-secondary-foreground transition-transform duration-300 hover:scale-105"
+								>
+									<Typography size="md" weight="normal" className="p-1">
+										{translator.t('landing-page.contact')}
+									</Typography>
+								</Badge>
 							</PopoverTrigger>
 							<PopoverContent>
 								<div className="space-y-2">
-								<Typography size="md" lineHeight="normal" weight="medium">
-									{translator.t('landing-page.contact-title')}
-								</Typography>
+									<Typography size="md" lineHeight="normal" weight="medium">
+										{translator.t('landing-page.contact-title')}
+									</Typography>
 									<a href="mailto:hello@socialincome.org">
 										<Button variant="link" className="p-0">
 											<Typography size="md" lineHeight="normal" weight="normal">
-											{translator.t('landing-page.contact-email')}
-										</Typography>
+												{translator.t('landing-page.contact-email')}
+											</Typography>
 										</Button>
 									</a>
 								</div>
@@ -51,8 +54,11 @@ export default async function LandingPage({ lang }: { lang: WebsiteLanguage }) {
 					<div>
 						<Popover>
 							<PopoverTrigger>
-								<Badge variant="outline" className="hover:bg-muted-foreground text-muted-foreground hover:text-secondary-foreground transition-transform duration-300 hover:scale-105">
-									<Typography size="md" weight="normal"  className="p-1">
+								<Badge
+									variant="outline"
+									className="hover:bg-muted-foreground text-muted-foreground hover:text-secondary-foreground transition-transform duration-300 hover:scale-105"
+								>
+									<Typography size="md" weight="normal" className="p-1">
 										{translator.t('landing-page.registration')}
 									</Typography>
 								</Badge>
@@ -65,19 +71,19 @@ export default async function LandingPage({ lang }: { lang: WebsiteLanguage }) {
 									<Typography size="md" lineHeight="normal">
 										{translator.t('landing-page.registration-foundation')}
 									</Typography>
-									<div className="flex space-x-1 items-center">
+									<div className="flex items-center space-x-1">
 										<Typography size="md" lineHeight="normal">
 											{translator.t('landing-page.registration-uid')}
 										</Typography>
 										<a href="https://www.uid.admin.ch/Detail.aspx?uid_id=CHE-289.611.695&lang=en">
-											<Button variant="link" className="p-0 m-0">
+											<Button variant="link" className="m-0 p-0">
 												<Typography size="md" lineHeight="normal" weight="normal">
 													{translator.t('landing-page.registration-uid-nr')}
 												</Typography>
 											</Button>
 										</a>
 									</div>
-									<div className="flex space-x-1 items-center">
+									<div className="flex items-center space-x-1">
 										<Typography size="md" lineHeight="normal">
 											{translator.t('landing-page.registration-duns')}
 										</Typography>
@@ -88,7 +94,7 @@ export default async function LandingPage({ lang }: { lang: WebsiteLanguage }) {
 												</Typography>
 											</Button>
 										</a>
-								</div>
+									</div>
 								</div>
 							</PopoverContent>
 						</Popover>
