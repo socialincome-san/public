055de7bf6e72345637c67bbe17ed5fee55376d7e
mkue <michael@socialincome.org>
2023-11-29 12:46:17 +0100

    styling updates

--- website/src/app/[lang]/[region]/(website)/transparency/sustainability/page.tsx
+++ website/src/app/[lang]/[region]/(website)/transparency/sustainability/page.tsx
@@ -1,12 +1,43 @@
 import { DefaultPageProps } from '@/app/[lang]/[region]';
-import Section1 from './section-1';
-import Section2 from './section-2';
+import { SectionCard, TabTranslation } from '@/app/[lang]/[region]/(website)/transparency/sustainability/section-card';
+import { Translator } from '@socialincome/shared/src/utils/i18n';
+import { BaseContainer, Tabs, TabsContent, TabsList, TabsTrigger, Typography } from '@socialincome/ui';
 
-export default async function Page(props: DefaultPageProps) {
+export default async function Page({ params }: DefaultPageProps) {
+	const translator = await Translator.getInstance({
+		language: params.lang,
+		namespaces: ['website-sustainability'],
+	});
+	const tabs = translator.t<TabTranslation[]>(`section-2.tabs`);
+
 	return (
-		<>
-			<Section1 {...props} />
-			<Section2 {...props} />
-		</>
+		<BaseContainer className="mx-auto flex max-w-2xl flex-col	space-y-12 pb-16">
+			<div className="space-y-4">
+				<Typography as="h1" size="4xl" weight="bold">
+					{translator.t('section-1.title')}
+				</Typography>
+				<Typography as="h2" size="2xl" weight="medium">
+					{translator.t('section-1.subtitle')}
+				</Typography>
+			</div>
+			<Tabs defaultValue={tabs[0].trigger} className="theme-white mx-auto flex w-full flex-col rounded-lg">
+				<TabsList className="mb-10">
+					{tabs.map((tab, key) => {
+						return (
+							<TabsTrigger value={tab.trigger} key={key}>
+								{tab.trigger}
+							</TabsTrigger>
+						);
+					})}
+				</TabsList>
+				{tabs.map((tab, key) => {
+					return (
+						<TabsContent value={tab.trigger} key={key}>
+							<SectionCard tab={tab} />
+						</TabsContent>
+					);
+				})}
+			</Tabs>
+		</BaseContainer>
 	);
 }
--- website/src/app/[lang]/[region]/(website)/transparency/sustainability/section-card.tsx
+++ website/src/app/[lang]/[region]/(website)/transparency/sustainability/section-card.tsx
@@ -1,16 +1,17 @@
 'use client';
 
-import { Card, CardContent, Typography } from '@socialincome/ui';
+import { Button, Card, FontWeight, Typography } from '@socialincome/ui';
 import classNames from 'classnames';
 import Image from 'next/image';
+import Link from 'next/link';
 import MossEarth from './(assets)/moss_earth.png';
 
 type Card = {
 	type: string;
 	amount: string;
 	gas: string;
-	color?: string;
-	weight?: string;
+	weight?: FontWeight;
+	theme?: string;
 	href?: string;
 };
 
@@ -23,7 +24,7 @@ export type TabTranslation = {
 	href?: string;
 	cards?: Card[];
 	partner?: string;
-	partnerLink?: string;
+	partnerLink: string;
 };
 
 type SectionCardProps = {
@@ -32,60 +33,54 @@ type SectionCardProps = {
 
 export function SectionCard({ tab }: SectionCardProps) {
 	return tab.cards ? (
-		<div className="flex w-3/5 flex-col space-y-4 text-left">
+		<div className="flex flex-col space-y-4 px-4 py-2">
 			{tab.cards.map((card, key) => {
+				const cardContent = (
+					<>
+						<Typography as="span" size="lg" weight={card.weight}>
+							{card.type}:{' '}
+						</Typography>
+						<Typography as="span" size="lg" weight={card.weight}>
+							{card.amount}{' '}
+						</Typography>
+						<Typography as="span" size="lg" weight={card.weight}>
+							{card.gas}
+						</Typography>
+					</>
+				);
 				return (
-					<Card
-						key={key}
-						className={classNames('bg-inherit pt-4 duration-200 ease-in md:hover:scale-105', card.color, {
-							'text-white': card.href,
-						})}
-					>
-						<CardContent className="flex justify-between text-left">
-							<Typography as="span" size="lg" weight={card.weight}>
-								{card.type}
-							</Typography>
-							<Typography as="span" size="lg" weight={card.weight}>
-								{card.amount}
-							</Typography>
-							{card.href ? (
-								<a href={card.href}>
-									<u>
-										<Typography as="span" size="lg" weight={card.weight}>
-											{card.gas}
-										</Typography>
-									</u>
-								</a>
-							) : (
-								<Typography as="span" size="lg" weight={card.weight}>
-									{card.gas}
-								</Typography>
-							)}
-						</CardContent>
-					</Card>
+					<div key={key} className={classNames('rounded-lg border px-8 py-4', card.theme)}>
+						{card.href ? (
+							<Link href={card.href} target="_blank">
+								{cardContent}
+							</Link>
+						) : (
+							<>{cardContent}</>
+						)}
+					</div>
 				);
 			})}
-			<a href={tab.partnerLink} className="flex items-start justify-center space-x-2 pb-14 pt-10 text-blue-600">
-				<Typography as="span">{tab.partner}</Typography>
-				<Image src={MossEarth} alt="MossEarth" width={100} />
-			</a>
+			{tab.partnerLink && (
+				<Link href={tab.partnerLink} target="_blank" className="flex items-start justify-center space-x-2 pb-14 pt-10">
+					<Typography as="span">{tab.partner}</Typography>
+					<Image src={MossEarth} alt="MossEarth" width={100} />
+				</Link>
+			)}
 		</div>
 	) : (
-		<div className="flex w-3/5 flex-col space-y-4">
+		<div className="flex flex-col space-y-4 px-2 py-2 md:px-16">
 			<Typography size="xl" weight="medium" color="muted-foreground">
 				{tab.topic}
 			</Typography>
-			<Typography as="span" size="4xl" weight="bold">
+			<Typography weight="medium" size="2xl">
 				{tab.title}
 			</Typography>
-			<Typography size="2xl" weight="semibold" className="md:col-span-3">
-				{tab.subtitle}
-			</Typography>
-			<a href={tab.href}>
-				<Typography size="lg" className="text-blue-500">
-					{tab.text}
-				</Typography>
-			</a>
+			<Typography size="xl">{tab.subtitle}</Typography>
+			{tab.href && (
+				<Link href={tab.href} target="_blank" className="self-center">
+					<Button variant="link">{tab.text}</Button>
+				</Link>
+			)}
 		</div>
 	);
 }
