f9e5119f0dd161b4270e0c1fcbd76509634d1341
mkue <michael@kuendig.id>
2023-11-30 11:27:28 +0000

    Deploying to gh-pages from @ socialincome-san/public@0eee8180ed39f454acf110c2a029624450807c4a 🚀

--- assets/preview-8866d83f.js
+++ assets/preview-8866d83f.js
@@ -1,2 +1,2 @@
 /* empty css                */const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{expanded:!0,matchers:{color:/(background|color)$/i,date:/Date$/}}};export{o as parameters};
-//# sourceMappingURL=preview-2bce7723.js.map
+//# sourceMappingURL=preview-8866d83f.js.map
