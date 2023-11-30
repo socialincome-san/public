9c0215b09900b3af95a1f78d732db5092d049c63
mkue <michael@socialincome.org>
2023-11-30 14:10:54 +0100

    refactor(website): update transparency page

--- website/src/app/[lang]/[region]/(website)/transparency/finances/[currency]/section-3.tsx
+++ website/src/app/[lang]/[region]/(website)/transparency/finances/[currency]/section-3.tsx
@@ -1,3 +1,4 @@
+import { roundAmount } from '@/app/[lang]/[region]/(website)/transparency/finances/[currency]/section-1';
 import { Translator } from '@socialincome/shared/src/utils/i18n';
 import { Typography } from '@socialincome/ui';
 import { SectionProps } from './page';
@@ -19,7 +20,7 @@ export async function Section3({ params, contributionStats }: SectionProps) {
 			<Typography weight="bold" size="3xl">
 				{translator.t('section-3.title')}
 			</Typography>
-			<Typography weight="medium" size="xl" className="mb-8">
+			<Typography size="lg" className="mb-8">
 				{translator.t('section-3.subtitle', { context: { value: totalContributionsByCountry.length } })}
 			</Typography>
 			<CountryCardList buttonText={translator.t('section-3.all-countries')}>
@@ -32,7 +33,11 @@ export async function Section3({ params, contributionStats }: SectionProps) {
 							translations={{
 								country: translator.t(entry.country),
 								total: translator.t('section-3.country-amount', {
-									context: { value: entry.amount, currency: params.currency, contributorsCount: entry.usersCount },
+									context: {
+										value: roundAmount(entry.amount),
+										currency: params.currency,
+										contributorsCount: entry.usersCount,
+									},
 								}),
 							}}
 						/>
