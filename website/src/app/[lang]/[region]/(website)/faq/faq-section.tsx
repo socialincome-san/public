8394fb00def6bdbd8a70045049e2658494bed644
DarkMenacer <DarkMenacer@users.noreply.github.com>
2023-12-19 21:17:47 +0000

    Prettified Code!

--- website/src/app/[lang]/[region]/(website)/faq/faq-section.tsx
+++ website/src/app/[lang]/[region]/(website)/faq/faq-section.tsx
@@ -29,11 +29,13 @@ export function FAQSection({ title, questions }: FAQSectionProps) {
 							</Typography>
 						</AccordionTrigger>
 						<AccordionContent className="prose">
-							<Typography size="lg" className='text-left'>{question.answer}</Typography>
+							<Typography size="lg" className="text-left">
+								{question.answer}
+							</Typography>
 							{question.links && (
 								<ul className="mt-4 flex list-outside list-disc flex-col space-y-1 pl-3">
 									{question.links?.map((link, index2) => (
-										<li key={index2} className='pl-3 mb-0'>
+										<li key={index2} className="mb-0 pl-3">
 											<Link href={link.href} target="_blank" rel="noreferrer" className="no-underline">
 												<Typography as="span" size="lg" color="primary" className="font-normal hover:underline">
 													{link.title}
