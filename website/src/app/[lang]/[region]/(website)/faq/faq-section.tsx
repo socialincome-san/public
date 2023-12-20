e2171a80970f891a021d0caa0144102f2d92ef22
Sandino Scheidegger <sandino@socialincome.org>
2023-12-20 08:38:08 +0100

    feature(website): faq design update (#679)

--- website/src/app/[lang]/[region]/(website)/faq/faq-section.tsx
+++ website/src/app/[lang]/[region]/(website)/faq/faq-section.tsx
@@ -29,13 +29,15 @@ export function FAQSection({ title, questions }: FAQSectionProps) {
 							</Typography>
 						</AccordionTrigger>
 						<AccordionContent className="prose">
-							<Typography>{question.answer}</Typography>
+							<Typography size="lg" className="text-left">
+								{question.answer}
+							</Typography>
 							{question.links && (
-								<ul className="mt-4 flex list-inside list-disc flex-col space-y-1">
+								<ul className="mt-4 flex list-outside list-disc flex-col space-y-1 pl-3">
 									{question.links?.map((link, index2) => (
-										<li key={index2}>
-											<Link href={link.href} target="_blank" rel="noreferrer">
-												<Typography as="span" color="primary" className="hover:underline">
+										<li key={index2} className="mb-0 pl-3">
+											<Link href={link.href} target="_blank" rel="noreferrer" className="no-underline">
+												<Typography as="span" size="lg" color="primary" className="font-normal hover:underline">
 													{link.title}
 												</Typography>
 											</Link>
