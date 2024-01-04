d6377c0baf72e7687bb216d34b89b3ba2905aedf
mkue <michael@kuendig.id>
2024-01-04 06:27:02 +0000

    Deploying to gh-pages from @ socialincome-san/public@91dc333eef06f60820c0732464c598c3a890a5a3 🚀

---
+++ assets/preview-e83df1b7.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-4ca47c81.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-8af7ad29.js"),["assets/DocsRenderer-NNNQARDV-8af7ad29.js","assets/chunk-HLWAVYOI-36619765.js","assets/iframe-4ca47c81.js","assets/index-c013ead5.js","assets/_commonjsHelpers-725317a4.js","assets/index-b06778de.js","assets/index-d37d4223.js","assets/index-356e4a49.js"]);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-e83df1b7.js.map
