6a463d7b334fff79679a562ab20020d063d6d2bb
Michael Kündig <michael@socialincome.org>
2024-01-19 16:58:23 +0700

    bugfix(website): random subscriptions list entries for users without stripe_customer_id (#699)

--- website/src/app/api/stripe/billing-portal-session/create/route.ts
+++ website/src/app/api/stripe/billing-portal-session/create/route.ts
@@ -13,6 +13,9 @@ export async function POST(request: CreateBillingPortalSessionRequest) {
 	const { returnUrl, firebaseAuthToken } = await request.json();
 	const stripe = initializeStripe(process.env.STRIPE_SECRET_KEY!);
 	const userDoc = await getUserDocFromAuthToken(firebaseAuthToken);
+	if (!userDoc || !userDoc.get('stripe_customer_id')) {
+		return NextResponse.json(null);
+	}
 	const session = await stripe.billingPortal.sessions.create({
 		return_url: returnUrl,
 		customer: userDoc?.get('stripe_customer_id'),
