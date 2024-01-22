be21135d209fd09651e8c1830079596c46de650c
Michael Kündig <michael@socialincome.org>
2024-01-22 15:28:32 +0700

    bugfix(functions): fix translations in functions runtime (#703)

--- functions/src/webhooks/admin/donation-certificates/index.ts
+++ functions/src/webhooks/admin/donation-certificates/index.ts
@@ -35,65 +35,69 @@ export default onCall<CreateDonationCertificatesFunctionProps, Promise<string>>(
 		userIds = request.data.userIds;
 	}
 
-	for await (const userId of userIds) {
-		try {
-			const writer = await DonationCertificateWriter.getInstance(userId, request.data.year);
-			if (writer.contributionsByCurrency.size() === 0) {
-				console.info(`No contributions found for user ${userId}`);
-				continue;
-			}
+	await Promise.all(
+		userIds.map(async (userId) => {
+			try {
+				const writer = await DonationCertificateWriter.getInstance(userId, request.data.year);
+				if (writer.contributionsByCurrency.size() === 0) {
+					console.info(`No contributions found for user ${userId}`);
+					return;
+				}
 
-			await withFile(async ({ path }) => {
-				await writer.writeDonationCertificatePDF(path);
+				await withFile(async ({ path }) => {
+					await writer.writeDonationCertificatePDF(path);
 
-				const { downloadUrl } = await storageAdmin.uploadAndGetDownloadURL({
-					sourceFilePath: path,
-					destinationFilePath: `users/${userId}/${writer.year}_${writer.user.language}.pdf`,
-				});
-
-				await firestoreAdmin
-					.collection(`${USER_FIRESTORE_PATH}/${userId}/${DONATION_CERTIFICATE_FIRESTORE_PATH}`)
-					.doc(`${writer.year}-${writer.user.address?.country}`)
-					.set({ year: writer.year, country: writer.user.address?.country, url: downloadUrl } as DonationCertificate, {
-						merge: true,
+					const { downloadUrl } = await storageAdmin.uploadAndGetDownloadURL({
+						sourceFilePath: path,
+						destinationFilePath: `users/${userId}/${writer.year}_${writer.user.language}.pdf`,
 					});
 
-				console.info(`Donation certificate document written for user ${userId}`);
-				successCount += 1;
+					await firestoreAdmin
+						.collection(`${USER_FIRESTORE_PATH}/${userId}/${DONATION_CERTIFICATE_FIRESTORE_PATH}`)
+						.doc(`${writer.year}-${writer.user.address?.country}`)
+						.set(
+							{ year: writer.year, country: writer.user.address?.country, url: downloadUrl } as DonationCertificate,
+							{
+								merge: true,
+							},
+						);
+					console.info(`Donation certificate document written for user ${userId}`);
+					successCount += 1;
 
-				// if (request.data.sendEmails) {
-				// await sendEmail({
-				// 	to: user.email,
-				// 	subject: translator.t('email-subject'),
-				// 	// TODO: Use renderEmailTemplate() instead of renderTemplate()
-				// 	content: await renderTemplate({
-				// 		language: user.language || 'de',
-				// 		translationNamespace: 'donation-certificate',
-				// 		hbsTemplatePath: 'email/donation-certificate.hbs',
-				// 		context: {
-				// 			title: translator.t('title', { context: { year } }),
-				// 			signature: translator.t('title', { context: { year } }),
-				// 			firstname: user.personal?.name,
-				// 			year: year,
-				// 		},
-				// 	}),
-				// 	attachments: [
-				// 		{
-				// 			filename: translator.t('filename', { context: { year } }),
-				// 			path: path,
-				// 		},
-				// 	],
-				// 	from: NOTIFICATION_EMAIL_USER_KERRIN,
-				// 	user: NOTIFICATION_EMAIL_USER_KERRIN,
-				// 	password: NOTIFICATION_EMAIL_PASSWORD_KERRIN,
-				// });
-				// }
-			});
-		} catch (e) {
-			usersWithFailures.push(userId);
-			console.error(e);
-		}
-	}
+					// if (request.data.sendEmails) {
+					// await sendEmail({
+					// 	to: user.email,
+					// 	subject: translator.t('email-subject'),
+					// 	// TODO: Use renderEmailTemplate() instead of renderTemplate()
+					// 	content: await renderTemplate({
+					// 		language: user.language || 'de',
+					// 		translationNamespace: 'donation-certificate',
+					// 		hbsTemplatePath: 'email/donation-certificate.hbs',
+					// 		context: {
+					// 			title: translator.t('title', { context: { year } }),
+					// 			signature: translator.t('title', { context: { year } }),
+					// 			firstname: user.personal?.name,
+					// 			year: year,
+					// 		},
+					// 	}),
+					// 	attachments: [
+					// 		{
+					// 			filename: translator.t('filename', { context: { year } }),
+					// 			path: path,
+					// 		},
+					// 	],
+					// 	from: NOTIFICATION_EMAIL_USER_KERRIN,
+					// 	user: NOTIFICATION_EMAIL_USER_KERRIN,
+					// 	password: NOTIFICATION_EMAIL_PASSWORD_KERRIN,
+					// });
+					// }
+				});
+			} catch (e) {
+				usersWithFailures.push(userId);
+				console.error(e);
+			}
+		}),
+	);
 	return `Successfully created ${successCount} donation certificates for ${request.data.year} 
 	Users with errors (${usersWithFailures.length}): ${usersWithFailures.join(',')}`;
 });
