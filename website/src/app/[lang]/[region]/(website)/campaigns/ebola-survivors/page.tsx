991e52615078bc6de116ba9da428edcd82718be0
Sandino Scheidegger <sandino@socialincome.org>
2024-01-22 23:22:11 +0100

    Small issue with unused lang parameter

--- website/src/app/[lang]/[region]/(website)/campaigns/ebola-survivors/page.tsx
+++ website/src/app/[lang]/[region]/(website)/campaigns/ebola-survivors/page.tsx
@@ -3,11 +3,9 @@ import { CampaignCard } from '@/app/[lang]/[region]/(website)/campaigns/ebola-su
 import { CampaignFaq } from '@/app/[lang]/[region]/(website)/campaigns/ebola-survivors/campaign-faq';
 import { CampaignTitle } from '@/app/[lang]/[region]/(website)/campaigns/ebola-survivors/campaign-title';
 import { DonationForm } from '@/app/[lang]/[region]/(website)/campaigns/ebola-survivors/donation-form';
-import { Translator } from '@socialincome/shared/src/utils/i18n';
 import { BaseContainer } from '@socialincome/ui';
 
 export default async function Page({ params: { lang } }: DefaultPageProps) {
-	const translator = await Translator.getInstance({ language: lang, namespaces: 'website-campaign-ebola-survivors' });
 
 	return (
 		<BaseContainer className="min-h-screen-navbar flex flex-col space-y-12 py-16 md:py-32">
