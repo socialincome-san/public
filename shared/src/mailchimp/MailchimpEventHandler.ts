35ace3501b588173f5b19dbac62e9b5f9419e8f6
brennerthomas <brennerthomas@users.noreply.github.com>
2024-02-12 13:01:23 +0000

    Prettified Code!

--- shared/src/mailchimp/MailchimpEventHandler.ts
+++ shared/src/mailchimp/MailchimpEventHandler.ts
@@ -3,68 +3,64 @@
 import mailchimp from '@mailchimp/mailchimp_marketing';
 
 export type MailchimpSubscriptionData = {
-    firstname?: string;
-    lastname?: string;
-    gender?: string;
-    email: string;
-    country?: string;
-    language: string;
-    status: "subscribed" | "unsubscribed";
+	firstname?: string;
+	lastname?: string;
+	gender?: string;
+	email: string;
+	country?: string;
+	language: string;
+	status: 'subscribed' | 'unsubscribed';
 };
 
 export class MailchimpEventHandler {
 	constructor(apiKey: string, server: string) {
-        mailchimp.setConfig({
-            apiKey: apiKey,
-            server: server
-        })
+		mailchimp.setConfig({
+			apiKey: apiKey,
+			server: server,
+		});
 	}
 
-    checkSubscription = async (email: string, listId: string) => {
-        try {
-            const response = await mailchimp.lists.getListMember(listId, this.md5(email.toLowerCase()));
-            if (response?.status === 'subscribed') {
-                return 'subscribed';
-            } else {
-                return 'unsubscribed';
-            }
-        } catch (error) {
-            return 'unsubscribed';
-        }
+	checkSubscription = async (email: string, listId: string) => {
+		try {
+			const response = await mailchimp.lists.getListMember(listId, this.md5(email.toLowerCase()));
+			if (response?.status === 'subscribed') {
+				return 'subscribed';
+			} else {
+				return 'unsubscribed';
+			}
+		} catch (error) {
+			return 'unsubscribed';
+		}
 	};
 
-    updateUser = async (data: MailchimpSubscriptionData, listId: string) => {
-        try {
-            const response = await mailchimp.lists.batchListMembers(listId, {
-                members: [
-                    {
-                        email_address: data.email,
-                        email_type: "text",
-                        status: data.status,
-                        merge_fields: {
-                            "FNAME": data.firstname ?? "",
-	                        "LNAME": data.lastname ?? "",
-                            "COUNTRY": data.country ?? "",
-                            "LANGUAGE": data.language ?? "",
-                            "GENDER": data.gender ?? ""
-                        }
-                    }
-                ],
-                update_existing: true
-            });
-            return response;
-        } catch (error) {
-            return null;
-        }
-    }
+	updateUser = async (data: MailchimpSubscriptionData, listId: string) => {
+		try {
+			const response = await mailchimp.lists.batchListMembers(listId, {
+				members: [
+					{
+						email_address: data.email,
+						email_type: 'text',
+						status: data.status,
+						merge_fields: {
+							FNAME: data.firstname ?? '',
+							LNAME: data.lastname ?? '',
+							COUNTRY: data.country ?? '',
+							LANGUAGE: data.language ?? '',
+							GENDER: data.gender ?? '',
+						},
+					},
+				],
+				update_existing: true,
+			});
+			return response;
+		} catch (error) {
+			return null;
+		}
+	};
 
-    
-
-    // Helper function to calculate the MD5 hash
-    md5 = (str: string):string => {
-        const crypto = require('crypto');
-        return crypto.createHash('md5').update(str).digest('hex');
-    }
-
-	
+	// Helper function to calculate the MD5 hash
+	md5 = (str: string): string => {
+		const crypto = require('crypto');
+		return crypto.createHash('md5').update(str).digest('hex');
+	};
 }
--- website/src/app/api/mailchimp/checkSubscriptionStatus/route.ts
+++ website/src/app/api/mailchimp/checkSubscriptionStatus/route.ts
@@ -1,19 +1,21 @@
-import { NextResponse } from 'next/server';
 import { MailchimpEventHandler } from '@socialincome/shared/src/mailchimp/MailchimpEventHandler';
+import { NextResponse } from 'next/server';
 
 export type MailchimpData = {
 	email: string;
 	subscriptionStatus: string;
 };
 
-
 export async function GET(request: Request) {
-    const { searchParams } = new URL(request.url);
-	const email: string = searchParams.get('email') ?? "";
-    const mailchimpEventHandler = new MailchimpEventHandler(process.env.MAILCHIMP_API_KEY!, process.env.MAILCHIMP_SERVER!);
-    const mailchimpData: MailchimpData = {
-        email: email,
-        subscriptionStatus: await mailchimpEventHandler.checkSubscription(email, process.env.MAILCHIMP_LIST_ID!)
-    };
-    return NextResponse.json(mailchimpData);
+	const { searchParams } = new URL(request.url);
+	const email: string = searchParams.get('email') ?? '';
+	const mailchimpEventHandler = new MailchimpEventHandler(
+		process.env.MAILCHIMP_API_KEY!,
+		process.env.MAILCHIMP_SERVER!,
+	);
+	const mailchimpData: MailchimpData = {
+		email: email,
+		subscriptionStatus: await mailchimpEventHandler.checkSubscription(email, process.env.MAILCHIMP_LIST_ID!),
+	};
+	return NextResponse.json(mailchimpData);
 }
