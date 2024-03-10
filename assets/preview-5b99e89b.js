0c250dbe0b37fcf2706fbd0992e84c206ea3cc94
andrashee <andrashee@users.noreply.github.com>
2024-03-10 15:08:27 +0000

    Deploying to gh-pages from @ socialincome-san/public@02a771f3569c1f3b4a53533d78846fccd7075cf1 🚀

--- assets/preview-5b99e89b.js
+++ assets/preview-5b99e89b.js
@@ -1,2 +1,2 @@
 /* empty css                */const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{expanded:!0,matchers:{color:/(background|color)$/i,date:/Date$/}}};export{o as parameters};
-//# sourceMappingURL=preview-1273b0ad.js.map
+//# sourceMappingURL=preview-5b99e89b.js.map
