ab040d7205fefb476c2a2de8660218b26851d4f0
mkue <michael@socialincome.org>
2024-03-16 16:19:44 +0100

    chore(functions): remove postfinance balance import

--- functions/src/cron/index.ts
+++ functions/src/cron/index.ts
@@ -1,7 +1,5 @@
 import importExchangeRatesFunction from './exchange-rate-import';
-import importBalanceMailFunction from './postfinance-balance-import';
 import importPostfinancePaymentsFilesFunction from './postfinance-payments-files-import';
 
-export const importBalanceMail = importBalanceMailFunction;
 export const importPostfinancePaymentsFiles = importPostfinancePaymentsFilesFunction;
 export const importExchangeRates = importExchangeRatesFunction;
