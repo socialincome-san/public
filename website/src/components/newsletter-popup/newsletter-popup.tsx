b246fd69f71cda43ba6e28e980d1e4d7c0132ecf
mkue <michael@socialincome.org>
2024-03-24 21:49:58 +0100

    website(bugfix): fix client-side rendering issue

--- website/src/components/newsletter-popup/newsletter-popup.tsx
+++ website/src/components/newsletter-popup/newsletter-popup.tsx
@@ -1,5 +1,6 @@
 import { DefaultParams } from '@/app/[lang]/[region]';
 import { Translator } from '@socialincome/shared/src/utils/i18n';
+import { Suspense } from 'react';
 import { NewsletterPopupClient } from './newsletter-popup-client';
 
 export type NewsletterPopupProps = {
@@ -12,17 +13,19 @@ export default async function NewsletterPopup({ lang, region, delay = 0 }: Newsl
 		namespaces: ['website-newsletter'],
 	});
 	return (
-		<NewsletterPopupClient
-			lang={lang}
-			region={region}
-			delay={delay}
-			translations={{
-				informationLabel: translator.t('popup.information-label'),
-				toastSuccess: translator.t('popup.toast-success'),
-				toastFailure: translator.t('popup.toast-failure'),
-				emailPlaceholder: translator.t('popup.email-placeholder'),
-				buttonAddSubscriber: translator.t('popup.button-subscribe'),
-			}}
-		/>
+		<Suspense fallback={null}>
+			<NewsletterPopupClient
+				lang={lang}
+				region={region}
+				delay={delay}
+				translations={{
+					informationLabel: translator.t('popup.information-label'),
+					toastSuccess: translator.t('popup.toast-success'),
+					toastFailure: translator.t('popup.toast-failure'),
+					emailPlaceholder: translator.t('popup.email-placeholder'),
+					buttonAddSubscriber: translator.t('popup.button-subscribe'),
+				}}
+			/>
+		</Suspense>
 	);
 }
