67af47650ef82fdb9d3b59327088e58f30b79f08
Sandino Scheidegger <sandino@socialincome.org>
2024-03-25 01:05:22 +0100

    Styling all elements

--- website/src/app/[lang]/[region]/(website)/newsletter/page.tsx
+++ website/src/app/[lang]/[region]/(website)/newsletter/page.tsx
@@ -1,8 +1,10 @@
 import { DefaultPageProps } from '@/app/[lang]/[region]';
 import { SubscriptionInfoForm } from '@/app/[lang]/[region]/(website)/newsletter/subscription-info-form';
-
 import { Translator } from '@socialincome/shared/src/utils/i18n';
-import { Alert, AlertDescription, AlertTitle } from '@socialincome/ui';
+import { Typography, BaseContainer } from '@socialincome/ui';
+import Image from "next/image";
+import aurelieImage from "../about-us/(assets)/aurelie.jpeg";
+import Link from "next/link";
 
 export default async function Page({ params }: DefaultPageProps) {
 	const translator = await Translator.getInstance({
@@ -11,25 +13,92 @@ export default async function Page({ params }: DefaultPageProps) {
 	});
 
 	return (
-		<div className="mx-auto flex w-screen max-w-6xl flex-col space-y-4 md:px-5">
-			<Alert variant="primary">
-				<AlertTitle>{translator.t('updates.alert-title')}</AlertTitle>
-				<AlertDescription>{translator.t('updates.alert-description')}</AlertDescription>
-			</Alert>
-			<SubscriptionInfoForm
-				lang={params.lang}
-				region={params.region}
-				translations={{
-					firstname: translator.t('updates.firstname'),
-					lastname: translator.t('updates.lastname'),
-					email: translator.t('updates.email'),
-					country: translator.t('updates.country'),
-					language: translator.t('updates.language'),
-					updatesSubmitButton: translator.t('updates.submit-button'),
-					toastMessage: translator.t('updates.newsletter-updated-toast'),
-					toastErrorMessage: translator.t('updates.newsletter-error-toast'),
-				}}
-			/>
+		<>
+		<BaseContainer>
+		<div className="py-12">
+			<div className="grid grid-cols-1 gap-20 lg:grid-cols-2">
+				<div className="flex flex-col justify-center gap-3">
+					<div>
+						<Typography
+							weight="medium"
+							color="foreground"
+							style={{ lineHeight: '70px' }}
+							className="mt-2 text-[4rem]"
+						>
+							{translator.t('updates.title')}
+						</Typography>
+					</div>
+					<div className="my-8">
+						<Typography size="2xl" color="muted-foreground">
+							{translator.t('updates.description-1')}
+						</Typography>
+						<div className="mt-4">
+							<ul className="list-disc pl-5 mt-2 text-muted-foreground leading-relaxed">
+								<li><Typography size="2xl" color="muted-foreground">{translator.t('updates.bullet-1')}</Typography></li>
+								<li><Typography size="2xl" color="muted-foreground">{translator.t('updates.bullet-2')}</Typography></li>
+								<li><Typography size="2xl" color="muted-foreground">{translator.t('updates.bullet-3')}</Typography></li>
+							</ul>
+						</div>
+						<div className="mt-4">
+							<Typography size="2xl" color="muted-foreground">
+								{translator.t('updates.description-2')}
+							</Typography>
+						</div>
+						<hr className="my-8 bg-gray-200 h-px border-0" />
+						<div>
+							<div className="mb-4">
+									<Typography size="xl" color="muted-foreground">
+										{translator.t('updates.description-3')}
+									</Typography>
+								</div>
+								<div>
+									<Link
+										href="/about-us#team"
+										target="_blank"
+										rel="noopener noreferrer"
+										className="bg-background hover:bg-muted flex items-center rounded-full p-2 text-center text-muted-foreground"
+									>
+										<Image
+											src={aurelieImage}
+											width={40}
+											height={40}
+											className="mr-1 h-10 w-10 rounded-full transition-transform duration-300 object-cover"
+											alt="Avatar"
+										/>
+										<div className="pl-3">
+											<span className="text-xl text-muted-foreground">{translator.t('updates.author-name')}</span>
+											<span className="text-md pl-2 text-popover-foreground-muted">{translator.t('updates.author-city')}</span>
+										</div>
+									</Link>
+								</div>
+							</div>
+					</div>
+				</div>
+				<div className="flex items-center justify-center">
+					<div className="card bg-primary w-full rounded-xl p-6">
+						<div className="mb-4">
+							<Typography size="2xl" color="popover">
+								Newsletter Subscription
+							</Typography>
+						</div>
+						<div className="mt-3">
+							<SubscriptionInfoForm
+								lang={params.lang}
+								region={params.region}
+								translations={{
+									firstname: translator.t("updates.firstname"),
+									email: translator.t("updates.email"),
+									updatesSubmitButton: translator.t("updates.submit-button"),
+									toastMessage: translator.t("updates.newsletter-updated-toast"),
+									toastErrorMessage: translator.t("updates.newsletter-error-toast"),
+								}}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
+		</BaseContainer>
+		</>
 	);
 }
--- website/src/app/[lang]/[region]/(website)/newsletter/subscription-info-form.tsx
+++ website/src/app/[lang]/[region]/(website)/newsletter/subscription-info-form.tsx
@@ -5,7 +5,6 @@ import { useApi } from '@/hooks/useApi';
 import { useTranslator } from '@/hooks/useTranslator';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { NewsletterSubscriptionData } from '@socialincome/shared/src/mailchimp/MailchimpAPI';
-import { COUNTRY_CODES } from '@socialincome/shared/src/types/country';
 import {
 	Button,
 	Form,
@@ -15,24 +14,16 @@ import {
 	FormLabel,
 	FormMessage,
 	Input,
-	Select,
-	SelectContent,
-	SelectGroup,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
 } from '@socialincome/ui';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
 import * as z from 'zod';
+import { useState } from "react";
 
 type PersonalInfoFormProps = {
 	translations: {
 		firstname: string;
-		lastname: string;
 		email: string;
-		country: string;
-		language: string;
 		updatesSubmitButton: string;
 		toastMessage: string;
 		toastErrorMessage: string;
@@ -40,16 +31,12 @@ type PersonalInfoFormProps = {
 } & DefaultParams;
 
 export function SubscriptionInfoForm({ lang, translations }: PersonalInfoFormProps) {
-	const commonTranslator = useTranslator(lang, 'common');
-	const countryTranslator = useTranslator(lang, 'countries');
 	const api = useApi();
+	const [submitting, setSubmitting] = useState(false);
 
 	const formSchema = z.object({
 		firstname: z.string(),
-		lastname: z.string(),
 		email: z.string().email(),
-		country: z.enum(['', ...COUNTRY_CODES]).optional(),
-		language: z.enum(['en', 'de']),
 	});
 
 	type FormSchema = z.infer<typeof formSchema>;
@@ -57,20 +44,15 @@ export function SubscriptionInfoForm({ lang, translations }: PersonalInfoFormPro
 		resolver: zodResolver(formSchema),
 		defaultValues: {
 			firstname: '',
-			lastname: '',
 			email: '',
-			country: '' as any,
-			language: '' as any,
 		},
 	});
 
 	const onSubmit = async (values: FormSchema) => {
+		setSubmitting(true);
 		const data: NewsletterSubscriptionData = {
 			firstname: values.firstname,
-			lastname: values.lastname,
 			email: values.email,
-			country: values.country as any,
-			language: values.language,
 			status: 'subscribed',
 		};
 
@@ -85,28 +67,15 @@ export function SubscriptionInfoForm({ lang, translations }: PersonalInfoFormPro
 
 	return (
 		<Form {...form}>
-			<form className="grid grid-cols-1 gap-y-4 md:grid-cols-2 md:gap-x-4" onSubmit={form.handleSubmit(onSubmit)}>
+			<form className="grid grid-cols-1 gap-y-4" onSubmit={form.handleSubmit(onSubmit)}>
 				<FormField
 					control={form.control}
 					name="firstname"
 					render={({ field }) => (
 						<FormItem>
-							<FormLabel>{translations.firstname}</FormLabel>
+							<FormLabel className="text-md text-primary-foreground-muted font-normal">{translations.firstname}</FormLabel>
 							<FormControl>
-								<Input type="text" {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="lastname"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>{translations.lastname}</FormLabel>
-							<FormControl>
-								<Input type="text" {...field} />
+								<Input type="text" className="h-14 text-xl p-4 w-full" {...field} />
 							</FormControl>
 							<FormMessage />
 						</FormItem>
@@ -117,63 +86,20 @@ export function SubscriptionInfoForm({ lang, translations }: PersonalInfoFormPro
 					name="email"
 					render={({ field }) => (
 						<FormItem>
-							<FormLabel>{translations.email}</FormLabel>
+							<FormLabel className="text-md text-primary-foreground-muted font-normal">{translations.email}</FormLabel>
 							<FormControl>
-								<Input type="email" {...field} />
+								<Input type="email" className="h-14 text-xl p-4 w-full" {...field} />
 							</FormControl>
 							<FormMessage />
 						</FormItem>
 					)}
 				/>
-				<FormField
-					control={form.control}
-					name="country"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>{translations.country}</FormLabel>
-							<Select onValueChange={field.onChange}>
-								<FormControl>
-									<SelectTrigger>
-										<SelectValue>{field.value && countryTranslator?.t(field.value)}</SelectValue>
-									</SelectTrigger>
-								</FormControl>
-								<SelectContent className="max-h-[16rem] overflow-y-auto">
-									<SelectGroup>
-										{COUNTRY_CODES.map((country) => (
-											<SelectItem key={country} value={country}>
-												{countryTranslator?.t(country)}
-											</SelectItem>
-										))}
-									</SelectGroup>
-								</SelectContent>
-							</Select>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="language"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>{translations.language}</FormLabel>
-							<Select onValueChange={field.onChange}>
-								<FormControl>
-									<SelectTrigger>
-										<SelectValue>{field.value && commonTranslator?.t(`languages.${field.value}`)}</SelectValue>
-									</SelectTrigger>
-								</FormControl>
-								<SelectContent>
-									<SelectItem value="en">{commonTranslator?.t('languages.en')}</SelectItem>
-									<SelectItem value="de">{commonTranslator?.t('languages.de')}</SelectItem>
-								</SelectContent>
-							</Select>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-
-				<Button variant="ghost" size="lg" type="submit" className="md:col-span-2">
+				<Button
+					size="lg"
+					type="submit"
+					variant="default"
+					showLoadingSpinner={submitting}
+					className="bg-accent text-accent-foreground hover:bg-accent-muted active:bg-accent-muted rounded-full text-lg font-medium mt-4">
 					{translations.updatesSubmitButton}
 				</Button>
 			</form>
