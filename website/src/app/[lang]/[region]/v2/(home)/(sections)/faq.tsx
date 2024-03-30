1cc64b8fe8949c4f0f72ed299ea1885e5c459a52
Pranav Chatur <pranavchatur@gmail.com>
2024-03-30 18:08:01 +0530

    Add faq (english)

--- website/src/app/[lang]/[region]/v2/(home)/(sections)/faq.tsx
+++ website/src/app/[lang]/[region]/v2/(home)/(sections)/faq.tsx
@@ -1,7 +1,17 @@
 import { DefaultParams } from '@/app/[lang]/[region]';
 import { Translator } from '@socialincome/shared/src/utils/i18n';
-import { BaseContainer, Typography } from '@socialincome/ui';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger, BaseContainer, Typography } from '@socialincome/ui';
 import { FontColor } from '@socialincome/ui/src/interfaces/color';
+import Link from 'next/link';
+
+type FaqQuestion = {
+	prompt: string,
+	answer: string,
+	links: {
+		title: string,
+		href: string
+	}[]
+};
 
 export async function Faq({ lang, region }: DefaultParams) {
 	const translator = await Translator.getInstance({
@@ -9,17 +19,44 @@ export async function Faq({ lang, region }: DefaultParams) {
 		namespaces: ['website-home2'],
 	});
 
+	const questions = translator.t<FaqQuestion[]>('section-6.questions');
+
 	return (
 		<BaseContainer>
-			<Typography>{translator.t('section-6.title-1')}</Typography>
-			<div>
-				{translator.t<{ text: string; color?: FontColor }[]>('section-6.question-1').map((title, index) => (
-					<Typography as="span" key={index} color={title.color}>
-						{title.text}{' '}
-					</Typography>
+			<Typography size='2xl' className='text-center mb-12'>{translator.t('section-6.title-1')}</Typography>
+			<hr/>
+			<Accordion type="single" collapsible className="w-full mb-10">
+				{questions.map((question, index) => (
+					<AccordionItem key={index} value={`item-${index}`}>
+						<AccordionTrigger>
+							<Typography size="lg" className="text-left">
+								{question.prompt}
+							</Typography>
+						</AccordionTrigger>
+						<AccordionContent className="prose">
+							<Typography size="lg" className="text-left">
+								{question.answer}
+							</Typography>
+							{question.links && (
+								<ul className="mt-4 flex list-outside list-disc flex-col space-y-1 pl-3">
+									{question.links?.map((link, index2) => (
+										<li key={index2} className="mb-0 pl-3">
+											<Link href={link.href} target="_blank" rel="noreferrer" className="no-underline">
+												<Typography as="span" size="lg" color="primary" className="font-normal hover:underline">
+													{link.title}
+												</Typography>
+											</Link>
+										</li>
+									))}
+								</ul>
+							)}
+						</AccordionContent>
+					</AccordionItem>
 				))}
-			</div>
-			<Typography>{translator.t('section-6.cta')}</Typography>
+			</Accordion>
+			<Link href={'/faq'}>
+				<Typography color='accent' className='text-center'>{translator.t('section-6.cta')+' ›'}</Typography>
+			</Link>
 		</BaseContainer>
 	);
 }
