7e9e6eab8bcd9caf9fea62c08474575ad5f31904
mkue <michael@socialincome.org>
2024-04-12 17:55:35 +0200

    wip

--- website/src/app/[lang]/[region]/(website)/(home)/(sections)/hero.tsx
+++ website/src/app/[lang]/[region]/(website)/(home)/(sections)/hero.tsx
@@ -11,7 +11,7 @@ export async function Hero({ lang, region }: DefaultParams) {
 
 	return (
 		<BaseContainer className="min-h-screen-navbar grid grid-cols-1 content-center items-start gap-x-4 gap-y-8 align-top sm:gap-y-16 lg:grid-cols-2 lg:gap-y-0">
-			<div className="mx-auto w-full max-w-2xl lg:pr-6">
+			<div className="mx-auto w-full max-w-2xl">
 				<Typography size="5xl" weight="bold">
 					{translator.t('section-1.title-1')}
 					<Typography as="span" size="5xl" weight="bold" color="secondary">
