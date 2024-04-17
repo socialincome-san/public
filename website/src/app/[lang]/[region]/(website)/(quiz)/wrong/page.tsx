9c76c35cabd8f7f8fb402d1e4eefa98ecbb17b31
Sandino Scheidegger <sandino@socialincome.org>
2024-04-17 15:30:47 +0200

    Adding two simple pages, which says right or wrong (it will be used in future newsletter for interactive quiz questions. The pages will be linked to the tell the right or wrong answer)

---
+++ website/src/app/[lang]/[region]/(website)/(quiz)/wrong/page.tsx
@@ -0,0 +1,20 @@
+import { DefaultPageProps } from '@/app/[lang]/[region]';
+import { Translator } from '@socialincome/shared/src/utils/i18n';
+import { BaseContainer, Typography} from '@socialincome/ui';
+
+export default async function Page({ params }: DefaultPageProps) {
+  const translator = await Translator.getInstance({
+    language: params.lang,
+    namespaces: ['website-quiz'],
+  });
+
+  return (
+    <BaseContainer className="flex items-center justify-center min-h-screen-navbar">
+      <div>
+        <Typography size="5xl" weight="medium" className="text-center">
+          {translator.t('result.wrong')}
+        </Typography>
+      </div>
+    </BaseContainer>
+  );
+}
