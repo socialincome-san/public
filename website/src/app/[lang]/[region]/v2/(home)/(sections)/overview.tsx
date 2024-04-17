ac55755927f1ae4dafdae770033cb2cdadd3aeff
Pranav Chatur <pranavchatur@gmail.com>
2024-04-17 20:04:26 +0530

    Rename translation keys

--- website/src/app/[lang]/[region]/v2/(home)/(sections)/overview.tsx
+++ website/src/app/[lang]/[region]/v2/(home)/(sections)/overview.tsx
@@ -25,13 +25,13 @@ export async function Overview({ lang, region }: DefaultParams) {
 				{translator.t('section-2.title-3')}
 			</Typography>
 			<ol className='w-3/5'>
-			<li><Typography>{translator.t('section-2.text-1.1')}</Typography></li>
-			<li><Typography>{translator.t('section-2.text-1.2')}</Typography></li>
+			<li><Typography>{translator.t('section-2.text-3.1')}</Typography></li>
+			<li><Typography>{translator.t('section-2.text-3.2')}</Typography></li>
 			</ol>
 			<Typography size="3xl" weight='medium' color="foreground-dark" className='text-center w-3/5 mt-12 mb-5'>{translator.t('section-2.title-4')}</Typography>
-			<Typography className='w-3/5'>{translator.t('section-2.text-2')}</Typography>
+			<Typography className='w-3/5'>{translator.t('section-2.text-4')}</Typography>
 			<Typography size="3xl" weight='medium' color="foreground-dark" className='text-center w-3/5 mt-12 mb-5'>{translator.t('section-2.title-5')}</Typography>
-			<Typography className='w-3/5'>{translator.t('section-2.text-3')}</Typography>
+			<Typography className='w-3/5'>{translator.t('section-2.text-5')}</Typography>
 		</BaseContainer>
 	);
 }
