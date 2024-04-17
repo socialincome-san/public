c360ffefbd69c67c66038ae4abb8759b40847f36
ssandino <ssandino@users.noreply.github.com>
2024-04-17 13:35:21 +0000

    Prettified Code!

--- website/src/app/[lang]/[region]/(website)/(quiz)/wrong/page.tsx
+++ website/src/app/[lang]/[region]/(website)/(quiz)/wrong/page.tsx
@@ -1,20 +1,20 @@
 import { DefaultPageProps } from '@/app/[lang]/[region]';
 import { Translator } from '@socialincome/shared/src/utils/i18n';
-import { BaseContainer, Typography} from '@socialincome/ui';
+import { BaseContainer, Typography } from '@socialincome/ui';
 
 export default async function Page({ params }: DefaultPageProps) {
-  const translator = await Translator.getInstance({
-    language: params.lang,
-    namespaces: ['website-quiz'],
-  });
+	const translator = await Translator.getInstance({
+		language: params.lang,
+		namespaces: ['website-quiz'],
+	});
 
-  return (
-    <BaseContainer className="flex items-center justify-center min-h-screen-navbar">
-      <div>
-        <Typography size="5xl" weight="medium" className="text-center">
-          {translator.t('result.wrong')}
-        </Typography>
-      </div>
-    </BaseContainer>
-  );
+	return (
+		<BaseContainer className="min-h-screen-navbar flex items-center justify-center">
+			<div>
+				<Typography size="5xl" weight="medium" className="text-center">
+					{translator.t('result.wrong')}
+				</Typography>
+			</div>
+		</BaseContainer>
+	);
 }
