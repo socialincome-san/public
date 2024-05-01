f61537202ca6b29267b568e5718c7abfe255ab17
mkue <michael@socialincome.org>
2024-05-01 19:08:45 +0200

    refactor(functions): update first payout email parameters

--- shared/src/sendgrid/SendgridMailClient.ts
+++ shared/src/sendgrid/SendgridMailClient.ts
@@ -1,10 +1,15 @@
 import { MailService } from '@sendgrid/mail';
 import { Currency } from '../types/currency';
+import { LanguageCode } from '../types/language';
 
 export type FirstPayoutEmailTemplateData = {
 	first_name?: string;
 	donation_amount: number;
 	currency: Currency;
+	n_days_ago: number;
+	donation_amount_sm: number;
+	donation_amount_md: number;
+	one_time_donation: boolean;
 };
 
 export class SendgridMailClient extends MailService {
@@ -13,11 +18,17 @@ export class SendgridMailClient extends MailService {
 		this.setApiKey(apiKey);
 	}
 
-	sendFirstPayoutEmail = async (email: string, data: FirstPayoutEmailTemplateData) => {
+	sendFirstPayoutEmail = async (email: string, language: LanguageCode, data: FirstPayoutEmailTemplateData) => {
+		let templateId = '';
+		if (language === 'de') {
+			templateId = 'd-3146eb9ee2054b28b22376113eb96ca9';
+		} else {
+			templateId = 'd-4e616d721b0240509f468c1e5ff22e6d';
+		}
 		await this.send({
 			to: email,
 			from: 'hello@socialincome.org',
-			templateId: 'd-4e616d721b0240509f468c1e5ff22e6d',
+			templateId,
 			dynamicTemplateData: data,
 		});
 	};
