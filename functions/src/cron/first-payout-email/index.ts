e10d1e3a8da51f49a6fa196a3a06a2049bb387b2
ssandino <ssandino@users.noreply.github.com>
2024-05-16 18:35:40 +0000

    Prettified Code!

--- functions/src/cron/first-payout-email/index.ts
+++ functions/src/cron/first-payout-email/index.ts
@@ -1,12 +1,12 @@
+import { onSchedule } from 'firebase-functions/v2/scheduler';
+import { DateTime } from 'luxon';
 import { FirestoreAdmin } from '../../../../shared/src/firebase/admin/FirestoreAdmin';
 import { toFirebaseAdminTimestamp } from '../../../../shared/src/firebase/admin/utils';
+import { FirstPayoutEmailTemplateData, SendgridMailClient } from '../../../../shared/src/sendgrid/SendgridMailClient';
 import { Contribution, CONTRIBUTION_FIRESTORE_PATH } from '../../../../shared/src/types/contribution';
 import { LanguageCode } from '../../../../shared/src/types/language';
 import { User, USER_FIRESTORE_PATH } from '../../../../shared/src/types/user';
 import { toDateTime } from '../../../../shared/src/utils/date';
-import { onSchedule } from 'firebase-functions/v2/scheduler';
-import { DateTime } from 'luxon';
-import { FirstPayoutEmailTemplateData, SendgridMailClient } from '../../../../shared/src/sendgrid/SendgridMailClient';
 
 export const getFirstPayoutEmailReceivers = async (
 	firestoreAdmin: FirestoreAdmin,
@@ -62,7 +62,8 @@ export const getFirstPayoutEmailReceivers = async (
 };
 
 // Run on the 16th of every month at 00:00
-export default onSchedule('0 18 16 * *', async () => { // TODO: change back to '0 0 16 * *
+export default onSchedule('0 18 16 * *', async () => {
+	// TODO: change back to '0 0 16 * *
 	const sendgridClient = new SendgridMailClient(process.env.SENDGRID_API_KEY!);
 	const firestoreAdmin = new FirestoreAdmin();
 
