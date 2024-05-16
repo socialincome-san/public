462da90b38943a0187535ae1f0f42cf1c70b724a
Michael Kündig <michael@cradle.bio>
2024-05-16 21:42:38 +0200

    refactor(functions): send confirmation email after sending first payout email

--- functions/src/cron/index.ts
+++ functions/src/cron/index.ts
@@ -1,7 +1,7 @@
 import importExchangeRatesFunction from './exchange-rate-import';
-import importPostfinancePaymentsFilesFunction from './postfinance-payments-files-import';
 import sendFirstPayoutEmailFunction from './first-payout-email';
+import importPostfinancePaymentsFilesFunction from './postfinance-payments-files-import';
 
 export const importPostfinancePaymentsFiles = importPostfinancePaymentsFilesFunction;
 export const importExchangeRates = importExchangeRatesFunction;
-export const sendFirstPayoutEmail = sendFirstPayoutEmailFunction
+export const sendFirstPayoutEmail = sendFirstPayoutEmailFunction;
