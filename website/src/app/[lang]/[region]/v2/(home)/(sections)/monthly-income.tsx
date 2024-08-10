744ea8b79c2fca1b18bf52c37ae9445d1ce66989
mkue <michael@socialincome.org>
2024-08-10 15:59:07 +0200

    feature(website): update monthly income section on new landing page

--- website/src/app/[lang]/[region]/v2/(home)/(sections)/monthly-income.tsx
+++ website/src/app/[lang]/[region]/v2/(home)/(sections)/monthly-income.tsx
@@ -3,7 +3,7 @@ import { Translator } from '@socialincome/shared/src/utils/i18n';
 import { BaseContainer, Typography } from '@socialincome/ui';
 import { IncomeInput } from '../(components)/income-input';
 
-export async function MonthlyIncome({ lang, region }: DefaultParams) {
+export async function MonthlyIncome({ lang }: DefaultParams) {
 	const translator = await Translator.getInstance({
 		language: lang,
 		namespaces: ['website-home2', 'common'],
