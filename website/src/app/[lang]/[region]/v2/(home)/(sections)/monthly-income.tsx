0ba71d009f66f9a51df2b6abaf984792f7343553
mkue <michael@socialincome.org>
2024-08-10 15:58:47 +0200

    feature(website): update monthly income section on new landing page

--- website/src/app/[lang]/[region]/v2/(home)/(sections)/monthly-income.tsx
+++ website/src/app/[lang]/[region]/v2/(home)/(sections)/monthly-income.tsx
@@ -10,20 +10,15 @@ export async function MonthlyIncome({ lang, region }: DefaultParams) {
 	});
 
 	return (
-		// <BaseContainer backgroundColor="bg-foreground-dark" className="py-3">
-		<BaseContainer className="theme-blue-v2 py-3">
-			{' '}
-			{/* Is there a way to use theme-blue-v2 to set background blue? */}
-			<div className="my-4 py-12 text-center text-white">
-				<Typography size="lg" className="opacity-40">
-					{translator.t('section-3.title-faded')}
-				</Typography>
-				<Typography size="3xl" className="py-8">
-					{translator.t('section-3.title')}
-				</Typography>
-				<IncomeInput />
-				<Typography className="pb-2 pt-8 opacity-40">{translator.t('section-3.subtitle')}</Typography>
-			</div>
+		<BaseContainer className="theme-blue-v2 my-4 py-20 text-center">
+			<Typography size="lg" className="opacity-40">
+				{translator.t('section-3.title-faded')}
+			</Typography>
+			<Typography size="3xl" className="py-8">
+				{translator.t('section-3.title')}
+			</Typography>
+			<IncomeInput />
+			<Typography className="pb-2 pt-8 opacity-40">{translator.t('section-3.subtitle')}</Typography>
 		</BaseContainer>
 	);
 }
