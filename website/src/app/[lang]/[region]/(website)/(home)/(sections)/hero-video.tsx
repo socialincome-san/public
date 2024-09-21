918b0399b79dd10d9caf6580897f535b2863cdb8
Sandino Scheidegger <sandino.scheidegger@me.com>
2024-09-21 17:39:24 +0200

    WIP Translations

--- website/src/app/[lang]/[region]/(website)/(home)/(sections)/hero-video.tsx
+++ website/src/app/[lang]/[region]/(website)/(home)/(sections)/hero-video.tsx
@@ -31,7 +31,7 @@ export async function HeroVideo({ lang, region }: DefaultParams) {
 						))}
 					</div>
 					<Link href={`/${lang}/${region}/donate/individual`}>
-						<Button className="mx-auto hidden md:block">Handle Jetzt</Button>
+						<Button className="mx-auto hidden md:block"><Typography>{translator.t('section-1.take-action')}</Typography></Button>
 					</Link>
 				</div>
 			</div>
