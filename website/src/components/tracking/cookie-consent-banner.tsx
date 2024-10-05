688a9e7468e10fbb72a7e0b269f14bf68883b49c
ssandino <ssandino@users.noreply.github.com>
2024-10-05 21:55:51 +0000

    Prettified Code!

--- website/src/components/tracking/cookie-consent-banner.tsx
+++ website/src/components/tracking/cookie-consent-banner.tsx
@@ -27,16 +27,24 @@ export function CookieConsentBanner({ translations }: CookieConsentBannerClientP
 	if (hideBanner) return null;
 
 	return (
-		<Card className="fixed bottom-2 right-2 mx-auto max-w-6xl md:right-4 bg-white w-fit rounded-full px-4 border-white">
-			<CardContent className="flex flex-col space-y-2 p-4 md:flex-row md:items-center md:justify-between md:h-full">
-				<Typography className="md:col-span-3 md:flex-1 md:self-center md:mr-4">
+		<Card className="fixed bottom-2 right-2 mx-auto w-fit max-w-6xl rounded-full border-white bg-white px-4 md:right-4">
+			<CardContent className="flex flex-col space-y-2 p-4 md:h-full md:flex-row md:items-center md:justify-between">
+				<Typography className="md:col-span-3 md:mr-4 md:flex-1 md:self-center">
 					<Typography as="span" dangerouslySetInnerHTML={{ __html: translations.text }} />
 				</Typography>
-				<div className="pb-1 flex space-x-2 md:flex-none md:self-center md:items-center md:h-full md:justify-center md:flex-row md:align-middle">
-					<Button variant="outline" className="border-primary text-primary bg-transparent md:self-center" onClick={() => setCookieConsent('denied')}>
+				<div className="flex space-x-2 pb-1 md:h-full md:flex-none md:flex-row md:items-center md:justify-center md:self-center md:align-middle">
+					<Button
+						variant="outline"
+						className="border-primary text-primary bg-transparent md:self-center"
+						onClick={() => setCookieConsent('denied')}
+					>
 						{translations.buttonRefuse}
 					</Button>
-					<Button variant="outline" className="border-primary text-primary bg-transparent md:self-center" onClick={() => setCookieConsent('granted')}>
+					<Button
+						variant="outline"
+						className="border-primary text-primary bg-transparent md:self-center"
+						onClick={() => setCookieConsent('granted')}
+					>
 						{translations.buttonAccept}
 					</Button>
 				</div>
