8aa5bfed0f27ba6aeb05f9dd8070a20f516892b4
Sandino Scheidegger <sandino.scheidegger@me.com>
2024-10-05 23:53:20 +0200

    Improved cookie banner

--- website/src/components/tracking/cookie-consent-banner.tsx
+++ website/src/components/tracking/cookie-consent-banner.tsx
@@ -27,17 +27,17 @@ export function CookieConsentBanner({ translations }: CookieConsentBannerClientP
 	if (hideBanner) return null;
 
 	return (
-		<Card className="fixed bottom-2 left-2 right-2 mx-auto max-w-6xl shadow-xl md:left-4 md:right-4">
-			<CardContent className="flex flex-col space-y-2 p-4">
-				<Typography className="md:col-span-3">
+		<Card className="fixed bottom-2 right-2 mx-auto max-w-6xl md:right-4 bg-white w-fit rounded-full px-4 border-white">
+			<CardContent className="flex flex-col space-y-2 p-4 md:flex-row md:items-center md:justify-between md:h-full">
+				<Typography className="md:col-span-3 md:flex-1 md:self-center md:mr-4">
 					<Typography as="span" dangerouslySetInnerHTML={{ __html: translations.text }} />
 				</Typography>
-				<div className="flex space-x-2">
-					<Button variant="outline" onClick={() => setCookieConsent('granted')}>
-						{translations.buttonAccept}
-					</Button>
-					<Button variant="destructive" onClick={() => setCookieConsent('denied')}>
+				<div className="pb-1 flex space-x-2 md:flex-none md:self-center md:items-center md:h-full md:justify-center md:flex-row md:align-middle">
+					<Button variant="outline" className="border-primary text-primary bg-transparent md:self-center" onClick={() => setCookieConsent('denied')}>
 						{translations.buttonRefuse}
+					</Button>
+					<Button variant="outline" className="border-primary text-primary bg-transparent md:self-center" onClick={() => setCookieConsent('granted')}>
+						{translations.buttonAccept}
 					</Button>
 				</div>
 			</CardContent>
