106192e45cb59ed5503443fafa553d9923d79583
Sandino Scheidegger <sandino.scheidegger@me.com>
2024-10-13 23:37:08 +0200

    wip list with 1 ngo

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -25,3 +25,5 @@ export * from './components/table';
 export * from './components/tabs';
 export * from './components/tooltip';
 export * from './components/typography';
+export * from './components/separator';
+
