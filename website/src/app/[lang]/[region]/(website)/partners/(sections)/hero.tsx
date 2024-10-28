b777b55303ed5e7a3f0214928149cb471b771860
Sandino Scheidegger <sandino@socialincome.org>
2024-10-28 16:59:01 +0100

    Small changes to send it to NGOs for editing

--- website/src/app/[lang]/[region]/(website)/partners/(sections)/hero.tsx
+++ website/src/app/[lang]/[region]/(website)/partners/(sections)/hero.tsx
@@ -1,6 +1,6 @@
 import { DefaultParams } from '@/app/[lang]/[region]';
 import { Translator } from '@socialincome/shared/src/utils/i18n';
-import { BaseContainer, Typography } from '@socialincome/ui';
+import { Typography } from '@socialincome/ui';
 import { FontColor } from '@socialincome/ui/src/interfaces/color';
 
 export async function Hero({ lang }: DefaultParams) {
@@ -10,15 +10,17 @@ export async function Hero({ lang }: DefaultParams) {
 	});
 
 	return (
-		<BaseContainer className="mx-auto mb-8 mt-20 flex w-4/5 flex-col items-center justify-center md:mb-20 lg:w-3/5">
+		<div className="mx-auto mb-8 mt-20 flex w-4/5 flex-col items-center justify-center md:mb-20 lg:w-3/5">
 			<div className="mb-8 text-center">
 				{translator.t<{ text: string; color?: FontColor }[]>('hero.title-1').map((title, index) => (
-					<Typography as="span" size="3xl" weight="medium" color={title.color} key={index}>
+					<Typography as="span" size="5xl" weight="medium" color={title.color} key={index}>
 						{title.text}{' '}
 					</Typography>
 				))}
 			</div>
-			<Typography className="mb-8">{translator.t('hero.subtitle')}</Typography>
-		</BaseContainer>
+			<Typography size="2xl" className="mb-8 text-center">
+				{translator.t('hero.subtitle')}
+			</Typography>
+		</div>
 	);
 }
