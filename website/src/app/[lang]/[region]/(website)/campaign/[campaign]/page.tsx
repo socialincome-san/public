4285c81eaf7c69aa0173cb9f70207d571e6d84a1
Sandino Scheidegger <sandino@socialincome.org>
2024-12-10 18:12:36 +0100

    Website: Generic FAQ for fundraisers (#979)

--- website/src/app/[lang]/[region]/(website)/campaign/[campaign]/page.tsx
+++ website/src/app/[lang]/[region]/(website)/campaign/[campaign]/page.tsx
@@ -25,6 +25,7 @@ import {
 	Typography,
 } from '@socialincome/ui';
 import { Progress } from '@socialincome/ui/src/components/progress';
+import Link from 'next/link';
 
 export type CampaignPageProps = {
 	params: {
@@ -312,10 +313,16 @@ export default async function Page({ params }: CampaignPageProps) {
 						{translator.t('campaign.title')}
 					</Typography>
 					<div className="space-y-6">
-						<Accordion type={'single'} collapsible className="w-full">
+						<Accordion type="single" collapsible className="w-full">
 							{translator
-								.t<{ question: string; answer: string }[]>('campaign.questions')
-								.map(({ question, answer }, index) => (
+								.t<
+									{
+										question: string;
+										answer: string;
+										links?: { title: string; href: string }[];
+									}[]
+								>('campaign.questions')
+								.map(({ question, answer, links }, index) => (
 									<AccordionItem value={`item-${index}`} key={index}>
 										<AccordionTrigger>
 											<Typography size="xl" color="muted-foreground" className="text-left" weight="normal">
@@ -329,6 +336,19 @@ export default async function Page({ params }: CampaignPageProps) {
 												className="mt-2"
 												dangerouslySetInnerHTML={{ __html: answer }}
 											/>
+											{links && (
+												<ul className="mt-4 flex list-outside list-disc flex-col space-y-1 pl-3">
+													{links.map((link: { title: string; href: string }, index: number) => (
+														<li key={index} className="mb-0 pl-3">
+															<Link href={link.href} target="_blank" rel="noreferrer" className="no-underline">
+																<Typography as="span" size="lg" color="primary" className="font-normal hover:underline">
+																	{link.title}
+																</Typography>
+															</Link>
+														</li>
+													))}
+												</ul>
+											)}
 										</AccordionContent>
 									</AccordionItem>
 								))}
