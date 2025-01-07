a85d93fbb1498f62e17eab93597a3336bca5a0a7
Aleksei.Shestakov <shestakov.alex@gmail.com>
2025-01-07 22:09:30 +0100

    Add storybook to the /ui folder

---
+++ ui/.storybook/preview.js
@@ -0,0 +1,15 @@
+import '../src/globals.css';
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+};
+
+export default preview;
