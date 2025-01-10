d3a1fc99c9aa83398b13c250fb1e8ac3b6e510e1
Gavriil <gavriiltzortzakis@gmail.com>
2025-01-10 20:13:30 +0100

    draft initial commit

--- website/src/app/[lang]/[region]/(website)/layout.tsx
+++ website/src/app/[lang]/[region]/(website)/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children, params: { lang, region } }: PropsWith
 	return (
 		<div className="theme-default [&:has(.hero-video)_nav]:bg-transparent">
 			<Navbar lang={lang} region={region} />
-			<main className="min-h-screen-navbar mt-16 py-8 md:mt-20 md:py-16">{children}</main>
+			<main className="min-h-screen-navbar mt-16 py-8 md:mt-20 md:py-16 [&:has(.blog)]:py-0">{children}</main>
 			<Footer lang={lang} region={region} />
 		</div>
 	);
