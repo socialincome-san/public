1d6845397efcea728c08bbb5093fe879fbc88d24
Inna Abdrakhmanova <inna.pogorelova05@gmail.com>
2025-01-24 19:09:44 +0100

    Rollback unnecessary change

--- ui/src/components/card.tsx
+++ ui/src/components/card.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 
 import { cn } from '../lib/utils';
 
-const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => {
-	return <div ref={ref} className={cn('bg-card text-card-foreground rounded-lg border-2', className)} {...props} />;
-});
+const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
+	<div ref={ref} className={cn('bg-card text-card-foreground rounded-lg border-2', className)} {...props} />
+));
 Card.displayName = 'Card';
 
 const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
