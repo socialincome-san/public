beae53d938ed2a2ec8e46ed4f8b5b0f308f50f44
Sarvesh Dwivedi <sarveshd@google.com>
2025-01-25 17:13:16 +0100

    More testing with sitemap

--- website/src/app/sitemap.ts
+++ website/src/app/sitemap.ts
@@ -1,41 +1,46 @@
 import { MetadataRoute } from 'next';
+import { mainWebsiteLanguages, websiteRegions } from '@/i18n';
 
 export const BASE_URL = process.env.BASE_URL || 'https://socialincome.org';
-
-// export default function sitemap(): MetadataRoute.Sitemap {
-// 	const pages = ['', 'about-us', 'our-work'];
-
-// 	return pages.flatMap((page) =>
-// 		mainWebsiteLanguages.flatMap((lang) =>
-// 			websiteRegions.map((region) => ({
-// 				url: `${BASE_URL}/${lang}/${region}/${page}`,
-// 				lastModified: new Date(),
-// 			})),
-// 		),
-// 	);
-// }
 
 export default function sitemap(): MetadataRoute.Sitemap {
 	const pages = ['', 'about-us', 'our-work'];
 
-	return [
-		{
-			url: 'https://socialincome.org',
-			lastModified: new Date(),
-			changeFrequency: 'yearly',
-			priority: 1,
-		},
-		{
-			url: 'https://socialincome.org/about',
-			lastModified: new Date(),
-			changeFrequency: 'monthly',
-			priority: 0.8,
-		},
-		{
-			url: 'https://socialincome.org/blog',
-			lastModified: new Date(),
-			changeFrequency: 'weekly',
-			priority: 0.5,
-		},
-	];
+	return pages.flatMap((page) => ({
+        url: `${BASE_URL}/en/ch/${page}`,
+        lastModified: new Date(),
+    }));
+	// return pages.flatMap((page) =>
+	// 	mainWebsiteLanguages.flatMap((lang) =>
+	// 		websiteRegions.map((region) => ({
+	// 			url: `${BASE_URL}/${lang}/${region}/${page}`,
+	// 			lastModified: new Date(),
+	// 		})),
+	// 	),
+	// );
 }
+
+// export default function sitemap(): MetadataRoute.Sitemap {
+// 	const pages = ['', 'about-us', 'our-work'];
+
+// 	return [
+// 		{
+// 			url: 'https://socialincome.org',
+// 			lastModified: new Date(),
+// 			changeFrequency: 'yearly',
+// 			priority: 1,
+// 		},
+// 		{
+// 			url: 'https://socialincome.org/about',
+// 			lastModified: new Date(),
+// 			changeFrequency: 'monthly',
+// 			priority: 0.8,
+// 		},
+// 		{
+// 			url: 'https://socialincome.org/blog',
+// 			lastModified: new Date(),
+// 			changeFrequency: 'weekly',
+// 			priority: 0.5,
+// 		},
+// 	];
+// }
