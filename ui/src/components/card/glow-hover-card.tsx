32904f5dd7d0539ff57d10284cf6a6b5cc4703d9
Inna Abdrakhmanova <inna.pogorelova05@gmail.com>
2025-01-25 16:57:56 +0100

    Fix types error

--- ui/src/components/card/glow-hover-card.tsx
+++ ui/src/components/card/glow-hover-card.tsx
@@ -6,7 +6,7 @@ import { cn } from '../../lib/utils';
 import { useGlowHover } from "../use-glow-hover";
 
 export const GlowHoverCard = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }) => {
-	const refCard = useGlowHover({ lightColor: '#CEFF00' });
+	const { refCard }  = useGlowHover({ lightColor: '#CEFF00' });
 
 	return (
 		<div ref={refCard} className={cn('bg-card text-card-foreground rounded-lg border-2', className)} {...props} />
