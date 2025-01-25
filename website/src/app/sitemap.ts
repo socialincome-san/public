e8c81daf7bb7fb2e64fd35e4860ebf87b2cc136f
Sarvesh Dwivedi <sarveshd@google.com>
2025-01-25 16:36:37 +0100

    Just testing a dummy hardcoded thing. since the other one is generating an empty sitemap as seen on vercel

--- website/src/app/sitemap.ts
+++ website/src/app/sitemap.ts
@@ -1,17 +1,41 @@
-import { mainWebsiteLanguages, websiteRegions } from '@/i18n';
 import { MetadataRoute } from 'next';
 
 export const BASE_URL = process.env.BASE_URL || 'https://socialincome.org';
 
+// export default function sitemap(): MetadataRoute.Sitemap {
+// 	const pages = ['', 'about-us', 'our-work'];
+
+// 	return pages.flatMap((page) =>
+// 		mainWebsiteLanguages.flatMap((lang) =>
+// 			websiteRegions.map((region) => ({
+// 				url: `${BASE_URL}/${lang}/${region}/${page}`,
+// 				lastModified: new Date(),
+// 			})),
+// 		),
+// 	);
+// }
+
 export default function sitemap(): MetadataRoute.Sitemap {
 	const pages = ['', 'about-us', 'our-work'];
 
-	return pages.flatMap((page) =>
-		mainWebsiteLanguages.flatMap((lang) =>
-			websiteRegions.map((region) => ({
-				url: `${BASE_URL}/${lang}/${region}/${page}`,
-				lastModified: new Date(),
-			})),
-		),
-	);
+	return [
+		{
+			url: 'https://socialincome.org',
+			lastModified: new Date(),
+			changeFrequency: 'yearly',
+			priority: 1,
+		},
+		{
+			url: 'https://socialincome.org/about',
+			lastModified: new Date(),
+			changeFrequency: 'monthly',
+			priority: 0.8,
+		},
+		{
+			url: 'https://socialincome.org/blog',
+			lastModified: new Date(),
+			changeFrequency: 'weekly',
+			priority: 0.5,
+		},
+	];
 }
