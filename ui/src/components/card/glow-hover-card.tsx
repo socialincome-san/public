393c0ca0f97a465d982c047f0dfc6f211791c9bc
IPogorelova <IPogorelova@users.noreply.github.com>
2025-01-25 16:09:32 +0000

    Prettified Code!

--- ui/src/components/card/glow-hover-card.tsx
+++ ui/src/components/card/glow-hover-card.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, {LegacyRef} from 'react';
+import React, { LegacyRef } from 'react';
 
 import { cn } from '../../lib/utils';
 import { useGlowHover } from '../use-glow-hover';
@@ -10,7 +10,11 @@ export const GlowHoverCard = React.forwardRef<HTMLDivElement, React.HTMLAttribut
 		const refCard = useGlowHover({ lightColor: '#CEFF00' });
 
 		return (
-			<div ref={refCard as LegacyRef<HTMLDivElement>} className={cn('bg-card text-card-foreground rounded-lg border-2', className)} {...props} />
+			<div
+				ref={refCard as LegacyRef<HTMLDivElement>}
+				className={cn('bg-card text-card-foreground rounded-lg border-2', className)}
+				{...props}
+			/>
 		);
 	},
 );
