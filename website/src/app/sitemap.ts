ad60297e2f33ff1efe7b2846f9ed597b96157a90
Sarvesh Dwivedi <sarveshd@google.com>
2025-01-25 15:34:47 +0100

    Prettified sitemap.ts

--- website/src/app/sitemap.ts
+++ website/src/app/sitemap.ts
@@ -1,6 +1,5 @@
-import { MetadataRoute } from 'next';
 import { mainWebsiteLanguages, websiteRegions } from '@/i18n';
-import { BA } from 'country-flag-icons/react/1x1';
+import { MetadataRoute } from 'next';
 // import { BASE_URL } from '@/app/lib/constants'
 
 export const BASE_URL = () => {
@@ -8,14 +7,14 @@ export const BASE_URL = () => {
 };
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const pages = [
-        '',
-        'about-us',
-        'our-work'
-    ]
+	const pages = ['', 'about-us', 'our-work'];
 
-    return pages.flatMap((page) => mainWebsiteLanguages.flatMap((lang) => websiteRegions.map((region) => ({
-        url: `${BASE_URL}/${lang}/${region}/${page}`,
-        lastModified: new Date(), 
-    }))))
+	return pages.flatMap((page) =>
+		mainWebsiteLanguages.flatMap((lang) =>
+			websiteRegions.map((region) => ({
+				url: `${BASE_URL}/${lang}/${region}/${page}`,
+				lastModified: new Date(),
+			})),
+		),
+	);
 }
