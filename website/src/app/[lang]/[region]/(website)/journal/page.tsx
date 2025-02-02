5d164af3f5091e01afe54998d956e699cb576105
Gavriil <gavriiltzortzakis@gmail.com>
2025-02-02 15:25:33 +0100

    Temporary overview page

--- website/src/app/[lang]/[region]/(website)/journal/page.tsx
+++ website/src/app/[lang]/[region]/(website)/journal/page.tsx
@@ -1,7 +1,21 @@
+import { StoryBlokArticle } from '@socialincome/shared/src/storyblok/article';
+import { Card, Typography } from '@socialincome/ui';
+import { getStoryblokApi } from '@storyblok/react';
+import Link from 'next/link';
+import type { ISbStoriesParams } from 'storyblok-js-client/src/interfaces';
+
 export const revalidate = 3600; // Update once an hour
 
 export default async function Page(props: { params: { lang: string } }) {
 	const lang = props.params.lang;
+	const params: ISbStoriesParams = {
+		resolve_relations: ['article.author', 'article.topics'],
+		language: lang,
+		with_tag: 'overview',
+	};
+	const { data } = await getStoryblokApi().get(`cdn/stories`, params);
+	console.log(JSON.stringify(data, null, 2));
+	const blogObject: { content: StoryBlokArticle; default_full_slug: string }[] = data.stories;
 
 	function getPublishedDateFormatted(date: string, lang: string) {
 		const dateObject = new Date(date);
@@ -9,5 +23,21 @@ export default async function Page(props: { params: { lang: string } }) {
 		return `${month} ${dateObject.getDate()}, ${dateObject.getFullYear()}`;
 	}
 
-	return <div className="blog w-full justify-center">OVERVIEW PAGE</div>;
+	return (
+		<div className="container mx-auto p-6">
+			<Typography size={'5xl'}>Blog Overview</Typography>
+			<div className="mt-5 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
+				{blogObject.map((blog) => (
+					<Card key={blog.content.title} className="overflow-hidden rounded-2xl bg-white shadow-lg">
+						<img src={blog.content.image.filename} alt={blog.content.image.alt} className="h-40 w-full object-cover" />
+						<div className="p-4">
+							<Typography>
+								<Link href={'/' + blog.default_full_slug}>{blog.content.title}</Link>
+							</Typography>
+						</div>
+					</Card>
+				))}
+			</div>
+		</div>
+	);
 }
