39a545594c016d1fe8e3a74ffa9748ae97137673
Gavriil <gavriiltzortzakis@gmail.com>
2025-02-04 23:27:43 +0100

    - Add translations - Not found - Extract component

--- website/src/app/[lang]/[region]/(website)/journal/page.tsx
+++ website/src/app/[lang]/[region]/(website)/journal/page.tsx
@@ -4,17 +4,24 @@ import { getStoryblokApi } from '@storyblok/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import type { ISbStoriesParams } from 'storyblok-js-client/src/interfaces';
+import StoryBlokAuthorImage from '@/app/[lang]/[region]/(website)/journal/StoryBlokAuthorImage';
+import { Translator } from '@socialincome/shared/src/utils/i18n';
+import { LanguageCode } from '@socialincome/shared/src/types/language';
 
 export const revalidate = 3600; // Update once an hour
 
-export default async function Page(props: { params: { lang: string; region: string } }) {
+export default async function Page(props: { params: { lang: LanguageCode; region: string } }) {
 	const lang = props.params.lang;
 	const region = props.params.region;
 	const params: ISbStoriesParams = {
 		resolve_relations: ['article.author', 'article.topics'],
 		language: lang,
-		with_tag: 'overview',
+		with_tag: 'overview'
 	};
+	const translator = await Translator.getInstance({
+		language: lang,
+		namespaces: ['website-journal']
+	});
 	const { data } = await getStoryblokApi().get(`cdn/stories`, params);
 	const blogObject: { content: StoryBlokArticle; default_full_slug: string; published_at: string }[] = data.stories;
 
@@ -23,12 +30,16 @@ export default async function Page(props: { params: { lang: string; region: stri
 		return dateObject.toLocaleDateString();
 	}
 
+	function getHref(blog: { content: StoryBlokArticle; default_full_slug: string; published_at: string }) {
+		return `/${lang}/${region}/${blog.default_full_slug}`;
+	}
+
 	return (
 		<BaseContainer>
-			<Typography size={'xl'}>Journal Overview</Typography>
+			<Typography size={'xl'}>{translator.t('overview.title')}</Typography>
 			<div className="mt-5 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
 				{blogObject.map((blog) => (
-					<Link href={'/' + lang + '/' + region + '/' + blog.default_full_slug} key={blog.content.title}>
+					<Link href={getHref(blog)} key={blog.content.title}>
 						<Card className="overflow-hidden transition-transform duration-300 hover:scale-105">
 							<Image
 								src={blog.content.image.filename}
@@ -38,18 +49,12 @@ export default async function Page(props: { params: { lang: string; region: stri
 								className="h-48 w-full object-cover"
 							/>
 							<CardContent className="flex flex-grow flex-col p-6">
-								<Typography className="mb-4 line-clamp-2 h-14 flex-grow text-xl font-medium">
+								<Typography size={'xl'} className="mb-4 line-clamp-2 h-14 flex-grow font-medium">
 									{blog.content.title}
 								</Typography>
 								<div className="mt-auto flex items-center justify-between">
 									<div className="flex items-center space-x-2">
-										<Image
-											src={blog.content.author.content.avatar.filename}
-											alt="author"
-											className="w-12 h-12 flex-none rounded-full object-cover object-top"
-											width={100}
-											height={100}
-										/>
+										<StoryBlokAuthorImage author={blog.content.author} />
 										<Typography>{blog.content.author.content.fullName}</Typography>
 									</div>
 									<Typography>{getPublishedDateFormatted(blog.published_at)}</Typography>
