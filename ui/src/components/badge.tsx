7393b532a57507a528566317596a7bcdf63aa804
Gavriil <gavriiltzortzakis@gmail.com>
2025-02-07 23:39:00 +0100

    styling

--- ui/src/components/badge.tsx
+++ ui/src/components/badge.tsx
@@ -15,8 +15,7 @@ const badgeVariants = cva(
 				muted: 'border-muted-foreground text-muted-foreground',
 				accent: 'border-transparent bg-accent text-accent-foreground',
 				outline: 'text-primary border-primary',
-				foreground:
-					'border-transparent bg-primary-foreground-muted text-primary focus:ring-2 focus:ring-ring focus:ring-offset-2',
+				white: 'border-white bg-transparent text-white',
 				interactive:
 					'border-transparent bg-primary bg-opacity-10 text-primary hover:bg-opacity-100 hover:text-white focus:ring-2 focus:ring-ring focus:ring-offset-2',
 				'interactive-accent':
