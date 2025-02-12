cfd29bd2027c840b8648d6f86d0a4d8f2a9c36f4
Inna Abdrakhmanova <innaspbsu@gmail.com>
2025-02-12 10:31:41 +0100

    Add glow hover effect to the main page (#1030)

---
+++ ui/src/components/card/glow-hover-card.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import React, { LegacyRef } from 'react';
+
+import { cn } from '../../lib/utils';
+import { useGlowHover } from '../use-glow-hover';
+
+export const GlowHoverCard = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+	({ className, ...props }) => {
+		const refCard = useGlowHover({ lightColor: '#CEFF00' });
+
+		return (
+			<div
+				ref={refCard as LegacyRef<HTMLDivElement>}
+				className={cn('bg-card text-card-foreground rounded-lg border-2', className)}
+				{...props}
+			/>
+		);
+	},
+);
+GlowHoverCard.displayName = 'GlowHoverCard';
