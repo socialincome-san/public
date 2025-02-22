2f7e861b5bbad14fdf5cfbf20eba1fc47ff307e0
Michael Kündig <michael@socialincome.org>
2025-02-22 13:26:01 +0700

    Website: Refactor login page by removing unused alert components

--- website/src/app/[lang]/[region]/(website)/login/page.tsx
+++ website/src/app/[lang]/[region]/(website)/login/page.tsx
@@ -3,19 +3,8 @@ import LoginForm from '@/app/[lang]/[region]/(website)/login/login-form';
 import RequestPasswordResetDialog from '@/app/[lang]/[region]/(website)/login/request-password-reset-dialog';
 import { SocialSignInButtons } from '@/app/[lang]/[region]/(website)/login/social-sign-in-buttons';
 import { getMetadata } from '@/metadata';
-import { InformationCircleIcon } from '@heroicons/react/24/outline';
 import { Translator } from '@socialincome/shared/src/utils/i18n';
-import {
-	Alert,
-	AlertDescription,
-	AlertTitle,
-	BaseContainer,
-	Tooltip,
-	TooltipContent,
-	TooltipProvider,
-	TooltipTrigger,
-	Typography,
-} from '@socialincome/ui';
+import { BaseContainer } from '@socialincome/ui';
 
 export async function generateMetadata({ params }: DefaultLayoutProps) {
 	return getMetadata(params.lang, 'website-login');
@@ -26,22 +15,6 @@ export default async function Page({ params }: DefaultPageProps) {
 
 	return (
 		<BaseContainer className="min-h-screen-navbar mx-auto flex max-w-lg flex-col">
-			<Alert variant="accent" className="mb-8">
-				<AlertTitle className="flex-inline flex">
-					{translator.t('alert.title')}
-					<TooltipProvider delayDuration={100}>
-						<Tooltip>
-							<TooltipTrigger>
-								<InformationCircleIcon className="ml-1 h-5 w-5" />
-							</TooltipTrigger>
-							<TooltipContent className="max-w-md">{translator.t('alert.tooltip')}</TooltipContent>
-						</Tooltip>
-					</TooltipProvider>
-				</AlertTitle>
-				<AlertDescription>
-					<Typography>{translator.t('alert.text')}</Typography>
-				</AlertDescription>
-			</Alert>
 			<LoginForm
 				lang={params.lang}
 				region={params.region}
