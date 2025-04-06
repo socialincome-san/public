91f03bcbef413af53e922ba8ede51d6fa07dbcdf
Sandino Scheidegger <sandino@socialincome.org>
2025-04-06 21:23:22 +0200

    Added more padding below and above, no padding to the left. Text size same as p for author. Plus possibility to add organisation or function of author

--- ui/src/components/typography/quotedText.tsx
+++ ui/src/components/typography/quotedText.tsx
@@ -1,14 +1,19 @@
 import { Typography } from './typography';
 
-export function QuotedText({ text, author }: { text: string; author: string }) {
+export function QuotedText({ text, author, author_org }: { text: string; author: string; author_org: string }) {
 	return (
-		<div className="w-full p-2">
+		<div className="w-full py-16 px-0">
 			<Typography className="m-0 p-0" weight="bold" color="foreground" size="3xl">
 				« {text} »
 			</Typography>
-			<Typography color="foreground" size="xs" className="mt-1 p-0">
-				{author}
-			</Typography>
+			<div className="mt-4">
+				<Typography color="foreground" size="md" className="p-0 m-0">
+					{author}
+				</Typography>
+				<Typography color="foreground" size="md" className="p-0 m-0">
+					{author_org}
+				</Typography>
+			</div>
 		</div>
 	);
 }
