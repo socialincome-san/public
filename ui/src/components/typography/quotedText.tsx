81de17f60da3c3c7378bcf61e374d0bc2594aecd
ssandino <ssandino@users.noreply.github.com>
2025-04-06 19:26:40 +0000

    Prettified Code!

--- ui/src/components/typography/quotedText.tsx
+++ ui/src/components/typography/quotedText.tsx
@@ -2,15 +2,15 @@ import { Typography } from './typography';
 
 export function QuotedText({ text, author, author_org }: { text: string; author: string; author_org: string }) {
 	return (
-		<div className="w-full py-16 px-0">
+		<div className="w-full px-0 py-16">
 			<Typography className="m-0 p-0" weight="bold" color="foreground" size="3xl">
 				« {text} »
 			</Typography>
 			<div className="mt-4">
-				<Typography color="foreground" size="md" className="p-0 m-0">
+				<Typography color="foreground" size="md" className="m-0 p-0">
 					{author}
 				</Typography>
-				<Typography color="foreground" size="md" className="p-0 m-0">
+				<Typography color="foreground" size="md" className="m-0 p-0">
 					{author_org}
 				</Typography>
 			</div>
