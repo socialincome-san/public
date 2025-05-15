a06490d62b96c6606abc844ab5f19b739138b1fb
Raphi <raphael@smartive.ch>
2025-05-15 16:50:05 +0200

    remove prerender

--- portal/src/app/page.tsx
+++ portal/src/app/page.tsx
@@ -1,18 +1,16 @@
-import { prisma } from '@/server/prisma';
-
 export default async function Home() {
-	const users = await prisma.user.findMany()
+	// const users = await prisma.user.findMany()
 
 	return (
 		<div>
 			<h1>Users</h1>
-			<ul>
-				{users.map((user) => (
-					<li key={user.id}>
-						{user.firstName} {user.lastName}
-					</li>
-				))}
-			</ul>
+			{/*<ul>*/}
+			{/*	{users.map((user) => (*/}
+			{/*		<li key={user.id}>*/}
+			{/*			{user.firstName} {user.lastName}*/}
+			{/*		</li>*/}
+			{/*	))}*/}
+			{/*</ul>*/}
 		</div>
 	)
 }
