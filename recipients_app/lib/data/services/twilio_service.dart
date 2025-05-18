31638049dd63bbba1daee46ff7729dc6a5dc6fb9
Karin Berg <karin@karinberg.de>
2025-05-18 19:53:41 +0200

    Disabled "initializeVerificationService" because it creates every time a new verification service in Twilio

--- recipients_app/lib/data/services/twilio_service.dart
+++ recipients_app/lib/data/services/twilio_service.dart
@@ -2,33 +2,33 @@ import "package:twilio_flutter/twilio_flutter.dart";
 
 class TwilioService {
   late final TwilioFlutter _twilioFlutter;
-  String? _verificationServiceId;
+  final _verificationServiceId = "VA7912dd3224b9a672c60fae4297dfee64"; // Social Income Staging V2
 
   TwilioService({required String accountSid, required String authToken, required String twilioNumber}) {
     _twilioFlutter = TwilioFlutter(accountSid: accountSid, authToken: authToken, twilioNumber: twilioNumber);
   }
 
-  Future<bool> initializeVerificationService() async {
-    try {
-      final response = await _twilioFlutter.createVerificationService(serviceName: "SMS Authentication Service");
-      if (response.responseState == ResponseState.SUCCESS) {
-        _verificationServiceId = response.metadata?["sid"] as String?;
-        return true;
-      }
-      return false;
-    } catch (e) {
-      return false;
-    }
-  }
+  // Future<bool> initializeVerificationService() async {
+  //   try {
+  //     final response = await _twilioFlutter.createVerificationService(serviceName: "Social Income Staging V2");
+  //     if (response.responseState == ResponseState.SUCCESS) {
+  //       _verificationServiceId = response.metadata?["sid"] as String?;
+  //       return true;
+  //     }
+  //     return false;
+  //   } catch (e) {
+  //     return false;
+  //   }
+  // }
 
   Future<TwilioResponse> sendVerificationCode(String phoneNumber) async {
-    if (_verificationServiceId == null) {
-      final initialized = await initializeVerificationService();
-      if (!initialized) throw Exception("Failed to initialize verification service");
-    }
+    // if (_verificationServiceId == null) {
+    //   final initialized = await initializeVerificationService();
+    //   if (!initialized) throw Exception("Failed to initialize verification service");
+    // }
 
     final response = await _twilioFlutter.sendVerificationCode(
-      verificationServiceId: _verificationServiceId!,
+      verificationServiceId: _verificationServiceId,
       recipient: phoneNumber,
       verificationChannel: VerificationChannel.SMS,
     );
