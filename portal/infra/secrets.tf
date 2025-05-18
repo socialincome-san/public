c1a1c8146a8bb000c884041ab503e5bef306b7a7
Raphi <raphael@smartive.ch>
2025-05-18 22:51:20 +0200

    refactor secret management and update portal build process

--- portal/infra/secrets.tf
+++ portal/infra/secrets.tf
@@ -1,4 +1,4 @@
 data "google_secret_manager_secret_version" "google_secret_manager_secret_version" {
-  secret  = var.db_password_secret_name
+  secret  = "${var.env}_${var.app_name}_google_secret_manager_secret_version"
   version = "latest"
 }
--- portal/infra/variables.tf
+++ portal/infra/variables.tf
@@ -23,11 +23,6 @@ variable "docker_image_url" {
   type        = string
 }
 
-variable "db_password_secret_name" {
-  description = "Secret Manager name for DB password"
-  type        = string
-}
-
 variable "domain_name" {
   description = "Custom domain for Cloud Run"
   type        = string
