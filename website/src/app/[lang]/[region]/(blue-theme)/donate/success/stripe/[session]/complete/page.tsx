f42ca7e4fc527548c9c03bc2854f32cd2e322338
Marco Bohler <marco@smartive.ch>
2025-05-21 17:41:01 +0200

    add completion step for stripe payments

---
+++ website/src/app/[lang]/[region]/(blue-theme)/donate/success/stripe/[session]/complete/page.tsx
@@ -0,0 +1,31 @@
+import { StripeSuccessPageProps } from '@/app/[lang]/[region]/(blue-theme)/donate/success/stripe/[session]/page';
+import { Translator } from '@socialincome/shared/src/utils/i18n';
+import { Card, Typography } from '@socialincome/ui';
+import SignIn from './signin';
+
+export default async function Page({ params }: StripeSuccessPageProps) {
+	const { lang, region, session } = await params;
+
+	const translator = await Translator.getInstance({ language: lang, namespaces: 'website-donate' });
+	const onSuccessURL = `/${lang}/${region}/me/personal-info`;
+
+	return (
+		<div className="mx-auto flex max-w-3xl flex-col space-y-8">
+			<Typography size="4xl" color="accent" weight="bold">
+				{translator.t('success.complete.title')}
+			</Typography>
+			<Card className="p-4">
+				<Typography>{translator.t('success.complete.message')}</Typography>
+				<SignIn
+					lang={lang}
+					onSuccessURL={onSuccessURL}
+					checkoutSessionId={session}
+					translations={{
+						updateUserError: translator.t('success.complete.updateUserError'),
+						redirecting: translator.t('success.complete.redirecting'),
+					}}
+				/>
+			</Card>
+		</div>
+	);
+}
