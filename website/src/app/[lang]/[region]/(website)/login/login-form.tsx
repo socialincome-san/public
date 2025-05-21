e1b50738b0dd1c42dc5fb9e27edcd34efa9184ae
Marco Bohler <marco@smartive.ch>
2025-05-21 11:42:24 +0200

    add email authentication

--- website/src/app/[lang]/[region]/(website)/login/login-form.tsx
+++ website/src/app/[lang]/[region]/(website)/login/login-form.tsx
@@ -1,64 +1,82 @@
 'use client';
 
 import { DefaultParams } from '@/app/[lang]/[region]';
+import { useEmailAuthentication } from '@/hooks/useEmailAuthentication';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button, Form, FormControl, FormField, FormItem, FormMessage, Input, Typography } from '@socialincome/ui';
-import { FirebaseError } from 'firebase/app';
-import { browserSessionPersistence, signInWithEmailAndPassword } from 'firebase/auth';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { Link } from '@mui/material';
+import {
+	Button,
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormMessage,
+	Input,
+	Typography,
+} from '@socialincome/ui';
 import { useForm } from 'react-hook-form';
-import toast from 'react-hot-toast';
-import { useAuth } from 'reactfire';
 import * as z from 'zod';
 
 type LoginFormProps = {
 	translations: {
 		title: string;
 		email: string;
-		password: string;
-		forgotPassword: string;
 		submitButton: string;
-
-		// Errors
+		checkEmail: string;
 		invalidEmail: string;
-		invalidUserOrPassword: string;
+		confirmEmail: string;
+		confirmEmailTitle: string;
+		backToLogin: string;
 	};
 } & DefaultParams;
 
 export default function LoginForm({ lang, region, translations }: LoginFormProps) {
-	const router = useRouter();
-	const auth = useAuth();
-	const [submitting, setSubmitting] = useState(false);
+	const { signIn, sendLinkEmail, loading, emailSent, isSignIn } = useEmailAuthentication({
+		lang,
+		region,
+		translations,
+	});
 
 	const formSchema = z.object({
 		email: z.string().email({ message: translations.invalidEmail }),
-		password: z.string(),
 	});
 	type FormSchema = z.infer<typeof formSchema>;
 
 	const form = useForm<FormSchema>({
 		resolver: zodResolver(formSchema),
-		defaultValues: { email: '', password: '' },
+		defaultValues: { email: '' },
 	});
 
-	const onSubmit = async (values: FormSchema) => {
-		setSubmitting(true);
-		await auth.setPersistence(browserSessionPersistence);
-		await signInWithEmailAndPassword(auth, values.email, values.password)
-			.then(() => {
-				router.push(`/${lang}/${region}/me`);
-			})
-			.catch((error: FirebaseError) => {
-				setSubmitting(false);
-				if (error.code === 'auth/wrong-password' || error.code === 'auth/user-not-found')
-					toast.error(translations.invalidUserOrPassword);
-			});
-	};
+	if (emailSent) {
+		return (
+			<div className="flex flex-col items-center justify-center">
+				<Card className="theme-default bg-white">
+					<CardHeader>
+						<Typography weight="bold" size="2xl">
+							{translations.confirmEmailTitle}
+						</Typography>
+					</CardHeader>
+					<CardContent>
+						<Typography>{translations.checkEmail}</Typography>
+					</CardContent>
+					<CardFooter>
+						<Link href={`/${lang}/${region}/login`}>{translations.backToLogin}</Link>
+					</CardFooter>
+				</Card>
+			</div>
+		);
+	}
 
 	return (
 		<Form {...form}>
-			<form className="flex flex-col space-y-2 text-center" onSubmit={form.handleSubmit(onSubmit)}>
+			<form
+				className="flex flex-col space-y-2 text-center"
+				onSubmit={form.handleSubmit((values) => (isSignIn ? signIn(values.email) : sendLinkEmail(values.email)))}
+			>
 				<Typography weight="bold" size="2xl" className="mb-4">
 					{translations.title}
 				</Typography>
@@ -74,19 +92,7 @@ export default function LoginForm({ lang, region, translations }: LoginFormProps
 						</FormItem>
 					)}
 				/>
-				<FormField
-					control={form.control}
-					name="password"
-					render={({ field }) => (
-						<FormItem>
-							<FormControl>
-								<Input type="password" placeholder={translations.password} {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<Button type="submit" className="mt-8" showLoadingSpinner={submitting}>
+				<Button type="submit" className="mt-8" showLoadingSpinner={loading}>
 					{translations.submitButton}
 				</Button>
 			</form>
