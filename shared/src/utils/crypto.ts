510ff91aca1297d5648298c7d8179ee0141ee1a9
Marco Bohler <marco@smartive.ch>
2025-05-22 21:01:05 +0200

    make rndString browser compatible

--- shared/src/utils/crypto.ts
+++ shared/src/utils/crypto.ts
@@ -1,16 +1,13 @@
-export const generateRandomString = (length: number = 16) => {
-	const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}|;:,.<>?';
-	const charsetLength = charset.length;
-	const password = [];
+import crypto from 'crypto';
 
-	const cryptoObj = typeof window !== 'undefined' && window.crypto ? window.crypto : require('crypto').webcrypto;
-
-	const randomValues = new Uint8Array(length);
-	cryptoObj.getRandomValues(randomValues);
-
-	for (let i = 0; i < length; i++) {
-		password.push(charset[randomValues[i] % charsetLength]);
+export const rndString = async (bytes: number, encoding: BufferEncoding = 'base64url'): Promise<string> => {
+	if (typeof window === 'undefined') {
+		// Node.js environment
+		return crypto.randomBytes(bytes).toString(encoding);
+	} else {
+		// Browser environment
+		const buffer = new Uint8Array(bytes);
+		window.crypto.getRandomValues(buffer);
+		return Buffer.from(buffer).toString(encoding);
 	}
-
-	return password.join('');
 };
