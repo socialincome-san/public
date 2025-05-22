02905386ae1316b8f43e3ac6fb8abb83d93de291
Marco Bohler <marco@smartive.ch>
2025-05-22 21:09:14 +0200

    handle buffer encoding

--- shared/src/utils/crypto.ts
+++ shared/src/utils/crypto.ts
@@ -2,12 +2,20 @@ import crypto from 'crypto';
 
 export const rndString = async (bytes: number, encoding: BufferEncoding = 'base64url'): Promise<string> => {
 	if (typeof window === 'undefined') {
-		// Node.js environment
+		// Node.js
 		return crypto.randomBytes(bytes).toString(encoding);
 	} else {
-		// Browser environment
+		// Browser
 		const buffer = new Uint8Array(bytes);
 		window.crypto.getRandomValues(buffer);
-		return Buffer.from(buffer).toString(encoding);
+
+		let str = Buffer.from(buffer).toString(encoding === 'base64url' ? 'base64' : encoding);
+
+		if (encoding === 'base64url') {
+			// Convert base64 to base64url
+			str = str.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+		}
+
+		return str;
 	}
 };
