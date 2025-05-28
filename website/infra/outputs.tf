14de2d6f626c20305651de687052fd0ac136f34e
Raphi <raphael@smartive.ch>
2025-05-28 16:33:59 +0200

    feat: add TF_LOG environment variable for improved Terraform logging

--- website/infra/outputs.tf
+++ website/infra/outputs.tf
@@ -1,4 +1,4 @@
 output "cloud_run_url" {
   description = "Public URL of the deployed Cloud Run service"
-  value = try(google_cloud_run_service.google_cloud_run_service.status[0].url, "")
+  value       = try(google_cloud_run_service.google_cloud_run_service.status[0].url, "")
 }
