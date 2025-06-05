c5a2828cb70d3f01b7dd1d0bc9ea5be1b266fc3b
Marco Bohler <triggertoo@gmail.com>
2025-06-05 11:49:44 +0200

    Website: Switch to email magic link authentication (#1113)

--- website/src/app/[lang]/[region]/(website)/login/login-form.tsx
+++ website/src/app/[lang]/[region]/(website)/login/login-form.tsx
@@ -1,64 +1,90 @@
 'use client';
 
 import { DefaultParams } from '@/app/[lang]/[region]';
+import { useEmailLogin } from '@/hooks/useEmailLogin';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button, Form, FormControl, FormField, FormItem, FormMessage, Input, Typography } from '@socialincome/ui';
-import { FirebaseError } from 'firebase/app';
-import { browserSessionPersistence, signInWithEmailAndPassword } from 'firebase/auth';
+import { Link } from '@mui/material';
+import {
+	Button,
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormMessage,
+	Input,
+	Typography,
+} from '@socialincome/ui';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import toast from 'react-hot-toast';
-import { useAuth } from 'reactfire';
 import * as z from 'zod';
 
 type LoginFormProps = {
 	translations: {
 		title: string;
 		email: string;
-		password: string;
-		forgotPassword: string;
 		submitButton: string;
-
-		// Errors
+		checkEmail: string;
 		invalidEmail: string;
-		invalidUserOrPassword: string;
+		confirmEmail: string;
+		confirmEmailTitle: string;
+		backToLogin: string;
+		signingIn: string;
 	};
 } & DefaultParams;
 
 export default function LoginForm({ lang, region, translations }: LoginFormProps) {
 	const router = useRouter();
-	const auth = useAuth();
-	const [submitting, setSubmitting] = useState(false);
+	const { sendSignInEmail, sendingEmail, signingIn, emailSent } = useEmailLogin({
+		lang,
+		onLoginSuccess: async () => {
+			router.push(`/${lang}/${region}/me`);
+		},
+	});
 
 	const formSchema = z.object({
 		email: z.string().email({ message: translations.invalidEmail }),
-		password: z.string(),
 	});
 	type FormSchema = z.infer<typeof formSchema>;
 
 	const form = useForm<FormSchema>({
 		resolver: zodResolver(formSchema),
-		defaultValues: { email: '', password: '' },
+		defaultValues: { email: '' },
 	});
 
-	const onSubmit = async (values: FormSchema) => {
-		setSubmitting(true);
-		await auth.setPersistence(browserSessionPersistence);
-		await signInWithEmailAndPassword(auth, values.email, values.password)
-			.then(() => {
-				router.push(`/${lang}/${region}/me`);
-			})
-			.catch((error: FirebaseError) => {
-				setSubmitting(false);
-				if (error.code === 'auth/wrong-password' || error.code === 'auth/user-not-found')
-					toast.error(translations.invalidUserOrPassword);
-			});
-	};
+	if (signingIn) {
+		return <div>{translations.signingIn}</div>;
+	}
+
+	if (emailSent) {
+		return (
+			<div className="flex flex-col items-center justify-center">
+				<Card className="theme-default bg-white">
+					<CardHeader>
+						<Typography weight="bold" size="2xl">
+							{translations.confirmEmailTitle}
+						</Typography>
+					</CardHeader>
+					<CardContent>
+						<Typography>{translations.checkEmail}</Typography>
+					</CardContent>
+					<CardFooter>
+						<Link href={`/${lang}/${region}/login`}>{translations.backToLogin}</Link>
+					</CardFooter>
+				</Card>
+			</div>
+		);
+	}
 
 	return (
 		<Form {...form}>
-			<form className="flex flex-col space-y-2 text-center" onSubmit={form.handleSubmit(onSubmit)}>
+			<form
+				className="flex flex-col space-y-2 text-center"
+				onSubmit={form.handleSubmit((values) => sendSignInEmail(values.email))}
+			>
 				<Typography weight="bold" size="2xl" className="mb-4">
 					{translations.title}
 				</Typography>
@@ -74,19 +100,7 @@ export default function LoginForm({ lang, region, translations }: LoginFormProps
 						</FormItem>
 					)}
 				/>
-				<FormField
-					control={form.control}
-					name="password"
-					render={({ field }) => (
-						<FormItem>
-							<FormControl>
-								<Input type="password" placeholder={translations.password} {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<Button type="submit" className="mt-8" showLoadingSpinner={submitting}>
+				<Button type="submit" className="mt-8" showLoadingSpinner={sendingEmail}>
 					{translations.submitButton}
 				</Button>
 			</form>
