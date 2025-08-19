6009804facd6163bd4b02fc5198b9cddf49899de
Cheryl Oberholzer <cheryl@smartive.ch>
2025-08-19 09:42:48 +0200

    feat: adapt breadcrumb

--- website/src/app/portal/components/pro-blocks/page-headers/page-header-4.tsx
+++ website/src/app/portal/components/pro-blocks/page-headers/page-header-4.tsx
@@ -5,26 +5,35 @@ import {
 	BreadcrumbItem,
 	BreadcrumbLink,
 	BreadcrumbList,
-	BreadcrumbPage,
 	BreadcrumbSeparator,
 } from '@/app/portal/components/ui/breadcrumb';
 
+import { usePathname } from 'next/navigation';
+
 export function PageHeader4() {
+	const pathname = usePathname();
+	const segments = pathname.split('/').filter(Boolean);
+
 	return (
 		<div className="container mx-auto flex flex-col py-9">
 			<Breadcrumb>
 				<BreadcrumbList>
 					<BreadcrumbItem>
-						<BreadcrumbLink>Home</BreadcrumbLink>
+						<BreadcrumbLink href="/">Home</BreadcrumbLink>
 					</BreadcrumbItem>
-					<BreadcrumbSeparator />
-					<BreadcrumbItem>
-						<BreadcrumbLink>Settings</BreadcrumbLink>
-					</BreadcrumbItem>
-					<BreadcrumbSeparator />
-					<BreadcrumbItem>
-						<BreadcrumbPage>Profile details</BreadcrumbPage>
-					</BreadcrumbItem>
+					{segments.map((segment, index) => {
+						const href = '/' + segments.slice(0, index + 1).join('/');
+						const isLast = index === segments.length - 1;
+
+						return (
+							<>
+								<BreadcrumbSeparator />
+								<BreadcrumbItem key={href}>
+									{isLast ? <span>{segment}</span> : <BreadcrumbLink href={href}>{segment}</BreadcrumbLink>}
+								</BreadcrumbItem>
+							</>
+						);
+					})}
 				</BreadcrumbList>
 			</Breadcrumb>
 		</div>
