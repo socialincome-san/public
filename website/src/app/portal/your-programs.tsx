540b81893dbb4902df91b366b3ba92ec766a59ed
Cheryl Oberholzer <cheryl@smartive.ch>
2025-08-21 14:24:47 +0200

    feat: remove duplicated content

--- website/src/app/portal/your-programs.tsx
+++ website/src/app/portal/your-programs.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardTitle } from '@/app/portal/components/card';
+import { CardContent, CardTitle } from '@/app/portal/components/card';
 import { Wallet } from '@/app/portal/components/wallet';
 import { getAuthenticatedUserOrRedirect } from '@/lib/firebase/current-user';
 import { ProgramService } from '@socialincome/shared/src/database/services/program/program.service';
@@ -24,25 +24,6 @@ export async function YourPrograms() {
 		<div>
 			<h2 className="py-6 text-3xl font-medium">Your programs</h2>
 			<div className="grid gap-8" style={{ gridTemplateColumns: 'repeat(auto-fill, minmax(20rem, 1fr))' }}>
-				<Card>
-					<div className="flex h-full w-full flex-col items-start justify-between pb-6 pl-8 pr-8 pt-20">
-						<div className="flex flex-col items-start">
-							<h2 className="text-4xl font-normal">Old Widows</h2>
-							<p className="text-sm font-medium tracking-wide">Sierra Leone</p>
-						</div>
-						<div className="flex w-full items-start justify-between">
-							<div className="flex flex-col items-start">
-								<p className="text-sm font-medium tracking-wide">Paid out</p>
-								<p className="text-4xl font-normal">USD 7,350</p>
-							</div>
-							<div className="flex flex-col items-end">
-								<p className="text-sm font-medium tracking-wide">Recipients</p>
-								<p className="text-4xl font-normal">132</p>
-							</div>
-						</div>
-					</div>
-				</Card>
-
 				{programs.map((program) => (
 					<Link href={`/portal/programs/${program.id}/overview`} key={program.id}>
 						<Wallet key={program.id}>
@@ -71,14 +52,7 @@ export async function YourPrograms() {
 						</Wallet>
 					</Link>
 				))}
-				{programs.map((program) => (
-					<Wallet key={program.id}>
-						<CardContent>
-							<Link href={`/portal/programs/${program.id}/overview`}>{program.name}</Link> (
-							{program.programPermission === 'viewer' ? 'read-only' : 'editable'})
-						</CardContent>
-					</Wallet>
-				))}
+
 				<Wallet variant="empty">
 					<CardContent>
 						<CardTitle>Create new program</CardTitle>
