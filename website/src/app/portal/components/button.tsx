d323cb2f14756c2226b02943a4d11cb697984a9f
Cheryl Oberholzer <cheryl@smartive.ch>
2025-08-22 09:13:57 +0200

    feat: refine primary button hover state

--- website/src/app/portal/components/button.tsx
+++ website/src/app/portal/components/button.tsx
@@ -4,12 +4,12 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import * as React from 'react';
 
 const buttonVariants = cva(
-	'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+	'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0  z-10',
 	{
 		variants: {
 			variant: {
 				default:
-					'bg-gradient-to-r from-[hsl(var(--gradient-button-from))] to-[hsl(var(--gradient-button-to))] text-primary-foreground shadow hover:bg-primary/90',
+					'text-primary-foreground shadow bg-primary/90 relative after:bg-gradient-to-r after:from-[hsl(var(--gradient-button-from))] after:to-[hsl(var(--gradient-button-to))] after:inset-0 after:-z-10 after:rounded-full after:absolute after:opacity-100 hover:after:opacity-0 after:transition-opacity ',
 				destructive: 'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
 				outline: 'border border-input hover:bg-accent hover:text-accent-foreground bg-background/5 ',
 				secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
