05f8dd0c74492eeb8d00cf41963f7e1619d395a2
Cheryl Oberholzer <cheryl@smartive.ch>
2025-08-22 09:44:25 +0200

    feat: add confirmed button

--- website/src/app/portal/components/button.tsx
+++ website/src/app/portal/components/button.tsx
@@ -4,17 +4,22 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import * as React from 'react';
 
 const buttonVariants = cva(
-	'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0  z-10',
+	'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 z-10',
 	{
 		variants: {
 			variant: {
-				default:
-					'text-primary-foreground shadow bg-primary/90 relative after:bg-gradient-to-r after:from-[hsl(var(--gradient-button-from))] after:to-[hsl(var(--gradient-button-to))] after:inset-0 after:-z-10 after:rounded-full after:absolute after:opacity-100 hover:after:opacity-0 after:transition-opacity ',
+				default: [
+					'text-primary-foreground shadow bg-primary/90 relative',
+					// Use a pseudo element to allow the gradient transition effect
+					'after:bg-gradient-to-r after:from-[hsl(var(--gradient-button-from))] after:to-[hsl(var(--gradient-button-to))] after:inset-0 after:-z-10 after:rounded-full after:absolute after:opacity-100 hover:after:opacity-0 focus:after:opacity-0 after:transition-opacity',
+				].join(' '),
 				destructive: 'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
 				outline: 'border border-input hover:bg-accent hover:text-accent-foreground bg-background/5 ',
 				secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
 				ghost: 'hover:bg-accent hover:text-accent-foreground',
 				link: 'text-primary underline-offset-4 hover:underline',
+				confirmed:
+					'bg-[hsl(var(--confirm))] text-[hsl(var(--confirm-foreground))] shadow-sm hover:bg-[hsl(var(--confirm)/0.9)]',
 			},
 			size: {
 				default: 'h-9 px-4 py-2',
