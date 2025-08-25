47d93eae94869da3e4f2089c5847bb695e7d5e40
Cheryl Oberholzer <cheryl@smartive.ch>
2025-08-25 13:39:53 +0200

    feat: remove unneeded breadcrumb

--- website/src/app/portal/page.tsx
+++ website/src/app/portal/page.tsx
@@ -1,15 +1,11 @@
-import { Breadcrumb } from '@/app/portal/components/breadcrumb/breadcrumb';
 import { YourPrograms } from '@/app/portal/your-programs';
 import { getAuthenticatedUserOrRedirect } from '@/lib/firebase/current-user';
 
 export default async function PortalPage() {
 	const user = await getAuthenticatedUserOrRedirect();
-	const breadcrumbLinks = [{ href: '/portal', label: 'Home' }];
 
 	return (
 		<>
-			<Breadcrumb links={breadcrumbLinks} />
-
 			<div className="flex flex-wrap items-center gap-4 md:flex-row md:items-center">
 				<h1 className="py-8 text-5xl">Welcome back, {user.firstName} 👋</h1>
 			</div>
