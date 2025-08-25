47c420bd583e40472c3a15982e89bd3dd2a45051
Raphi <raphael@smartive.ch>
2025-08-25 14:43:24 +0200

    fix lint

--- website/src/app/portal/not-found.tsx
+++ website/src/app/portal/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
 			<div className="w-full space-y-6 text-center">
 				<div className="space-y-3">
 					<h1 className="text-4xl font-bold tracking-tighter transition-transform hover:scale-110 sm:text-5xl">404</h1>
-					<p className="text-gray-500">Looks like you've ventured into the unknown digital realm.</p>
+					<p className="text-gray-500">Looks like you&#39;ve ventured into the unknown digital realm.</p>
 				</div>
 				<Button asChild>
 					<Link href="/portal" prefetch={false}>
