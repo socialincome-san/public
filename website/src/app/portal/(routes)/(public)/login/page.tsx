913c5f7ba375d0f64a733da8450129121d7b6cba
Raphi <raphael@smartive.ch>
2025-08-25 15:30:33 +0200

    add login route

--- website/src/app/portal/(routes)/(public)/login/page.tsx
+++ website/src/app/portal/(routes)/(public)/login/page.tsx
@@ -6,7 +6,7 @@ export default async function Page() {
 		<div className="flex min-h-screen items-center px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16">
 			<div className="w-full space-y-6 text-center">
 				<p className="text-gradient animate-pulse bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 bg-clip-text text-2xl font-semibold text-transparent">
-					🚀 Coming Soon!
+					🚀 Login Page Coming Soon!
 				</p>
 				<Button asChild>
 					<Link href="/login" prefetch={false}>
--- website/src/app/portal/layout.tsx
+++ website/src/app/portal/layout.tsx
@@ -1,5 +1,3 @@
-import { AppShell } from '@/app/portal/components/app-shell';
-import { getAuthenticatedUserOrRedirect } from '@/lib/firebase/current-user';
 import { notFound } from 'next/navigation';
 import type { ReactNode } from 'react';
 
@@ -9,7 +7,6 @@ const ENABLE_PORTAL = process.env.NEXT_PUBLIC_FEATURE_ENABLE_PORTAL === 'true';
 
 export default async function PortalLayout({ children }: { children: ReactNode }) {
 	if (!ENABLE_PORTAL) notFound();
-	const user = await getAuthenticatedUserOrRedirect();
 
-	return <AppShell user={user}>{children}</AppShell>;
+	return <>{children}</>;
 }
