151f6c2fda6d81cb2641c3604aa471153bb15475
Gavriil <gavriiltzortzakis@gmail.com>
2025-09-20 13:08:54 +0200

    Sitemap generator

---
+++ website/scripts/generate-paths.ts
@@ -0,0 +1,47 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+
+// Currently there is no standard way to get all static paths of NextJS
+function getStaticPages(startPath: string): string[] {
+	const routes: string[] = [];
+	const excludedPrefixes = ['_', '['];
+	function traverse(currentPath: string, currentRoute = '') {
+		const directoryEntries = fs.readdirSync(currentPath, { withFileTypes: true });
+
+		for (const directoryEntry of directoryEntries) {
+			const fullPath = path.join(currentPath, directoryEntry.name);
+			if (!excludedPrefixes.some((char) => directoryEntry.name.startsWith(char))) {
+				if (directoryEntry.isDirectory()) {
+					traverse(fullPath, 	directoryEntry.name.startsWith('(') ? currentRoute : `${currentRoute}/${directoryEntry.name}`);
+				} else if (
+					(directoryEntry.isFile() && directoryEntry.name === 'page.tsx') ||
+					directoryEntry.name === 'page.ts'
+				) {
+					const route = `${currentRoute}/${directoryEntry.name.replace(/page\.(tsx|ts)$/, '')}`
+						.replace(/^\//, '')
+						.replace(/\/+$/, '');
+					routes.push(route);
+				}
+			}
+		}
+	}
+	traverse(startPath);
+	return routes;
+}
+
+
+function writePagesToFile(pages: string[], folderPath: string, fileName: string) {
+	fs.mkdirSync(folderPath, { recursive: true });
+	const filePath = path.join(folderPath, fileName);
+	const content = JSON.stringify(pages, null, 2);
+	fs.writeFileSync(filePath, content);
+	console.log(`✅ Successfully wrote ${pages.length} paths to ${filePath}`);
+}
+
+const WEBSITE_LOCAL_PATH = 'src/app/[lang]/[region]/(website)';
+const pages = getStaticPages(path.join(process.cwd(), WEBSITE_LOCAL_PATH));
+const outputFolder = path.join(process.cwd(), 'src/app/');
+const outputFile = 'static-pages.json';
+
+writePagesToFile(pages, outputFolder, outputFile);
---
+++ website/src/app/sitemap.ts
@@ -0,0 +1,119 @@
+import { getAuthors, getOverviewArticles, getTags } from '@/components/storyblok/StoryblokApi';
+import { toDateObject } from '@/components/storyblok/StoryblokUtils';
+import { defaultLanguage } from '@/lib/i18n/utils';
+import { storyblokInitializationWorkaround } from '@/storyblok-init';
+import { type StoryblokArticle, StoryblokAuthor, StoryblokTag } from '@/types/journal';
+import fs from 'fs';
+import type { MetadataRoute } from 'next';
+import path from 'path';
+import { ISbStories } from 'storyblok-js-client/src/interfaces';
+
+export const revalidate = 86400; // per day
+const url = 'https://socialincome.org';
+storyblokInitializationWorkaround();
+
+
+
+const SUPPORTED_LANGUAGES = ['de', 'fr', 'it'];
+
+function articleUrl(slug: string, lang: string) {
+	return `${url}/${lang}/journal/${slug}`;
+}
+
+function tagUrl(slug: string, lang: string) {
+	return `${url}/${lang}/journal/tag/${slug}`;
+}
+
+function authorUrl(slug: string, lang: string) {
+	return `${url}/${lang}/journal/author/${slug}`;
+}
+
+function generateAlternativeLanguages(alternativeArticles: Record<string, string[]>, slug: string) {
+	return Object.fromEntries(
+		SUPPORTED_LANGUAGES.filter((lang) => alternativeArticles[lang].includes(slug)).map((lang) => [
+			lang,
+			articleUrl(slug, lang),
+		]),
+	);
+}
+
+function generateStoryblokArticlesSitemap(
+	blogsResponse: ISbStories<StoryblokArticle>,
+	blogsAlternativeLanguages: { lang: string; stories: ISbStories<StoryblokArticle> }[],
+): MetadataRoute.Sitemap {
+	const alternativeArticles = Object.fromEntries(
+		blogsAlternativeLanguages.map(({ lang, stories }) => [lang, stories.data.stories.map((it) => it.slug)]),
+	) as Record<string, string[]>;
+	return blogsResponse.data.stories.map((article) => ({
+		url: articleUrl(article.slug, defaultLanguage),
+		alternates: {
+			languages: generateAlternativeLanguages(alternativeArticles, article.slug),
+		},
+		changeFrequency: 'monthly',
+		lastModified: toDateObject(article.updated_at!, defaultLanguage).toString(),
+	}));
+}
+
+function generateStoryblokAuthorsSitemap(authorsResponse: ISbStories<StoryblokAuthor>): MetadataRoute.Sitemap {
+	return authorsResponse.data.stories.map((author) => ({
+		url: authorUrl(author.slug, defaultLanguage),
+		alternates: {
+			languages: Object.fromEntries(SUPPORTED_LANGUAGES.map((lang) => [lang, authorUrl(author.slug, lang)])),
+		},
+		changeFrequency: 'weekly',
+	}));
+}
+
+function generateStoryblokTagSitemap(tagsResponse: ISbStories<StoryblokTag>): MetadataRoute.Sitemap {
+	return tagsResponse.data.stories.map((tag) => ({
+		url: tagUrl(tag.slug, defaultLanguage),
+		alternates: {
+			languages: Object.fromEntries(SUPPORTED_LANGUAGES.map((lang) => [lang, tagUrl(tag.slug, lang)])),
+		},
+		changeFrequency: 'weekly',
+	}));
+}
+
+function staticPageUrl(route: string, lang: string) {
+	return `${url}/${lang}/${route}`;
+}
+
+function generateStaticPagesSitemap(): MetadataRoute.Sitemap {
+	const file = fs.readFileSync('src/app/static-pages.json').toString();
+	const staticRoutes: string[] = JSON.parse(file);
+	return staticRoutes.map((route) => ({
+		url: staticPageUrl(route, defaultLanguage),
+		alternates: {
+			languages: Object.fromEntries(SUPPORTED_LANGUAGES.map((lang) => [lang, staticPageUrl(route, lang)])),
+		},
+	}));
+}
+
+function getBlogsInAlternativeLanguagesPromise() {
+	return Promise.all(
+		SUPPORTED_LANGUAGES.map(async (lang) => ({
+			lang,
+			stories: await getOverviewArticles(lang),
+		})),
+	);
+}
+
+const STATIC_SITEMAP = generateStaticPagesSitemap();
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+	try {
+		const [blogsResponse, blogsAlternativeLanguages, authorsResponse, tagsResponse] = await Promise.all([
+			getOverviewArticles(defaultLanguage),
+			getBlogsInAlternativeLanguagesPromise(),
+			getAuthors(defaultLanguage),
+			getTags(defaultLanguage),
+		]);
+		return STATIC_SITEMAP.concat(
+			generateStoryblokArticlesSitemap(blogsResponse, blogsAlternativeLanguages),
+			generateStoryblokAuthorsSitemap(authorsResponse),
+			generateStoryblokTagSitemap(tagsResponse),
+		);
+	} catch (error) {
+		console.error('Failed to generate full sitemap', error);
+		return STATIC_SITEMAP;
+	}
+}
