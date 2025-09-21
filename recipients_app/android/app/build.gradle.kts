f2344db46a9237ad8e65813d266ef8ec9faff136
Karin Berg <karin@karinberg.de>
2025-09-21 23:08:21 +0200

    Fix gradle files

--- recipients_app/android/app/build.gradle.kts
+++ recipients_app/android/app/build.gradle.kts
@@ -44,16 +44,18 @@ android {
     }
 
     signingConfigs {
-        if (System.getenv()["CI"].toBoolean()) { // CI=true is exported by Codemagic
-            storeFile = file(System.getenv()["CM_KEYSTORE_PATH"])
-            storePassword = System.getenv()["CM_KEYSTORE_PASSWORD"]
-            keyAlias = System.getenv()["CM_KEY_ALIAS"]
-            keyPassword = System.getenv()["CM_KEY_PASSWORD"]
-        } else {
-            storeFile = file(keystoreProperties.getProperty("storeFile"))
-            storePassword = keystoreProperties.getProperty("storePassword")
-            keyAlias = keystoreProperties.getProperty("keyAlias")
-            keyPassword = keystoreProperties.getProperty("keyPassword")
+        create("release") {
+            if (System.getenv()["CI"].toBoolean()) { // CI=true is exported by Codemagic
+                storeFile = file(System.getenv()["CM_KEYSTORE_PATH"])
+                storePassword = System.getenv()["CM_KEYSTORE_PASSWORD"]
+                keyAlias = System.getenv()["CM_KEY_ALIAS"]
+                keyPassword = System.getenv()["CM_KEY_PASSWORD"]
+            } else {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+                storePassword = keystoreProperties["storePassword"] as String
+            }
         }
     }
 
