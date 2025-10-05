8b59c68f2d3aa19decd98573709d5f5ca81b6149
Karin Berg <karin@karinberg.de>
2025-10-05 15:40:22 +0200

    Android: Correct minSdk. to 24 because this is set anyway internally by Flutter 3.35.5

--- recipients_app/android/app/build.gradle.kts
+++ recipients_app/android/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         applicationId = "org.socialincome.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = 24
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
