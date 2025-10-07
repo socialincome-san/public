829724f4f00094ece6407cbecbe3d0a14805cd24
Raphi <raphael@smartive.ch>
2025-10-07 09:17:36 +0200

    add db migrations

--- shared/src/database/migrations/20250820173248_link_surveys_to_program/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "survey" ADD COLUMN     "program_id" TEXT;
-
--- AddForeignKey
-ALTER TABLE "survey" ADD CONSTRAINT "survey_program_id_fkey" FOREIGN KEY ("program_id") REFERENCES "program"("id") ON DELETE SET NULL ON UPDATE CASCADE;
