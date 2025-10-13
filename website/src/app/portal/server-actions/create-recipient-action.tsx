6609e3e1f4c3b608c97f6699b5c81bb3e303f3d4
rawi96 <rawi96@users.noreply.github.com>
2025-10-13 17:57:15 +0000

    Prettified Code!

--- website/src/app/portal/server-actions/create-recipient-action.tsx
+++ website/src/app/portal/server-actions/create-recipient-action.tsx
@@ -1,22 +1,8 @@
 'use server';
 
-import { RecipientService } from '@socialincome/shared/src/database/services/recipient/recipient.service';
 import { revalidatePath } from 'next/cache';
 
 export async function createRecipientAction() {
-	const recipientService = new RecipientService();
-
-	await recipientService.create({
-		accountId: 'todo',
-		contactId: 'todo',
-		programId: 'todo',
-		localPartnerId: 'todo',
-		status: 'active',
-		startDate: new Date(),
-		successorName: null,
-		termsAccepted: true,
-		paymentInformationId: null,
-	});
-
+	//todo create / update recipient
 	revalidatePath('/portal/management/recipients');
 }
