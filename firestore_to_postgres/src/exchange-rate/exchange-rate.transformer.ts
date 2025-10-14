bd79ce297ecda5dab6425b6cf10d57dfefa26e56
Raphi <raphael@smartive.ch>
2025-10-14 12:27:56 +0200

    feat: only import the last month of exchange-rates

--- firestore_to_postgres/src/exchange-rate/exchange-rate.transformer.ts
+++ firestore_to_postgres/src/exchange-rate/exchange-rate.transformer.ts
@@ -5,12 +5,14 @@ import { FirestoreExchangeRate } from './exchange-rate.types';
 export class ExchangeRateTransformer extends BaseTransformer<FirestoreExchangeRate, Prisma.ExchangeRateCreateInput> {
 	transform = async (input: FirestoreExchangeRate[]): Promise<Prisma.ExchangeRateCreateInput[]> => {
 		const transformed: Prisma.ExchangeRateCreateInput[] = [];
-		const oneYearAgo = new Date();
-		oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+		const oneMonthAgo = new Date();
+		oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
 
 		for (const entry of input) {
 			const timestamp = new Date(entry.timestamp * 1000);
-			if (timestamp < oneYearAgo) continue;
+			if (timestamp < oneMonthAgo) {
+				continue;
+			}
 
 			const baseId = entry.id;
 
