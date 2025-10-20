bdf2180216df7561275c1e324a03980d1cc0caf5
Fabrice Michaud <fabrice@smartive.ch>
2025-10-20 13:58:50 +0200

    chore: remove comments

--- shared/src/database/services/payout-forecast/payout-forecast.types.ts
+++ shared/src/database/services/payout-forecast/payout-forecast.types.ts
@@ -1,18 +1,10 @@
 export type PayoutForecastTableViewRow = {
-	/** Example: "November 2025" */
 	period: string;
-
-	/** Number of active recipients expected to receive payouts in this period */
 	numberOfRecipients: number;
-
-	/** Total payout sum in the program’s currency */
 	amountInProgramCurrency: number;
-
-	/** Converted payout sum in USD using latest exchange rate */
 	amountUsd: number;
 };
 
 export type PayoutForecastTableViewProgramScoped = {
-	/** The forecast rows for each upcoming month */
 	tableRows: PayoutForecastTableViewRow[];
 };
