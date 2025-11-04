1c506d088d4379939feb05e3083d104f6e0860e5
Fabrice Michaud <fabrice@smartive.ch>
2025-11-04 10:47:55 +0100

    chore: move form schema to helper

--- website/src/app/portal/(routes)/(protected)/management/campaigns/campaigns-form-helper.ts
+++ website/src/app/portal/(routes)/(protected)/management/campaigns/campaigns-form-helper.ts
@@ -1,8 +1,170 @@
+import { FormField } from '@/components/dynamic-form/dynamic-form';
 import {
 	CampaignsCreateInput,
 	CampaignsUpdateInput,
 } from '@socialincome/shared/src/database/services/campaign/campaign.types';
-import { CampaignsFormSchema } from './campaigns-form';
+import z from 'zod';
+
+type CampaignsFormSchema = {
+	label: string;
+	fields: {
+		title: FormField;
+		description: FormField;
+		secondDescriptionTitle: FormField;
+		secondDescription: FormField;
+		thirdDescriptionTitle: FormField;
+		thirdDescription: FormField;
+		linkWebsite: FormField;
+		linkInstagram: FormField;
+		linkTiktok: FormField;
+		linkFacebook: FormField;
+		linkX: FormField;
+		goal: FormField;
+		currency: FormField;
+		additionalAmountChf: FormField;
+		endDate: FormField;
+		isActive: FormField;
+		public: FormField;
+		featured: FormField;
+		slug: FormField;
+		metadataDescription: FormField;
+		metadataOgImage: FormField;
+		metadataTwitterImage: FormField;
+		creatorName: FormField;
+		creatorEmail: FormField;
+		program: FormField;
+	};
+};
+export const initialFormSchema: CampaignsFormSchema = {
+	label: 'Campaign',
+	fields: {
+		title: {
+			placeholder: 'Title of the content',
+			label: 'Title',
+			zodSchema: z.string().min(1, 'Title is required.'),
+		},
+		description: {
+			placeholder: 'A detailed description...',
+			label: 'Description',
+			zodSchema: z.string().min(1, 'Description is required.'),
+		},
+		secondDescriptionTitle: {
+			placeholder: 'Second Description Title',
+			label: 'Second Description Title',
+			zodSchema: z.string().optional(), // Optional field
+		},
+		secondDescription: {
+			placeholder: 'Additional details...',
+			label: 'Second Description',
+			zodSchema: z.string().optional(), // Optional field
+		},
+		thirdDescriptionTitle: {
+			placeholder: 'Third Description Title',
+			label: 'Third Description Title',
+			zodSchema: z.string().optional(), // Optional field
+		},
+		thirdDescription: {
+			placeholder: 'Final additional details...',
+			label: 'Third Description',
+			zodSchema: z.string().optional(), // Optional field
+		},
+		linkWebsite: {
+			placeholder: 'https://www.yourwebsite.com',
+			label: 'Website Link',
+			zodSchema: z.string().optional(),
+		},
+		linkInstagram: {
+			placeholder: 'Your Instagram handle or profile URL',
+			label: 'Instagram Link',
+			zodSchema: z.string().optional(),
+		},
+		linkTiktok: {
+			placeholder: 'Your TikTok handle or profile URL',
+			label: 'TikTok Link',
+			zodSchema: z.string().optional(),
+		},
+		linkFacebook: {
+			placeholder: 'Your Facebook profile or page URL',
+			label: 'Facebook Link',
+			zodSchema: z.string().optional(),
+		},
+		linkX: {
+			placeholder: 'Your X (Twitter) handle or profile URL',
+			label: 'X (Twitter) Link',
+			zodSchema: z.string().optional(),
+		},
+		goal: {
+			placeholder: 'Target amount for goal',
+			label: 'Goal Amount',
+			zodSchema: z.number().positive('Goal must be positive').nullable(),
+		},
+		currency: {
+			placeholder: 'e.g., USD, EUR, CHF',
+			label: 'Currency Code',
+			zodSchema: z.string().length(3, 'Currency code must be 3 letters.'), // Assuming a 3-letter currency code
+		},
+		additionalAmountChf: {
+			placeholder: 'Additional Amount in CHF',
+			label: 'Additional CHF Amount',
+			zodSchema: z.number().positive('Amount must be positive').nullable(),
+		},
+		endDate: {
+			label: 'End Date',
+			zodSchema: z.date().min(new Date('2020-01-01')).max(new Date('2050-12-31')),
+		},
+		isActive: {
+			label: 'Active',
+			zodSchema: z.boolean(),
+		},
+		public: {
+			placeholder: 'Is the item publicly visible?',
+			label: 'Public',
+			zodSchema: z.boolean().optional(),
+		},
+		featured: {
+			placeholder: 'Should the item be featured?',
+			label: 'Featured',
+			zodSchema: z.boolean().optional(),
+		},
+		slug: {
+			placeholder: 'unique-readable-id',
+			label: 'Slug (URL Identifier)',
+			zodSchema: z
+				.string()
+				.regex(/^[a-z0-9]+(?:[_-][a-z0-9]+)*$/, 'Invalid slug format.')
+				.optional(), // Common slug format
+		},
+		metadataDescription: {
+			placeholder: 'SEO description for search engines',
+			label: 'Metadata Description',
+			zodSchema: z.string().max(300).optional(),
+		},
+		metadataOgImage: {
+			placeholder: 'URL for Open Graph Image',
+			label: 'Open Graph Image URL',
+			zodSchema: z.string().optional(),
+		},
+		metadataTwitterImage: {
+			placeholder: 'URL for Twitter Card Image',
+			label: 'Twitter Image URL',
+			zodSchema: z.string().optional(),
+		},
+		creatorName: {
+			placeholder: 'Name of the creator',
+			label: 'Creator Name',
+			zodSchema: z.string().optional(),
+		},
+		creatorEmail: {
+			placeholder: 'Email of the creator',
+			label: 'Creator Email',
+			zodSchema: z.string().email('Must be a valid email address.').optional(),
+		},
+		program: {
+			placeholder: 'Program',
+			label: 'Program',
+		},
+	},
+};
 
 export function buildUpdateCampaignsInput(schema: CampaignsFormSchema): CampaignsUpdateInput {
 	return {
