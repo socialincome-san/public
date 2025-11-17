08b497659957adf6692e05989f9fa905259fe818
Raphael Wirth <37732078+rawi96@users.noreply.github.com>
2025-11-17 09:56:14 +0100

    fix: sentry logging (#1387)

--- website/sentry.server.config.ts
+++ website/sentry.server.config.ts
@@ -1,22 +1,8 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from '@sentry/nextjs';
+import { baseSentryConfig } from './sentry.base.config';
 
 if (process.env.NODE_ENV !== 'development') {
 	Sentry.init({
-		dsn: 'https://3ab06c73c11cd615db9df59b15786025@o4507045017026560.ingest.us.sentry.io/4507045018992640',
-
-		tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1,
-
-		// Setting this option to true will print useful information to the console while you're setting up Sentry.
-		debug: false,
-		sendDefaultPii: false,
-		release: process.env.NEXT_PUBLIC_APP_VERSION || undefined,
-		environment: process.env.NEXT_PUBLIC_APP_ENVIRONMENT, // 'prod' or 'staging'
-
-		// Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-		// spotlight: process.env.NODE_ENV === 'development',
+		...baseSentryConfig,
 	});
 }
