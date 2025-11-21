cd55b748fb71281da3985b8181478d8da4cf4c6f
Raphi <raphael@smartive.ch>
2025-11-21 13:42:38 +0100

    feat: remove functions and admin

--- functions/jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	testPathIgnorePatterns: ['.d.ts', '.js'],
-	testTimeout: 60000,
-};
