ed7bd6f758d018ea3ec6bf85ad4a239a9e55c51f
Gavriil <gavriiltzortzakis@gmail.com>
2025-11-22 13:58:07 +0100

    styling

--- website/src/app/[lang]/[region]/(website)/journal/[slug]/page.tsx
+++ website/src/app/[lang]/[region]/(website)/journal/[slug]/page.tsx
@@ -206,7 +206,7 @@ export default async function Page(props: {
 						<Typography size="4xl" className="text-center" weight="semibold">
 							{translator.t('article.keep-reading')}
 						</Typography>
-						<div className="mb-10 mt-3 grid grid-cols-1 content-center justify-center gap-4 p-10 md:pl-20 md:pr-20 lg:grid-cols-3">
+						<div className="mb-10 mt-3 grid grid-cols-1 content-center justify-center p-5 gap-4 md:pl-20 md:pr-20 lg:grid-cols-3">
 							{articles.map((article) => (
 								<StoryblokArticleCard
 									key={article.uuid}
