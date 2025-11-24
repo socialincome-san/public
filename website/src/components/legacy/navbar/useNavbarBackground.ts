538ae65d174ceea1c3e8216c2e3d7c6f48c5c5d0
Fabrice Michaud <fabrice@smartive.ch>
2025-11-24 11:33:00 +0100

    feat: remove non neccessary query cleint logic

--- website/src/components/legacy/navbar/useNavbarBackground.ts
+++ website/src/components/legacy/navbar/useNavbarBackground.ts
@@ -1,25 +1,9 @@
 'use client';
 
-import { useQuery, useQueryClient } from '@tanstack/react-query';
-import { useCallback } from 'react';
-
-const BACKGROUND_COLOR_QUERY_KEY = ['globalBackgroundColor'];
+import { useState } from 'react';
 
 export function useNavbarBackground() {
-	const queryClient = useQueryClient();
-
-	const { data: backgroundColor } = useQuery<string | null>({
-		queryKey: BACKGROUND_COLOR_QUERY_KEY,
-		queryFn: () => null,
-		staleTime: Infinity,
-	});
-
-	const setBackgroundColor = useCallback(
-		(color: string | null) => {
-			queryClient.setQueryData(BACKGROUND_COLOR_QUERY_KEY, color);
-		},
-		[queryClient],
-	);
+	const [backgroundColor, setBackgroundColor] = useState<string | null>();
 
 	return { backgroundColor, setBackgroundColor };
 }
