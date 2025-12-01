3fc746ebc1bc4048f646c2fafac42cf3055b38a7
Raphael Wirth <37732078+rawi96@users.noreply.github.com>
2025-12-01 11:30:44 +0100

    feat: improve error message when no account found in db (#1465)
    Co-authored-by: rawi96 <rawi96@users.noreply.github.com>

--- website/src/app/[lang]/[region]/(website)/me/page.tsx
+++ website/src/app/[lang]/[region]/(website)/me/page.tsx
@@ -1,10 +1,18 @@
+import { Translator } from '@socialincome/shared/src/utils/i18n';
+import { BaseContainer } from '@socialincome/ui';
 import { Suspense } from 'react';
+import { DefaultPageProps } from '../..';
 import { AccountRedirect } from './account-redirect';
 
-export default function Page() {
+export default async function Page({ params }: DefaultPageProps) {
+	const { lang } = await params;
+	const translator = await Translator.getInstance({ language: lang, namespaces: ['website-login'] });
+
 	return (
-		<Suspense fallback={<div>Detecting account type...</div>}>
-			<AccountRedirect />
-		</Suspense>
+		<BaseContainer className="min-h-screen-navbar mx-auto flex max-w-lg flex-col">
+			<Suspense fallback={translator.t('detect-account-type')}>
+				<AccountRedirect lang={lang} />
+			</Suspense>
+		</BaseContainer>
 	);
 }
