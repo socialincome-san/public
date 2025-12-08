36b74d98e051028e9a4e8065d331873516e436dd
Raphi <raphael@smartive.ch>
2025-12-08 14:57:40 +0100

    ignore used deps

--- website/knip.config.ts
+++ website/knip.config.ts
@@ -1,8 +1,39 @@
 import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
-	ignoreFiles: ['schema.prisma'],
-	ignoreDependencies: ['react', 'react-hook-form', 'zod', '@hookform/resolvers'],
+	ignoreFiles: ['src/lib/database/schema.prisma'],
+	ignoreDependencies: [
+		'react',
+		'react-hook-form',
+		'zod',
+		'@hookform/resolvers',
+		'@sendgrid/mail',
+		'dotenv-cli',
+		'prisma',
+		'next-openapi-gen',
+		'tsx',
+		'storyblok-js-client',
+		'@radix-ui/react-accordion',
+		'@radix-ui/react-popover',
+		'@radix-ui/react-progress',
+		'@radix-ui/react-switch',
+		'@firebase/analytics',
+		'firebase-admin',
+		'@sendgrid/client',
+	],
+	ignoreBinaries: [
+		'tsx',
+		'next',
+		'firebase',
+		'jest',
+		'playwright',
+		'eslint',
+		'next-openapi-gen',
+		'tsc',
+		'dotenv',
+		'prisma',
+		'knip',
+	],
 };
 
 export default config;
