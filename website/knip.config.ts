d018b3f12fa877b897bcec5a92138bbd314112a5
rawi96 <rawi96@users.noreply.github.com>
2025-12-08 13:46:45 +0000

    Prettified Code!

--- website/knip.config.ts
+++ website/knip.config.ts
@@ -1,7 +1,7 @@
 import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
-  ignoreFiles: ['schema.prisma'],
+	ignoreFiles: ['schema.prisma'],
 	ignoreDependencies: ['react', 'react-hook-form', 'zod', '@hookform/resolvers'],
 };
 
