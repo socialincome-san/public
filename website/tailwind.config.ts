500b3eaba3b0e2690bfa89279dbdc184bdd30d53
Raphi <raphael@smartive.ch>
2025-12-09 08:07:25 +0100

    move assets to ui

--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -1,4 +1,4 @@
 export default {
-	content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', '../ui/src/**/*.{js,jsx,ts,tsx,mdx,html}', '../shared/locales/**/*.json'],
+	content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', '../ui/src/**/*.{js,jsx,ts,tsx,mdx,html}'],
 	presets: [require('../ui/tailwind.config')],
 };
