5d5bb4b33faaa660b4c037ddec8297434482b6be
rawi96 <rawi96@users.noreply.github.com>
2026-01-14 12:06:40 +0000

    Prettified Code!

--- website/src/components/data-table/columns/countries.tsx
+++ website/src/components/data-table/columns/countries.tsx
@@ -4,10 +4,10 @@ import { CountryFlag } from '@/components/country-flag'; // adjust path to where
 import { ActionCell } from '@/components/data-table/elements/action-cell';
 import { DateCell } from '@/components/data-table/elements/date-cell';
 import { SortableHeader } from '@/components/data-table/elements/sortable-header';
+import { StatusCell } from '@/components/data-table/elements/status-cell';
 import { TextCell } from '@/components/data-table/elements/text-cell';
 import type { CountryTableViewRow } from '@/lib/services/country/country.types';
 import type { ColumnDef } from '@tanstack/react-table';
-import { StatusCell } from '@/components/data-table/elements/status-cell';
 
 export function makeCountryColumns(): ColumnDef<CountryTableViewRow>[] {
 	return [
