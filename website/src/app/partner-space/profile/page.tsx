66fe87d6925bb5dcbe6b693db2609c720e39ed33
Raphi <raphael@smartive.ch>
2026-01-19 11:05:52 +0100

    change text

--- website/src/app/partner-space/profile/page.tsx
+++ website/src/app/partner-space/profile/page.tsx
@@ -1,14 +1,5 @@
 import { Card } from '@/components/card';
 
 export default function ProfilePage() {
-	return (
-		<Card>
-			profile form
-			{/* <ProfileForm
-				contributor={contributor}
-				translations={translations}
-				isNewsletterSubscribed={newsletterSubscribed}
-			/> */}
-		</Card>
-	);
+	return <Card>Profile Form for Partners coming soon</Card>;
 }
