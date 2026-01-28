4f31f9caa3dc255ed67a0df4baef5cec9f1e7f63
Raphi <raphael@smartive.ch>
2026-01-28 09:32:48 +0100

    remove placeholder

--- website/src/app/portal/admin/countries/countries-form.tsx
+++ website/src/app/portal/admin/countries/countries-form.tsx
@@ -41,7 +41,6 @@ const initialFormSchema: CountryFormSchema = {
 			label: 'Country',
 			zodSchema: z.enum(isoCountries.map((c) => c.isoCode) as [string, ...string[]]),
 			useCombobox: true,
-			placeholder: 'Select country',
 			options: isoCountries.map((c) => ({
 				id: c.isoCode,
 				label: c.name,
