659b1cff6f07ca2b3ff456239004c506864aa37d
Raphi <raphael@smartive.ch>
2026-02-04 09:11:17 +0100

    feat: add e2e test for donation form

--- website/test/e2e/public-website/donation.e2e.ts
+++ website/test/e2e/public-website/donation.e2e.ts
@@ -1,50 +1,47 @@
 import { seedDatabase } from '@/lib/database/seed/run-seed';
-import { test } from '@playwright/test';
+import { FirebaseService } from '@/lib/services/firebase/firebase.service';
+import { expect, test } from '@playwright/test';
 
 test.beforeEach(async () => {
 	await seedDatabase();
 });
 
-// test('Stripe One Time Donation flow', async ({ page }) => {
-// 	const TEST_EMAIL = 'dean.winchester@supernatural.com';
+test('Stripe One Time Donation flow', async ({ page }) => {
+	const TEST_EMAIL = 'dean.winchester@supernatural.com';
 
-// 	const firebaseService = new FirebaseService();
-// 	await firebaseService.deleteByEmailIfExists(TEST_EMAIL);
+	const firebaseService = new FirebaseService();
+	await firebaseService.deleteByEmailIfExists(TEST_EMAIL);
 
-// 	await page.goto('/en/int/donate/one-time');
+	await page.goto('/en/int/donate/one-time');
 
-// 	await page.getByRole('button', { name: 'Donate Now' }).click();
+	await page.getByRole('button', { name: 'Donate Now' }).click();
 
-// 	await page.waitForURL(/https:\/\/checkout\.stripe\.com\/c\/pay\/.*/);
+	await page.waitForURL(/https:\/\/checkout\.stripe\.com\/c\/pay\/.*/);
 
-// 	await page.getByRole('textbox', { name: 'Email' }).fill(TEST_EMAIL);
-// 	await page.getByRole('textbox', { name: 'Card number' }).fill('4242 4242 4242 4242');
-// 	await page.getByRole('textbox', { name: 'Expiration' }).fill('12 / 66');
-// 	await page.getByRole('textbox', { name: 'CVC' }).fill('424');
-// 	await page.getByRole('textbox', { name: 'Cardholder name' }).fill('Dean Winchester');
+	await page.getByRole('textbox', { name: 'Email' }).fill(TEST_EMAIL);
+	await page.getByRole('textbox', { name: 'Card number' }).fill('4242 4242 4242 4242');
+	await page.getByRole('textbox', { name: 'Expiration' }).fill('12 / 66');
+	await page.getByRole('textbox', { name: 'CVC' }).fill('424');
+	await page.getByRole('textbox', { name: 'Cardholder name' }).fill('Dean Winchester');
 
-// 	await page.getByRole('combobox', { name: 'Address' }).fill('home');
-// 	await page.getByRole('combobox', { name: 'Address' }).press('Tab');
-// 	await page.getByRole('textbox', { name: 'Address line' }).fill('line 2');
-// 	await page.getByRole('textbox', { name: 'Postal code' }).fill('9000');
-// 	await page.getByRole('textbox', { name: 'City' }).fill('St.Gallen');
+	await page.getByRole('combobox', { name: 'Country or region' }).selectOption({ label: 'Switzerland' });
+	await page.getByText('Enter address manually').click();
+	await page.getByRole('textbox', { name: 'Address line 1' }).fill('Winchester Mansion');
+	await page.getByRole('textbox', { name: 'Postal code' }).fill('12345');
+	await page.getByRole('textbox', { name: 'City' }).fill('Unknown');
 
-// 	await page.getByTestId('hosted-payment-submit-button').click();
+	await page.getByTestId('hosted-payment-submit-button').click();
 
-// 	await page.waitForTimeout(10000);
+	await page.waitForURL(/\/en\/int\/donate\/success\/stripe\/.*/);
+	await page.getByTestId('terms-and-conditions').click();
+	await page.getByRole('button', { name: 'Confirm' }).click();
 
-// 	await expect(page).toHaveScreenshot({ fullPage: true });
+	await page.waitForURL(/\/en\/int\/login.*/);
 
-// 	await page.waitForURL(/\/en\/int\/donate\/success\/stripe\/.*/);
-// 	await page.getByTestId('terms-and-conditions').click();
-// 	await page.getByRole('button', { name: 'Confirm' }).click();
+	// Check if the auth user was created
+	await page.goto('http://localhost:4000/auth');
+	await page.getByPlaceholder('Search by user UID, email address, phone number, or display name').fill(TEST_EMAIL);
+	await expect(page.getByRole('cell', { name: 'Dean Winchester' })).toBeVisible();
 
-// 	await page.waitForURL(/\/en\/int\/login.*/);
-
-// 	// Check if the auth user was created
-// 	await page.goto('http://localhost:4000/auth');
-// 	await page.getByPlaceholder('Search by user UID, email address, phone number, or display name').fill(TEST_EMAIL);
-// 	await expect(page.getByRole('cell', { name: 'Dean Winchester' })).toBeVisible();
-
-// 	//Unfortunately we cannot test if the contribution was created in our DB as we are not able to send the webhook-call to the test environment
-// });
+	//Unfortunately we cannot test if the contribution was created in our DB as we are not able to send the webhook-call to the test environment
+});
