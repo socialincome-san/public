36b9c256f36a661d98203590da9f551fd89621f2
Walkeezy <Walkeezy@users.noreply.github.com>
2026-02-04 15:18:47 +0000

    Prettified Code!

--- website/src/components/block-wrapper.tsx
+++ website/src/components/block-wrapper.tsx
@@ -1,11 +1,7 @@
 import { cn } from '@/lib/utils/cn';
 import { HTMLAttributes, PropsWithChildren } from 'react';
 
-export const BlockWrapper = ({
-	children,
-	className,
-	...rest
-}: PropsWithChildren<HTMLAttributes<HTMLDivElement>>) => {
+export const BlockWrapper = ({ children, className, ...rest }: PropsWithChildren<HTMLAttributes<HTMLDivElement>>) => {
 	return (
 		<div className={cn('storyblok__outline my-8 lg:my-16', className)} {...rest}>
 			{children}
