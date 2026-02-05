910a703f2c0b9d2b8fb003f1042bf52b9420a8fc
Raphi <raphael@smartive.ch>
2026-02-05 14:48:25 +0100

    update scripts

--- website/prisma.config.ts
+++ website/prisma.config.ts
@@ -1,5 +1,8 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
 import { defineConfig, env } from 'prisma/config';
+
+dotenv.config({ path: '.env' });
+dotenv.config({ path: '.env.local', override: true });
 
 export default defineConfig({
 	schema: 'src/lib/database/schema.prisma',
