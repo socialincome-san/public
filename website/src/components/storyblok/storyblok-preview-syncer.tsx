4f5ab91d60ed9a9c2a97f600a89485f2f47070ad
Walkeezy <Walkeezy@users.noreply.github.com>
2026-02-06 13:10:05 +0000

    Prettified Code!

--- website/src/components/storyblok/storyblok-preview-syncer.tsx
+++ website/src/components/storyblok/storyblok-preview-syncer.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import PageContentType from '@/components/content-types/page';
+import type { Page } from '@/generated/storyblok/types/109655/storyblok-components';
 import { storyblokComponents } from '@/lib/services/storyblok/storyblok.config';
 import { registerStoryblokBridge } from '@/lib/storyblok-preview/register-bridge';
-import type { Page } from '@/generated/storyblok/types/109655/storyblok-components';
 import type { ISbStoryData } from '@storyblok/js';
 import { loadStoryblokBridge } from '@storyblok/js';
 import { apiPlugin, storyblokInit } from '@storyblok/react';
