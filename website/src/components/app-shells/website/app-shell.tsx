4236d267b01c28db3a7a46537071ffa90c07b47d
Kevin Walker <kevin@smartive.ch>
2026-02-10 16:11:08 +0100

    Update app-shell.tsx

--- website/src/components/app-shells/website/app-shell.tsx
+++ website/src/components/app-shells/website/app-shell.tsx
@@ -18,7 +18,7 @@ export function WebsiteAppShell({ children, session, lang, region, scope }: Webs
 		<div className="theme-new text-primary flex min-h-screen w-full flex-col bg-gradient-to-br from-[hsl(var(--gradient-background-from))] to-[hsl(var(--gradient-background-to))] bg-fixed antialiased">
 			<Navbar session={session} lang={lang} scope={scope} />
 			<div className="container my-8">{children}</div>
-			<Footer lang={lang} region={region} />
+			{scope === 'website' && <Footer lang={lang} region={region} />}
 		</div>
 	);
 }
