3ecd14078c2976e1e941b9fb0a729759cdf010ca
Raphi <raphael@smartive.ch>
2026-02-10 13:50:01 +0100

    add edit test

--- website/test/e2e/portal/admin/candidates.e2e.ts
+++ website/test/e2e/portal/admin/candidates.e2e.ts
@@ -1,7 +1,7 @@
 import { RecipientStatus } from '@/generated/prisma/enums';
 import { seedDatabase } from '@/lib/database/seed/run-seed';
 import { expect, test } from '@playwright/test';
-import { getCandidateService } from '../../utils';
+import { getCandidateService, getFirebaseAdminService } from '../../utils';
 
 test.beforeEach(async () => {
 	await seedDatabase();
@@ -52,104 +52,96 @@ test('Add new candidate', async ({ page }) => {
 	expect(row?.localPartnerName).toBe(expectedCandidate.localPartnerName);
 });
 
-// test('Edit existing recipient', async ({ page }) => {
-// 	const expected = {
-// 		firstName: 'Peter',
-// 		lastName: 'Parker',
-// 		successorName: 'May Parker',
-// 		callingName: 'Spidey',
-// 		email: 'peter.parker@example.com',
-// 		language: 'en',
-// 		profession: 'Photographer',
-// 		gender: 'female',
-// 		programName: 'Migros Education SL',
-// 		localPartnerName: 'Bo Women Empowerment Group',
-// 		phone: '+666666666',
-// 		paymentProvider: 'orange_money',
-// 		paymentCode: 'OM123456',
-// 		country: 'Sierra Leone',
-// 	};
+test('Edit existing candidate', async ({ page }) => {
+	const expected = {
+		firstName: 'Natasha',
+		lastName: 'Romanoff',
+		status: RecipientStatus.waitlisted,
+		successorName: 'Yelena Belova',
+		callingName: 'Black Widow',
+		email: 'natasha.romanoff@example.com',
+		language: 'en',
+		profession: 'Intelligence Operative',
+		gender: 'female',
+		localPartnerName: 'Bo Women Empowerment Group',
+		phone: '+444444444',
+		paymentProvider: 'orange_money',
+		paymentCode: 'OM123456',
+		country: 'Sierra Leone',
+	};
 
-// 	const firebaseService = await getFirebaseAdminService();
-// 	await firebaseService.deleteByPhoneNumberIfExists('+666666666');
+	const firebaseService = await getFirebaseAdminService();
+	await firebaseService.deleteByPhoneNumberIfExists('+444444444');
 
-// 	await page.goto('http://localhost:3000/portal/management/recipients');
-// 	await page.getByRole('cell', { name: 'Mohamed' }).click();
+	await page.goto('http://localhost:3000/portal/admin/candidates');
+	await page.getByRole('cell', { name: 'Hawa' }).click();
 
-// 	await page.getByTestId('form-item-startDate').locator('button').click();
-// 	await page.getByLabel('Choose the Month').selectOption('2');
-// 	await page.getByLabel('Choose the Year').selectOption('2020');
-// 	await page.getByRole('button', { name: 'Thursday, March 12th,' }).click();
+	await page.getByTestId('form-item-successorName').locator('input').fill(expected.successorName);
+	await page.getByTestId('form-item-termsAccepted').locator('button').click();
 
-// 	await page.getByTestId('form-item-successorName').locator('input').fill(expected.successorName);
-// 	await page.getByTestId('form-item-termsAccepted').locator('button').click();
+	await page.getByTestId('form-item-localPartner').click();
+	await page.getByRole('option', { name: expected.localPartnerName }).click();
 
-// 	await page.getByTestId('form-item-program').click();
-// 	await page.getByRole('option', { name: expected.programName }).click();
+	await page.getByTestId('form-accordion-trigger-paymentInformation').click();
 
-// 	await page.getByTestId('form-item-localPartner').click();
-// 	await page.getByRole('option', { name: expected.localPartnerName }).click();
+	await page.getByTestId('form-item-paymentInformation.provider').click();
+	await page.getByRole('option', { name: expected.paymentProvider }).click();
 
-// 	await page.getByTestId('form-accordion-trigger-paymentInformation').click();
+	await page.getByTestId('form-item-paymentInformation.code').locator('input').fill(expected.paymentCode);
+	await page.getByTestId('form-item-paymentInformation.phone').locator('input').fill(expected.phone);
 
-// 	await page.getByTestId('form-item-paymentInformation.provider').click();
-// 	await page.getByRole('option', { name: expected.paymentProvider }).click();
+	await page.getByTestId('form-accordion-trigger-contact').click();
 
-// 	await page.getByTestId('form-item-paymentInformation.code').locator('input').fill(expected.paymentCode);
-// 	await page.getByTestId('form-item-paymentInformation.phone').locator('input').fill(expected.phone);
+	await page.getByTestId('form-item-contact.firstName').locator('input').fill(expected.firstName);
+	await page.getByTestId('form-item-contact.lastName').locator('input').fill(expected.lastName);
+	await page.getByTestId('form-item-contact.callingName').locator('input').fill(expected.callingName);
+	await page.getByTestId('form-item-contact.email').locator('input').fill(expected.email);
 
-// 	await page.getByTestId('form-accordion-trigger-contact').click();
+	await page.getByTestId('form-item-contact.language').click();
+	await page.getByRole('option', { name: expected.language }).click();
 
-// 	await page.getByTestId('form-item-contact.firstName').locator('input').fill(expected.firstName);
-// 	await page.getByTestId('form-item-contact.lastName').locator('input').fill(expected.lastName);
-// 	await page.getByTestId('form-item-contact.callingName').locator('input').fill(expected.callingName);
-// 	await page.getByTestId('form-item-contact.email').locator('input').fill(expected.email);
+	await page.getByTestId('form-item-contact.dateOfBirth').locator('button').click();
+	await page.getByLabel('Choose the Year').selectOption('1996');
+	await page.getByLabel('Choose the Month').selectOption('2');
+	await page.getByRole('button', { name: 'Tuesday, March 12th,' }).click();
 
-// 	await page.getByTestId('form-item-contact.language').click();
-// 	await page.getByRole('option', { name: expected.language }).click();
+	await page.getByTestId('form-item-contact.profession').locator('input').fill(expected.profession);
 
-// 	await page.getByTestId('form-item-contact.dateOfBirth').locator('button').click();
-// 	await page.getByLabel('Choose the Year').selectOption('1996');
-// 	await page.getByLabel('Choose the Month').selectOption('2');
-// 	await page.getByRole('button', { name: 'Tuesday, March 12th,' }).click();
+	await page.getByTestId('form-item-contact.gender').click();
+	await page.getByRole('option', { name: expected.gender }).click();
 
-// 	await page.getByTestId('form-item-contact.profession').locator('input').fill(expected.profession);
+	await page.getByTestId('form-item-contact.street').locator('input').fill('Main Street');
+	await page.getByTestId('form-item-contact.number').locator('input').fill('42');
+	await page.getByTestId('form-item-contact.city').locator('input').fill('Freetown');
+	await page.getByTestId('form-item-contact.zip').locator('input').fill('1000');
+	await page.getByTestId('form-item-contact.country').click();
+	await page.getByRole('option', { name: expected.country }).click();
 
-// 	await page.getByTestId('form-item-contact.gender').click();
-// 	await page.getByRole('option', { name: expected.gender }).click();
+	await page.getByRole('button', { name: 'Save' }).click();
+	await page.getByTestId('dynamic-form').waitFor({ state: 'detached' });
 
-// 	await page.getByTestId('form-item-contact.street').locator('input').fill('Main Street');
-// 	await page.getByTestId('form-item-contact.number').locator('input').fill('42');
-// 	await page.getByTestId('form-item-contact.city').locator('input').fill('Freetown');
-// 	await page.getByTestId('form-item-contact.zip').locator('input').fill('1000');
-// 	await page.getByTestId('form-item-contact.country').click();
-// 	await page.getByRole('option', { name: expected.country }).click();
+	const candidateService = await getCandidateService();
+	const tableResult = await candidateService.getTableView('user-2');
 
-// 	await page.getByRole('button', { name: 'Save' }).click();
-// 	await page.getByTestId('dynamic-form').waitFor({ state: 'detached' });
+	if (!tableResult.success) {
+		throw new Error(tableResult.error);
+	}
 
-// 	const recipientService = await getRecipientService();
-// 	const tableResult = await recipientService.getTableView('user-2');
+	expect(tableResult.data.tableRows.length).toBe(12);
 
-// 	if (!tableResult.success) {
-// 		throw new Error(tableResult.error);
-// 	}
+	const row = tableResult.data.tableRows.find(
+		(r) => r.firstName === expected.firstName && r.lastName === expected.lastName,
+	);
 
-// 	expect(tableResult.data.tableRows.length).toBe(7);
+	expect(row).toBeDefined();
+	expect(row?.status).toBe(expected.status);
+	expect(row?.localPartnerName).toBe(expected.localPartnerName);
 
-// 	const row = tableResult.data.tableRows.find(
-// 		(r) => r.firstName === expected.firstName && r.lastName === expected.lastName,
-// 	);
-
-// 	expect(row).toBeDefined();
-// 	expect(row?.programName).toBe(expected.programName);
-// 	expect(row?.localPartnerName).toBe(expected.localPartnerName);
-
-// 	// Check if the auth user was created/updated
-// 	await page.goto('http://localhost:4000/auth');
-// 	await page.getByPlaceholder('Search by user UID, email address, phone number, or display name').fill('+666666');
-// 	await expect(page.getByRole('cell', { name: '+666666666' })).toBeVisible();
-// });
+	// Check if the auth user was created/updated
+	await page.goto('http://localhost:4000/auth');
+	await page.getByPlaceholder('Search by user UID, email address, phone number, or display name').fill('+444444');
+	await expect(page.getByRole('cell', { name: '+444444444' })).toBeVisible();
+});
 
 // test('Delete recipient', async ({ page }) => {
 // 	const firebaseService = await getFirebaseAdminService();
