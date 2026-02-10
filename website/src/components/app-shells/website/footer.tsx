cd0a90493f91fd526cd2eb6c5565b7a2c51a1042
Kevin Walker <kevin@smartive.ch>
2026-02-10 11:18:52 +0100

    align logo colors

--- website/src/components/app-shells/website/footer.tsx
+++ website/src/components/app-shells/website/footer.tsx
@@ -2,6 +2,7 @@ import { SocialIncomeLogo } from '@/components/svg/social-income-logo';
 
 export const Footer = () => (
 	<div className="bg-primary container rounded-3xl px-16 pb-8 pt-14 text-white">
-		<SocialIncomeLogo />
+		<SocialIncomeLogo width={222} height={22} />
+		<div className="flex items-center gap-4"></div>
 	</div>
 );
