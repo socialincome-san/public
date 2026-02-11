cf770aae9774dc7b06c775bf9bdd840b12767cea
Raphi <raphael@smartive.ch>
2026-02-11 21:33:52 +0100

    fix

--- website/src/lib/services/storyblok/storyblok.mock.ts
+++ website/src/lib/services/storyblok/storyblok.mock.ts
@@ -5,56 +5,40 @@ const STORYBLOK_HOST = 'api.storyblok.com';
 
 let fetchPatched = false;
 
-function createStableMockHash(url: URL, method: string) {
-	// IMPORTANT: clone + normalize URL for hashing
-	const normalizedUrl = new URL(url.toString());
+function stableHash(url: URL, method: string) {
+	// IMPORTANT: ignore token + cv completely
+	const cleanUrl = new URL(url.toString());
+	cleanUrl.searchParams.delete('token');
+	cleanUrl.searchParams.delete('cv');
 
-	// ignore volatile Storyblok params
-	normalizedUrl.searchParams.delete('token');
-	normalizedUrl.searchParams.delete('cv');
-
-	const dataToHash = {
-		method,
-		url: normalizedUrl.pathname + normalizedUrl.search,
-	};
-
-	return crypto.createHash('sha512').update(JSON.stringify(dataToHash)).digest('hex');
+	return crypto.createHash('sha256').update(`${method}:${cleanUrl.pathname}?${cleanUrl.search}`).digest('hex');
 }
 
 export function patchStoryblokFetch() {
-	if (fetchPatched) {
-		return;
-	}
+	if (fetchPatched) return;
 
-	const originalFetch: typeof global.fetch = global.fetch;
+	const originalFetch = global.fetch;
 
-	global.fetch = (async (input: string | URL | Request, init?: RequestInit): Promise<Response> => {
+	global.fetch = (async (input: any, init?: RequestInit) => {
 		const url = typeof input === 'string' ? new URL(input) : input instanceof URL ? input : new URL(input.url);
 
 		if (url.protocol === 'https:' && url.hostname === STORYBLOK_HOST) {
-			const method = init?.method ?? (input instanceof Request ? input.method : 'GET');
-
-			const mockHash = createStableMockHash(url, method);
+			const method = init?.method ?? 'GET';
+			const hash = stableHash(url, method);
 
 			const proxiedUrl = `${MOCKSERVER_BASE_URL}/${url.hostname}${url.pathname}${url.search}`;
-
-			console.log('[Storyblok Mock][FETCH]', {
-				originalUrl: url.toString(),
-				proxiedUrl,
-				mockHash,
-			});
 
 			return originalFetch(proxiedUrl, {
 				...init,
 				headers: {
 					...(init?.headers ?? {}),
-					'x-mock-hash': mockHash,
+					'x-mock-hash': hash,
 				},
 			});
 		}
 
 		return originalFetch(input, init);
-	}) as typeof global.fetch;
+	}) as typeof fetch;
 
 	fetchPatched = true;
 }
