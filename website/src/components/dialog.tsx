e0635d66d10c1ff729f10cdaa1ef39961d4c2f4a
Kevin Walker <kevin@smartive.ch>
2026-02-23 16:25:57 +0100

    fix

--- website/src/components/dialog.tsx
+++ website/src/components/dialog.tsx
@@ -46,7 +46,7 @@ const DialogContent = React.forwardRef<
 				{...props}
 			>
 				{children}
-				<DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-6 right-6 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden focus:outline-none disabled:pointer-events-none">
+				<DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-6 right-6 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
 					<X className="h-6 w-6" />
 					<span className="sr-only">Close</span>
 				</DialogPrimitive.Close>
