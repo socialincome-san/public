c09de3a2ffcc698beffa489c7a227fc68bed86e6
Kevin Walker <kevin@smartive.ch>
2026-03-02 08:52:44 +0100

    Update dialog.tsx

--- website/src/components/dialog.tsx
+++ website/src/components/dialog.tsx
@@ -39,7 +39,7 @@ const DialogContent = React.forwardRef<
 			<DialogPrimitive.Content
 				ref={ref}
 				className={cn(
-					'bg-background max-w-site-width data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 theme-new fixed top-[50%] left-[50%] z-50 z-200 grid translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200',
+					'bg-background max-w-site-width data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 theme-new fixed top-[50%] left-[50%] z-200 grid translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200',
 					sizeClasses,
 					className,
 				)}
