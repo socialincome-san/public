2e9a19224cb7c99faefe4b7f9b27c4d9185582b5
Sandino Scheidegger <sandino@socialincome.org>
2026-03-12 00:51:49 +0100

    Shorten badge

--- website/src/components/create-program-wizard/step-1/country-condition-badge.tsx
+++ website/src/components/create-program-wizard/step-1/country-condition-badge.tsx
@@ -25,7 +25,7 @@ const CONDITION_UI: Record<
 	},
 	[CountryCondition.RESTRICTIONS_APPLY]: {
 		variant: 'secondary',
-		label: 'Restrictions apply',
+		label: 'Restrictions',
 		Icon: AlertTriangleIcon,
 	},
 };
