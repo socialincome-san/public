045d66882a92041f897ab0239a072542a1ab1b3d
Raphi <raphael@smartive.ch>
2026-03-18 15:41:38 +0100

    ad test

--- website/test/e2e/projects/portal/profile.e2e.ts
+++ website/test/e2e/projects/portal/profile.e2e.ts
@@ -8,14 +8,14 @@ test.beforeEach(async () => {
 });
 
 test('portal profile-page matches screenshot', async ({ page }) => {
-	await page.goto('/portal/profile');
+	await page.goto('/portal/profile/account');
 	await expectToHaveScreenshot(page);
 });
 
 test('portal profile updates user personal info', async ({ page }) => {
 	const updatedFirstName = `Portal-${Date.now()}`;
 
-	await page.goto('/portal/profile');
+	await page.goto('/portal/profile/account');
 	await page.locator('input[name="firstName"]').fill(updatedFirstName);
 	await page.locator('button[type="submit"]').click();
 
@@ -29,4 +29,9 @@ test('portal profile updates user personal info', async ({ page }) => {
 			return user?.contact.firstName ?? null;
 		})
 		.toBe(updatedFirstName);
+});
+
+test('portal organization page matches screenshot', async ({ page }) => {
+	await page.goto('/portal/profile/organization?sortBy=member&sortDirection=asc');
+	await expectToHaveScreenshot(page);
 });
