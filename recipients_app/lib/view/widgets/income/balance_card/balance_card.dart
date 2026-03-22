f66c4bf08c7a5233884f3b3eb44ab19243783cea
Karin Berg <karin@karinberg.de>
2026-03-22 17:57:54 +0100

    Remove unused files

--- recipients_app/lib/view/widgets/income/balance_card/balance_card.dart
+++ recipients_app/lib/view/widgets/income/balance_card/balance_card.dart
@@ -1,6 +1,4 @@
-export "balance_card_bottom_action.dart";
 export "balance_card_container.dart";
 export "balance_card_grid.dart";
 export "balance_card_header.dart";
-export "balance_card_ui_state.dart";
 export "on_hold_bottom_card.dart";
