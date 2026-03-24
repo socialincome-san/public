e567957ddcfa8ce8e07c6eef3fbc87f006656bfd
Kevin Walker <kevin@smartive.ch>
2026-03-24 12:44:40 +0100

    refactor

--- website/src/components/app-shells/website/app-shell.tsx
+++ website/src/components/app-shells/website/app-shell.tsx
@@ -17,17 +17,7 @@ export const WebsiteAppShell = ({ children, sessions, lang, region, scope }: Web
 	const isContained = scope === 'dashboard' || scope === 'partner-space';
 
 	return (
-		<div
-			className="theme-new text-primary flex min-h-screen w-full flex-col antialiased"
-			style={{
-				background: `repeating-linear-gradient(
-      330deg,
-      var(--gradient-background-from) 0px,
-      var(--gradient-background-to) 600px,
-      var(--gradient-background-from) 1200px
-    )`,
-			}}
-		>
+		<div className="theme-new bg-website-gradient text-primary flex min-h-screen w-full flex-col antialiased">
 			<Navbar sessions={sessions} lang={lang} region={region} scope={scope} />
 			<div
 				className={
