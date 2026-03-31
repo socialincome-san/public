4afd1c75b747bfb9d6ff069f6970c031bd85d8b6
Raphi <raphael@smartive.ch>
2026-03-31 11:08:07 +0200

    fix ci

--- website/src/app/[lang]/[region]/new-website/local-partners/[slug]/preview/page.tsx
+++ website/src/app/[lang]/[region]/new-website/local-partners/[slug]/preview/page.tsx
@@ -1,6 +1,6 @@
 import { DefaultLayoutProps, DefaultParams } from '@/app/[lang]/[region]';
 import { StoryblokPreviewLocalPartnerPage } from '@/components/storyblok/storyblok-preview-local-partner-page';
-import { WebsiteLanguage, WebsiteRegion } from '@/lib/i18n/utils';
+import { WebsiteLanguage } from '@/lib/i18n/utils';
 import { NEW_WEBSITE_SLUG } from '@/lib/utils/const';
 
 type PreviewPageProps = DefaultLayoutProps<DefaultParams & { slug: string }> & {
@@ -15,7 +15,6 @@ export default async function PreviewLocalPartnerPage({ params, searchParams }: 
 		<StoryblokPreviewLocalPartnerPage
 			storyPath={`${NEW_WEBSITE_SLUG}/local-partners/${slug}`}
 			lang={lang as WebsiteLanguage}
-			region={region as WebsiteRegion}
 			previewRoutePath={`/${lang}/${region}/${NEW_WEBSITE_SLUG}/local-partners/${slug}/preview`}
 			searchParams={resolvedSearchParams}
 		/>
