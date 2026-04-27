9e6681bbbef8b4f97b9951f8e0a593692bda4f09
MumenthalerManuel <MumenthalerManuel@users.noreply.github.com>
2026-04-27 10:43:46 +0000

    Prettified Code!

--- website/src/components/carousel.tsx
+++ website/src/components/carousel.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import useEmblaCarousel, { type UseEmblaCarouselType } from 'embla-carousel-react';
-import { ArrowLeft, ArrowRight } from 'lucide-react';
 import * as React from 'react';
 
 import { cn } from '../lib/utils/cn';
