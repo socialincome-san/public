ea562ade2e3462c388758b3ee22e7e843476d33b
Raphi <raphael@smartive.ch>
2026-06-17 09:29:04 +0200

    fix tests

--- website/src/app/[lang]/[region]/layout.tsx
+++ website/src/app/[lang]/[region]/layout.tsx
@@ -3,12 +3,8 @@ import { WebsiteAppShell } from '@/components/app-shells/website/app-shell';
 import { getCurrentSessions } from '@/lib/firebase/current-account';
 import { I18nContextProvider } from '@/lib/i18n/i18n-context-provider';
 import { WebsiteLanguage } from '@/lib/i18n/utils';
-import type { Metadata } from 'next';
-import type { PropsWithChildren } from 'react';
 
-export const metadata: Metadata = {
-	robots: { index: false, follow: false },
-};
+import type { PropsWithChildren } from 'react';
 
 export default async function Layout({ children, params }: PropsWithChildren<DefaultLayoutProps>) {
 	const { lang, region } = await params;
