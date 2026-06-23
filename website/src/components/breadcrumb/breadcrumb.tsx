ff7bbec2084f90b39af82a126b4c96891db56dc9
SuryaNarayananDev <suryavignesh6238@gmail.com>
2026-06-23 22:14:27 +0530

    Adjust breadcrumb alignment for mobile layout

--- website/src/components/breadcrumb/breadcrumb.tsx
+++ website/src/components/breadcrumb/breadcrumb.tsx
@@ -13,7 +13,7 @@ export type { BreadcrumbLink as BreadcrumbLinkType } from '@/components/breadcru
 
 export const Breadcrumb = ({ links, className }: { links: BreadcrumbLinkItem[]; className?: string }) => {
 	return (
-		<div className={cn('py-5 pl-4 lg:py-9 lg:pl-0', className)}>
+		<div className={cn('py-9 pl-4 lg:pl-0', className)}>
 			<BreadcrumbElements>
 				<BreadcrumbList>
 					{links.map((link: BreadcrumbLinkItem, index: number) => {
