4e4625cd033547bfda202e2aa60344eb7e0bca71
Raphi <raphael@smartive.ch>
2026-06-24 08:54:26 +0200

    feat: branded error pages

---
+++ website/src/app/loading.tsx
@@ -0,0 +1,5 @@
+import { AppLoadingSkeleton } from '@/components/skeletons/app-loading-skeleton';
+
+export default function Loading() {
+	return <AppLoadingSkeleton message="Loading Social Income..." variant="page" />;
+}
