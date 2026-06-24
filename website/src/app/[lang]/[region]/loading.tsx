9741f50d27c9c56d2a760b8cdb3d2afde625fdbe
Raphael Wirth <37732078+rawi96@users.noreply.github.com>
2026-06-24 13:13:45 +0200

    feat: branded error pages (#2258)

---
+++ website/src/app/[lang]/[region]/loading.tsx
@@ -0,0 +1,5 @@
+import { AppLoadingSkeleton } from '@/components/skeletons/app-loading-skeleton';
+
+export default function WebsiteLoading() {
+	return <AppLoadingSkeleton message="Loading Social Income..." variant="page" />;
+}
