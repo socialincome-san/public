676ea1b3f4f0491e30fbf31bbf00c2b47bf354e6
Raphi <raphael@smartive.ch>
2026-06-25 09:39:52 +0200

    feat: improve typography

--- website/src/components/storyblok/rich-text/shared-resolvers.tsx
+++ website/src/components/storyblok/rich-text/shared-resolvers.tsx
@@ -89,12 +89,12 @@ export const storyblokRichTextMarkResolvers = {
 };
 
 const headingStyles: Record<number, string> = {
-	1: 'text-5xl',
-	2: 'text-4xl',
-	3: 'text-3xl',
-	4: 'text-2xl',
-	5: 'text-xl',
-	6: 'text-lg',
+	1: 'text-5xl md:text-6xl',
+	2: 'text-4xl md:text-5xl',
+	3: 'text-3xl md:text-4xl',
+	4: 'text-2xl md:text-3xl',
+	5: 'text-xl md:text-2xl',
+	6: 'text-lg md:text-xl',
 };
 
 export const storyblokRichTextBasicNodeResolvers = {
