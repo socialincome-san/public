4067e7499ca8f526f5d607075f8e4da314bf8eb0
Sandino Scheidegger <sandino@socialincome.org>
2026-07-22 09:11:44 +0200

    Journal: add lottie animations (#2415)

--- website/src/components/storyblok/rich-text-renderer.tsx
+++ website/src/components/storyblok/rich-text-renderer.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import { LottieBlock } from '@/components/content-blocks/lottie';
 import {
 	storyblokRichTextBasicNodeResolvers,
 	storyblokRichTextMarkResolvers,
 } from '@/components/storyblok/rich-text/shared-resolvers';
+import type { Lottie } from '@/generated/storyblok/types/109655/storyblok-components';
 import type { StoryblokRichtext } from '@/generated/storyblok/types/storyblok';
 import { ReactNode } from 'react';
 import { render } from 'storyblok-rich-text-react-renderer';
@@ -16,5 +18,7 @@ export const RichTextRenderer = ({ richTextDocument }: RichTextRendererProps) =>
 	render(richTextDocument, {
 		markResolvers: storyblokRichTextMarkResolvers,
 		nodeResolvers: storyblokRichTextBasicNodeResolvers,
-		blokResolvers: {},
+		blokResolvers: {
+			lottie: (props) => <LottieBlock blok={props as Lottie} />,
+		},
 	}) as ReactNode;
