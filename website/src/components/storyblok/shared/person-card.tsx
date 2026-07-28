a6da41ed1ec927e3822a6f4bcc2f88ca27a3d917
Sandino Scheidegger <sandino@socialincome.org>
2026-07-28 12:45:06 +0200

    removing transform-gpu

--- website/src/components/storyblok/shared/person-card.tsx
+++ website/src/components/storyblok/shared/person-card.tsx
@@ -106,7 +106,7 @@ export const PersonCard = ({ person, href, size = 'default', className, voluntee
 			className={cn(
 				'bg-card w-full overflow-hidden rounded-xl shadow-[0px_4px_28px_0px_rgba(0,30,101,0.07)]',
 				isSmall ? 'max-w-[260px] p-2.5' : 'max-w-[305px] p-3',
-				href && 'transform-gpu transition-transform hover:scale-[1.01]',
+				href && 'transition-transform hover:scale-[1.01]',
 				className,
 			)}
 		>
