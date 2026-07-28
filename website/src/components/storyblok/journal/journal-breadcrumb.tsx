8c249f8d8c3145a83010b761d21e1b14b8723861
Sandino Scheidegger <sandino@socialincome.org>
2026-07-28 11:28:47 +0200

    Adding spacing btw lead and breadcrumb (#2462)

--- website/src/components/storyblok/journal/journal-breadcrumb.tsx
+++ website/src/components/storyblok/journal/journal-breadcrumb.tsx
@@ -1,5 +1,6 @@
 import { Breadcrumb, type BreadcrumbLinkType } from '@/components/breadcrumb/breadcrumb';
+import { cn } from '@/lib/utils/cn';
 
-export const JournalBreadcrumb = ({ links }: { links: BreadcrumbLinkType[] }) => (
-	<Breadcrumb links={links} className="py-0" />
+export const JournalBreadcrumb = ({ links, className }: { links: BreadcrumbLinkType[]; className?: string }) => (
+	<Breadcrumb links={links} className={cn('py-0', className)} />
 );
