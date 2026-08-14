bc36f1b994bcfd9812d58b837991ca9ba963326a
levi <levi@smartive.ch>
2026-08-14 11:33:03 +0200

    feat(storybook): add component design links

--- website/src/components/card/card.stories.tsx
+++ website/src/components/card/card.stories.tsx
@@ -15,6 +15,10 @@ const meta = {
 				component: 'A reusable card component for content with optional link behavior.',
 			},
 		},
+		design: {
+			type: 'figma',
+			url: 'https://www.figma.com/design/IDEMMGr7QkVOY4Ksbgbc57/Social-Income---shadcn-UI-Kit?node-id=46-65&p=f&t=zjQYKr57x1DPvxtF-0',
+		},
 	},
 	argTypes: {
 		href: {
