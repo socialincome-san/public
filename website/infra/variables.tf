1428c5422b0e8836c6b62213cefab4d11e534be8
Manuel Mumenthaler <manuel@smartive.ch>
2026-08-17 14:53:44 +0200

    remove duplicate adding of turnstile site key (public key)

--- website/infra/variables.tf
+++ website/infra/variables.tf
@@ -97,12 +97,6 @@ variable "stripe_webhook_secret" {
   sensitive   = true
 }
 
-variable "turnstile_site_key" {
-  description = "Cloudflare Turnstile site key for the campaign submission widget"
-  type        = string
-  sensitive   = true
-}
-
 variable "turnstile_secret_key" {
   description = "Cloudflare Turnstile secret key for server-side token verification"
   type        = string
