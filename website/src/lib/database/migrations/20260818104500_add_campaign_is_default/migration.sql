06a4e6c94ac7be80740894f11a24df3419c1564c
Manuel Mumenthaler <manuel@smartive.ch>
2026-08-19 13:24:52 +0200

    restore migration

--- website/src/lib/database/migrations/20260818104500_add_campaign_is_default/migration.sql
+++ website/src/lib/database/migrations/20260818104500_add_campaign_is_default/migration.sql
@@ -1,2 +1,6 @@
--- AlterTable
 ALTER TABLE "campaign" ADD COLUMN "is_default" BOOLEAN NOT NULL DEFAULT false;
+
+-- Backfill defaults from campaign titles
+UPDATE "campaign"
+SET "is_default" = true
+WHERE "title" ILIKE '%Default%';
