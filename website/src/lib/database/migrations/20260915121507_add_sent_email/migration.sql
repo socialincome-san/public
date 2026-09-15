9217fb7a1183eefaa669f8058e32256d934adbe2
levi <levi@smartive.ch>
2026-09-15 14:16:12 +0200

    feat: add sent email database table

---
+++ website/src/lib/database/migrations/20260915121507_add_sent_email/migration.sql
@@ -0,0 +1,22 @@
+-- DropForeignKey
+ALTER TABLE "campaign" DROP CONSTRAINT "campaign_contributor_id_fkey";
+
+-- CreateTable
+CREATE TABLE "sent_email" (
+    "id" TEXT NOT NULL,
+    "recipient" TEXT NOT NULL,
+    "from_email" TEXT NOT NULL,
+    "subject" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "sent_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(3),
+
+    CONSTRAINT "sent_email_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "sent_email_sent_at_idx" ON "sent_email"("sent_at");
+
+-- AddForeignKey
+ALTER TABLE "campaign" ADD CONSTRAINT "campaign_contributor_id_fkey" FOREIGN KEY ("contributor_id") REFERENCES "contributor"("id") ON DELETE SET NULL ON UPDATE CASCADE;
